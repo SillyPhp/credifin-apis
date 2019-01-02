435a07ac9b9216bb96b9cc336bb302ef2d3a9e35
Tarandeep Singh <tarandeep@dsbedutech.in>
2019-01-02 18:30:53 +0530

    Files added
    common/models/AssignedIndustries.php
    
    FIles Updated
    
    frontend/controllers/AccountsController.php
    frontend/controllers/IndustriesController.php

---
+++ frontend/controllers/IndustriesController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\web\Response;
+use common\models\BusinessActivities;
+use common\models\Industries;
+use common\models\AssignedIndustries;
+
+class IndustriesController extends Controller
+{
+
+    /**
+     * @inheritdoc
+     */
+
+    public function actionGetIndustriesByBusinessActivity()
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $id = Yii::$app->request->get('id');
+        if (!empty($id)) {
+            $industries = AssignedIndustries::find()
+                ->alias('a')
+                ->select(['b.industry_enc_id industry_id', 'b.industry'])
+                ->innerJoin(Industries::tableName() . 'as b', 'b.industry_enc_id = a.industry_enc_id')
+                ->innerJoin(BusinessActivities::tableName() . 'as c', 'c.business_activity_enc_id = a.business_activity_enc_id')
+                ->where(['c.business_activity_enc_id' => $id])
+                ->orderBy(['b.industry' => SORT_ASC])
+                ->asArray()
+                ->all();
+            if ($industries) {
+                $response = [
+                    'status' => 200,
+                    'industries' => $industries,
+                ];
+            } else {
+                $response = [
+                    'status' => 201,
+                ];
+            }
+
+            return $response;
+        }
+    }
+
+//    public function actionList()
+//    {
+//        Yii::$app->response->format = Response::FORMAT_JSON;
+//        return Industries::find()
+//            ->orderBy([new \yii\db\Expression('FIELD (industry, "Same Industry", "No Preference") DESC, industry ASC')])
+//            ->asArray()
+//            ->all();
+//    }
+
+}
