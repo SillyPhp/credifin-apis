8fb29c39e4297fa572d26a8edf5f8dd1d0b86e4a
Tarandeep Singh <tarandeep@dsbedutech.in>
2019-01-05 14:57:05 +0530

    Added Configure files for "API"

---
+++ config/api/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'F21pforjcFkGasrFudhAIwjULM7-XHoU129c91283h81231313',
+        ],
+    ],
+];
