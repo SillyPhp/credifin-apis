aef3386d6bd8eb6a2e6a01df785856fc47cf2495
Tarandeep Singh <tarandeep@dsbedutech.in>
2019-01-14 18:00:52 +0530

    Updated Employer Applications Active Record

--- common/models/EmployerApplications.php
+++ common/models/EmployerApplications.php
@@ -2,6 +2,8 @@
 
 namespace common\models;
 
+use Yii;
+
 /**
  * This is the model class for table "{{%employer_applications}}".
  *
@@ -16,7 +18,6 @@ namespace common\models;
  * @property string $designation_enc_id Foreign Key to Designations Table
  * @property string $type Type (Full Time, Part Time, Work from Home)
  * @property string $preferred_industry Foreign Key to Industries Table
- * @property string $questionnaire_enc_id Foreign Key to Questionnaires Table
  * @property string $interview_process_enc_id Foreign Key to Organization Interview Process
  * @property string $timings_from Timings From
  * @property string $timings_to Timings To
@@ -24,7 +25,8 @@ namespace common\models;
  * @property string $last_date Last Date to Apply
  * @property string $experience Minimum Experience Required
  * @property string $preferred_gender Preferred Gender (1 as Male, 2 as Female, 3 as Both)
- * @property string $fill_questionnaire_on Fill Questionnaire (1 as at the time of application, 2 as after initial approval of the application)
+ * @property int $has_questionnaire Has Questionnaire (0 as No, 1 as Yes)
+ * @property int $has_benefits Has Benefits (0 as No, 1 as Yes)
  * @property int $is_sponsored Is Application Sponsored (0 as False, 1 as True)
  * @property int $is_featured Is Application Featured (0 as False, 1 as True)
  * @property string $published_on On which date application was published
@@ -54,7 +56,6 @@ namespace common\models;
  * @property Organizations $organizationEnc
  * @property Users $createdBy
  * @property Users $lastUpdatedBy
- * @property OrganizationQuestionnaire $questionnaireEnc
  * @property InterviewScheduler[] $interviewSchedulers
  * @property ReviewedApplications[] $reviewedApplications
  * @property ShortlistedApplications[] $shortlistedApplications
@@ -75,11 +76,11 @@ class EmployerApplications extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['application_enc_id', 'application_number', 'organization_enc_id', 'application_type_enc_id', 'slug', 'title', 'type', 'timings_from', 'timings_to', 'joining_date', 'last_date', 'preferred_gender', 'published_on', 'image', 'image_location', 'created_on', 'created_by'], 'required'],
-            [['application_number', 'is_sponsored', 'is_featured', 'is_deleted'], 'integer'],
-            [['description', 'type', 'experience', 'preferred_gender', 'fill_questionnaire_on', 'status'], 'string'],
+            [['application_enc_id', 'application_number', 'organization_enc_id', 'application_type_enc_id', 'slug', 'title', 'type', 'timings_from', 'timings_to', 'joining_date', 'last_date', 'preferred_gender', 'published_on', 'image', 'image_location', 'created_by'], 'required'],
+            [['application_number', 'has_questionnaire', 'has_benefits', 'is_sponsored', 'is_featured', 'is_deleted'], 'integer'],
+            [['description', 'type', 'experience', 'preferred_gender', 'status'], 'string'],
             [['timings_from', 'timings_to', 'joining_date', 'last_date', 'published_on', 'created_on', 'last_updated_on'], 'safe'],
-            [['application_enc_id', 'organization_enc_id', 'application_type_enc_id', 'slug', 'title', 'designation_enc_id', 'preferred_industry', 'questionnaire_enc_id', 'interview_process_enc_id', 'image', 'image_location', 'created_by', 'last_updated_by'], 'string', 'max' => 100],
+            [['application_enc_id', 'organization_enc_id', 'application_type_enc_id', 'slug', 'title', 'designation_enc_id', 'preferred_industry', 'interview_process_enc_id', 'image', 'image_location', 'created_by', 'last_updated_by'], 'string', 'max' => 100],
             [['application_enc_id'], 'unique'],
             [['application_number'], 'unique'],
             [['slug'], 'unique'],
@@ -91,7 +92,6 @@ class EmployerApplications extends \yii\db\ActiveRecord
             [['organization_enc_id'], 'exist', 'skipOnError' => true, 'targetClass' => Organizations::className(), 'targetAttribute' => ['organization_enc_id' => 'organization_enc_id']],
             [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => Users::className(), 'targetAttribute' => ['created_by' => 'user_enc_id']],
             [['last_updated_by'], 'exist', 'skipOnError' => true, 'targetClass' => Users::className(), 'targetAttribute' => ['last_updated_by' => 'user_enc_id']],
-            [['questionnaire_enc_id'], 'exist', 'skipOnError' => true, 'targetClass' => OrganizationQuestionnaire::className(), 'targetAttribute' => ['questionnaire_enc_id' => 'questionnaire_enc_id']],
         ];
     }
 
@@ -229,14 +229,6 @@ class EmployerApplications extends \yii\db\ActiveRecord
     public function getLastUpdatedBy()
     {
         return $this->hasOne(Users::className(), ['user_enc_id' => 'last_updated_by']);
-    }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getQuestionnaireEnc()
-    {
-        return $this->hasOne(OrganizationQuestionnaire::className(), ['questionnaire_enc_id' => 'questionnaire_enc_id']);
     }
 
     /**
