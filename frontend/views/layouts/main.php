6cd7aed526aca365f1a8e87922ed2469009c647e
Tarandeep Singh <tarandeep@dsbedutech.in>
2019-01-15 17:25:49 +0530

    Bugs Fixed in Homepage and Footer

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -160,22 +160,22 @@ AppAssets::register($this);
                 <div class="footer-bottom">
                     <div class="container pt-20 pb-20">
                         <div class="row">
-                            <div class="col-md-6 col-sm-6 ">
+                            <div class="col-md-12 col-sm-12 text-center">
                                 <p class="font-11 copyright-text"><?= Yii::t('frontend', 'Copyright') . ' &copy; ' . date('Y') . ' ' . Yii::$app->params->site_name . ' ' . Yii::t('frontend', 'All Rights Reserved') . '.'; ?></p>
                             </div>
-                            <div class="col-md-6 col-sm-6 text-right">
-                                <div class="widget no-border m-0">
-                                    <ul class="list-inline  sm-text-center mt-5 font-12">
-                                        <li>
-                                            <a href="<?= Url::to('/about-us'); ?>" class="">Terms & Conditions</a>
-                                        </li>
-                                        |
-                                        <li>
-                                            <a href="<?= Url::to('/contact-us'); ?>" class="">Privacy Policies</a>
-                                        </li>    
-                                    </ul>
-                                </div>
-                            </div>
+<!--                            <div class="col-md-6 col-sm-6 text-right">-->
+<!--                                <div class="widget no-border m-0">-->
+<!--                                    <ul class="list-inline  sm-text-center mt-5 font-12">-->
+<!--                                        <li>-->
+<!--                                            <a href="--><?//= Url::to('/about-us'); ?><!--" class="">Terms & Conditions</a>-->
+<!--                                        </li>-->
+<!--                                        |-->
+<!--                                        <li>-->
+<!--                                            <a href="--><?//= Url::to('/contact-us'); ?><!--" class="">Privacy Policies</a>-->
+<!--                                        </li>    -->
+<!--                                    </ul>-->
+<!--                                </div>-->
+<!--                            </div>-->
                         </div>
                     </div>
                 </div>
