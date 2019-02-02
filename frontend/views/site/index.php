6607f935385895ec6737bd83e62ebcd0e87e473d
Ajay juneja <ajayjuneja@dsbedutech.in>
2019-02-02 14:06:30 +0530

    Widget created for Employers landing page floating widget.

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -386,43 +386,9 @@ $this->params['header_dark'] = false;
         </div>
     </article>
 </div>
-<div class="fixed-btn background-logo-blue">
-    <a href="/site/company-index"> <img
-                src="<?= Url::to('@eyAssets/images/flaticon-png/small/team-white.png'); ?>"/><br/>
-        <?= Yii::t('frontend', 'Are you an Employer?'); ?><br/>
-        <span><?= Yii::t('frontend', 'Want to post a Job or an Intenship?'); ?></span></a>
-</div>
-
 <?php
+echo $this->render('/widgets/employers-landing-page-floating-widget');
 $this->registerCss('
-.fixed-btn a{
-    position: fixed;
-    text-align: center;
-    width: 150px;
-    color: #fff !important;
-    bottom: 0px;
-    left:0px;
-    border-right: 4px solid orange;
-    z-index: 999999;
-    height: 112px;
-    opacity: 0.9;
-    padding: 10px 0px;
-    transition: ease-in-out .3s;
-    cursor: pointer;
-    bottom: -42px;
-    border-top-right-radius: 28px;
-}
-.fixed-btn a span{
-    font-weight: 700;
-    color: #fff;
-}
-.fixed-btn a:hover{
-    opacity: 1;
-    bottom: 0px;
-}
-.background-logo-blue a{
-    background-color: #49a1e3;
-}
 /*try now sec*/
 .fixed-bttn{
     padding:60px 0 100px;
