c95eb15a4591c57fbededea416437cb0d3d991a4
Nitesh <nitesh@dsbedutech.in>
2019-02-04 14:47:36 +0530

    1. worked on add plus button. functionallity if user click on add button then validation will given the toastr messege like already shortlisted, already in review list, removed successfully and added successfully,  on http://www.nitesh.eygb.me/jobs/list (candidate logged in).

--- frontend/views/widgets/sidebar-review.php
+++ frontend/views/widgets/sidebar-review.php
@@ -254,7 +254,17 @@ function Ajax_call(itemid) {
         }
     }).done(function(data) {
         check_list();
-        if (data == 'error') {
+        if(data.status == 200){
+            console.log(data.message,'test2');
+            toastr.success(data.message, 'Success');
+        } else if(data.status == 201) {
+            console.log(data.message,'test');
+            toastr.error(data.message, 'Error');
+        } else if (data.status == 'short') {
+            toastr.success(data.message, 'Reviewed Success');
+        } else if (data.status == 'unshort') {
+            toastr.success(data.message, 'Unreviewd Success');
+        } else if (data == 'error') {
             alert('Please Login first..');
         }
     });
@@ -318,6 +328,8 @@ JS;
 $this->registerJs($script);
 $this->registerJsFile('@eyAssets/js/jquery-ui.min.js', ['depends' => [\yii\bootstrap\BootstrapAsset::className()]]);
 $this->registerJsFile('https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.min.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
+$this->registerCssFile('@backendAssets/global/plugins/bootstrap-toastr/toastr.min.css');
+$this->registerJsFile('@backendAssets/global/plugins/bootstrap-toastr/toastr.min.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
 ?>
 
 <script id="review-cards" type="text/template">
