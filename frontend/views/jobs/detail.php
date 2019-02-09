8bfde1b553dd367e52aa8f61ec3bf40214922481
Ajay juneja <ajayjuneja@dsbedutech.in>
2019-02-09 17:13:11 +0530

    Recruiter spelling and Fixed all Taskbar issues.

--- frontend/views/jobs/detail.php
+++ frontend/views/jobs/detail.php
@@ -311,7 +311,7 @@ $logo_image = Yii::$app->params->upload_directories->organizations->logo . $org[
             <img src="https://i.ibb.co/TmV51CY/done.png">
             <h1 class="heading_submit">Submitted!</h1>
             <p class="sub_description_1">Your Application Has been successfully registerd with the recruiter. keep check
-                your Dashboard Regularly for further confirmation from the Requiter side.</p>
+                your Dashboard Regularly for further confirmation from the recruiter side.</p>
             <p class="sub_description_2">Your Application Has been successfully registerd But There Are Some
                 Questionnaire Pending From Your Side you can fill them now By clicking <a
                         href="<?= URL::to('/account/dashboard') ?>" target="_blank">Here</a> Or You can fill them Later.
