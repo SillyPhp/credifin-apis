cf1a02c8f189cc400e07b1f3b7a9b0fd96a0feff
Tarandeep Singh <er.tarandeep@yahoo.com>
2019-04-19 15:57:40 +0530

    Added team image path

--- frontend/models/OrganizationEmployeesForm.php
+++ frontend/models/OrganizationEmployeesForm.php
@@ -8,7 +8,8 @@ use common\models\Utilities;
 use common\models\Organizations;
 use common\models\OrganizationEmployees;
 
-class OrganizationEmployeesForm extends Model{
+class OrganizationEmployeesForm extends Model
+{
 
     public $first_name;
     public $last_name;
@@ -18,16 +19,18 @@ class OrganizationEmployeesForm extends Model{
     public $linkedin;
     public $designation;
 
-    public function rules() {
+    public function rules()
+    {
         return [
-            [['first_name','last_name','designation'], 'required'],
-            [['facebook','twitter','linkedin'], 'url', 'defaultScheme' => 'http'],
+            [['first_name', 'last_name', 'designation'], 'required'],
+            [['facebook', 'twitter', 'linkedin'], 'url', 'defaultScheme' => 'http'],
             [['image'], 'file', 'skipOnEmpty' => true, 'extensions' => 'png, jpg, jpeg', 'mimeTypes' => 'image/jpeg, image/png', 'maxSize' => 1024 * 1024 * 1],
-            [['first_name','last_name','facebook','twitter','linkedin'], 'trim'],
+            [['first_name', 'last_name', 'facebook', 'twitter', 'linkedin'], 'trim'],
         ];
     }
 
-    public function attributeLabels() {
+    public function attributeLabels()
+    {
         return [
             'first_name' => Yii::t('frontend', 'First Name'),
             'last_name' => Yii::t('frontend', 'Last Name'),
@@ -39,48 +42,49 @@ class OrganizationEmployeesForm extends Model{
     }
 
 
-    public function save() {
-        if ($this->validate()) {
-            $utilitiesModel = new Utilities();
-            $organizationsModel = new Organizations();
-            $organizationEmployeesModel = new OrganizationEmployees();
-            $organization = $organizationsModel->find()
-                ->where(['organization_enc_id' => Yii::$app->user->identity->organization->organization_enc_id, 'is_deleted' => 0])
-                ->one();
-            if ($organization) {
-                $utilitiesModel->variables['string'] = time() . rand(100, 100000);
-                $organizationEmployeesModel->employee_enc_id = $utilitiesModel->encrypt();
-                $organizationEmployeesModel->organization_enc_id = Yii::$app->user->identity->organization->organization_enc_id;
-                $organizationEmployeesModel->image_location = Yii::$app->getSecurity()->generateRandomString();
-                $base_path = $organizationEmployeesModel->image_location;
-                $organizationEmployeesModel->image = Yii::$app->getSecurity()->generateRandomString() . '.' . $this->image->extension;
-                $organizationEmployeesModel->first_name = $this->first_name;
-                $organizationEmployeesModel->last_name = $this->last_name;
-                $organizationEmployeesModel->designation = $this->designation;
-                $organizationEmployeesModel->facebook = $this->facebook;
-                $organizationEmployeesModel->twitter = $this->twitter;
-                $organizationEmployeesModel->linkedin = $this->linkedin;
-                $organizationEmployeesModel->created_by = Yii::$app->user->identity->user_enc_id;
-                if (!is_dir($base_path)) {
-                    if (mkdir($base_path, 0755, true)) {
-                        if ($this->image->saveAs($base_path . DIRECTORY_SEPARATOR . $organizationEmployeesModel->image)) {
-                            if ($organizationEmployeesModel->validate() && $organizationEmployeesModel->save()) {
-                                return true;
-                            } else {
-                                return false;
-                            }
-                        } else {
-                            return false;
-                        }
-                    } else {
-                        return false;
-                    }
-                }
-            } else {
+    public function save()
+    {
+        if (!$this->validate()) {
+            return false;
+        }
+        $utilitiesModel = new Utilities();
+        $organizationsModel = new Organizations();
+        $organizationEmployeesModel = new OrganizationEmployees();
+        $organization = $organizationsModel->find()
+            ->where(['organization_enc_id' => Yii::$app->user->identity->organization->organization_enc_id, 'is_deleted' => 0])
+            ->one();
+
+        if (!$organization) {
+            return false;
+        }
+        $utilitiesModel->variables['string'] = time() . rand(100, 100000);
+        $organizationEmployeesModel->employee_enc_id = $utilitiesModel->encrypt();
+        $organizationEmployeesModel->organization_enc_id = Yii::$app->user->identity->organization->organization_enc_id;
+        $organizationEmployeesModel->image_location = Yii::$app->getSecurity()->generateRandomString();
+        $base_path = Yii::$app->params->upload_directories->organizations->employees->image_path . $organizationEmployeesModel->image_location;
+        $organizationEmployeesModel->image = Yii::$app->getSecurity()->generateRandomString() . '.' . $this->image->extension;
+        $organizationEmployeesModel->first_name = $this->first_name;
+        $organizationEmployeesModel->last_name = $this->last_name;
+        $organizationEmployeesModel->designation = $this->designation;
+        $organizationEmployeesModel->facebook = $this->facebook;
+        $organizationEmployeesModel->twitter = $this->twitter;
+        $organizationEmployeesModel->linkedin = $this->linkedin;
+        $organizationEmployeesModel->created_by = Yii::$app->user->identity->user_enc_id;
+        if (!is_dir($base_path)) {
+            if (!mkdir($base_path, 0755, true)) {
                 return false;
             }
+        }
+
+        if (!$this->image->saveAs($base_path . DIRECTORY_SEPARATOR . $organizationEmployeesModel->image)) {
+            return false;
+        }
+
+        if ($organizationEmployeesModel->validate() && $organizationEmployeesModel->save()) {
+            return true;
         } else {
             return false;
         }
+
     }
 }
