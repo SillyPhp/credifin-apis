011177c7c8e910a2a4916c302ee1916d203443ed
Ravinder Singh <ravindersaini15697@gmail.com>
2019-05-01 12:27:26 +0530

    cards showing on map marker.

--- frontend/views/jobs/near-me-beta.php
+++ frontend/views/jobs/near-me-beta.php
@@ -1,6 +1,7 @@
 <?php
 $this->params['header_dark'] = true;
-?>
+
+use yii\web\JqueryAsset; ?>
 <div class="row">
     <div class="col-md-6 near-me-map pr-0">
         <div id="map"></div>
@@ -52,7 +53,8 @@ $this->params['header_dark'] = true;
 <script id="cards" type="text/template">
     {{#.}}
     <div id="card-hover" class="col-md-12 col-sm-12 col-xs-12 pt-5">
-        <div data-id="{{application_enc_id}}" data-key="{{application_enc_id}}-{{location_enc_id}}" class="application-card-main ui-draggable ui-draggable-handle">
+        <div data-id="{{application_enc_id}}" data-key="{{application_enc_id}}-{{location_enc_id}}"
+             class="application-card-main ui-draggable ui-draggable-handle">
             {{#city_name}}
             <span class="application-card-type location" data-lat="{{latitude}}" data-long="{{longitude}}"
                   data-locations="">
@@ -72,7 +74,7 @@ $this->params['header_dark'] = true;
                         <img src="{{logo}}" id="{{logo}}" class="company-logo">
                         {{/logo}}
                         {{^logo}}
-                        <canvas class="user-icon" name="{{name}}" width="80" height="80"
+                        <canvas class="user-icon company-logo" name="{{name}}" width="80" height="80"
                                 color="{{color}}" font="35px"></canvas>
                         {{/logo}}
                     </a>
@@ -118,7 +120,6 @@ $this->params['header_dark'] = true;
     </div>
     {{/.}}
 </script>
-
 
 
 <?php
@@ -258,11 +259,12 @@ $this->registerCss('
 ');
 $script = <<< JS
 
-var data = [$lat_long];
+
+var data = [$lat_long]
 var map;
 var marker;
 var  purple_icon = 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png';
-// var infowindow = null;
+var infowindow = new google.maps.InfoWindow();
 function showCards(lat,long){
     
     var i;
@@ -306,23 +308,12 @@ function showCards(lat,long){
                         var response = JSON.parse(res);
                         console.log(response);
                         $('#total-jobs').text("available Jobs("+response.total+")");
-                        // console.log(response[0].latitude);
-                        // console.log(response[0].longitude);
                         for(i=0;i<response[0].length;i++){
-                            // console.log(response[0][i].latitude,' , ',response[0][i].longitude);
-                            var contentString = '<p>'+response[0][i].name+'<\p>';
-                            // console.log(Number(response[i].latitude)," , ",Number(response[i].longitude));
                                 marker = new google.maps.Marker({
                                 position: {lat: Number(response[0][i].latitude), lng: Number(response[0][i].longitude)},
                                 map: map,
                                 draggable: false
-                            });
-                           
-                            infowindow = new google.maps.InfoWindow({
-                            content: contentString,
-                            maxWidth: 200
-                            });
-                            
+                            });                          
                             
                         }
                         var template = $('#cards').html();
@@ -338,10 +329,15 @@ function showCards(lat,long){
               });
               
               card();
+              
+              map.addListener('click', function(e) {
+                  infowindow.close();
+              });
     
 }
 
 $(document).on("mouseenter","#card-hover",function() {
+    
         if (infowindow) {
             infowindow.close();
         }
@@ -354,11 +350,16 @@ $(document).on("mouseenter","#card-hover",function() {
         last_date = $(this).find('.last-date').attr('id');
         exp = $(this).find('.exp').text();
         company =  $(this).find('.company-name').text();
-        logos = $(this).find('.company-logo').attr('id');
+        logo = $(this).find('.company-logo').attr('id');
+        logo_color = $(this).find('.company-logo').attr('color');
         slug = $(this).find('.application-card-open').attr('id');
         org_slug = $(this).find('#organization-slug').attr('class');
-         // var contentString = '<div style="width:400px;" class="product shadow iconbox-border iconbox-theme-colored"><span class="type tag-sale color-o pl-20 pr-20 ">'+types+'</span><div class="row"><div class="col-md-4 col-xs-4 pt-5" ><a href="#" class="icon set_logo"><img class="logo" src="'+logos+'"></a></div><div class="col-md-8 col-xs-8 pt-20"><h4 class="title icon-box-title"><strong>'+titles+'</strong></h4><h5><i class="location fa fa-map-marker" lat="'+lat+'" long="'+lon+'"> '+locations+'</i></h5><h5><i class="period fa fa-clock-o"> '+periods+'</i></h5></div><div class="btn-add-to-cart-wrapper"><a class="btn btn-theme-colored btn-sm btn-flat pl-20 pr-20 btn-add-to-cart text-uppercase font-weight-700 custom_color" href="/internships/detail">VIEW DETAILS</a><a style="background-color:#FF4500" class="btn btn-sm btn-flat pl-20 pr-20 btn-add-to-cart text-uppercase font-weight-700 custom_color2" href="#"><i class="fa fa-plus"></i></a></div></div><hr class="hr"><h6 class="pull-left pl-20 custom_set2" align="center"><strong>Last Date to Apply</strong><br><div class="lastDate">'+lastDates+'</div></h6><h4 class="company pull-right pr-10 pt-20 custom_set" align="center"><strong>'+companys+'</strong></h4></div>';    
-        var contentString = '<div class="col-md-12 col-sm-12 col-xs-12 p-0"><div class="application-card-main in-map"><span class="application-card-type location"><i class="fa fa-map-marker"></i>&nbsp;'+locations+'</span><div class="col-md-12 col-sm-12 col-xs-12 application-card-border-bottom"><div class="application-card-img"><a href="/'+org_slug+'"><img src="'+logos+'"></a></div><div class="application-card-description"><a href="/job/'+slug+'"><h4 class="application-title">'+titles+'</h4></a><h5><i class="fa fa-inr"></i>&nbsp;'+salary+'</h5><h5 class="type">'+types+'</h5><h5 class="exp"><i class="fa fa-clock-o"></i>&nbsp;'+exp+'</h5></div></div><h6 class="col-md-5 pl-20 custom_set2 text-center last-date">Last Date to Apply<br>'+last_date+'</h6><h4 class="col-md-7 org_name text-right pr-10 company-name">'+company+'</h4><div class="application-card-wrapper"><a href="/job/'+slug+'" class="application-card-open">View Detail</a><a href="#" class="application-card-add">&nbsp;<i class="fa fa-plus"></i>&nbsp;</a></div></div></div>';
+        if(!logo){
+           logo = '<canvas class="user-icon image-partners" name="'+company+'" color="'+logo_color+'" width="40" height="40" font="18px"></canvas>';
+        }else{
+            logo = '<img class="side-bar_logo" src="' + logo + '" height="40px">';
+        }
+        var contentString = '<div class="col-md-12 col-sm-12 col-xs-12 p-0"><div class="application-card-main in-map"><span class="application-card-type location"><i class="fa fa-map-marker"></i>&nbsp;'+locations+'</span><div class="col-md-12 col-sm-12 col-xs-12 application-card-border-bottom"><div class="application-card-img"><a href="/'+org_slug+'">'+logo+'</a></div><div class="application-card-description"><a href="/job/'+slug+'"><h4 class="application-title">'+titles+'</h4></a><h5><i class="fa fa-inr"></i>&nbsp;'+salary+'</h5><h5 class="type">'+types+'</h5><h5 class="exp"><i class="fa fa-clock-o"></i>&nbsp;'+exp+'</h5></div></div><h6 class="col-md-5 pl-20 custom_set2 text-center last-date">Last Date to Apply<br>'+last_date+'</h6><h4 class="col-md-7 org_name text-right pr-10 company-name">'+company+'</h4><div class="application-card-wrapper"><a href="/job/'+slug+'" class="application-card-open">View Detail</a><a href="#" class="application-card-add">&nbsp;<i class="fa fa-plus"></i>&nbsp;</a></div></div></div>';
          infowindow = new google.maps.InfoWindow({
           content: contentString
         });
@@ -366,23 +367,16 @@ $(document).on("mouseenter","#card-hover",function() {
             position: new google.maps.LatLng(Number(lat),Number(lon)),
             map: map,
             // icon: purple_icon,
-            draggable: true
+            draggable: false
         });
-         // var position = new google.maps.LatLng(Number(lat),Number(lon));
-         // map.setCenter(position);
-         // map.setZoom(16);
           infowindow.open(map, marker);
     });
-        
-
 
 function initMap(){
         
     if(navigator.geolocation){
             navigator.geolocation.getCurrentPosition(successCallback,showError);
-            // console.log('Getting the position information....');
         }else{
-            console.log('Sorry, your browser does not support HTML5 geolocation.');
         }
         
         function successCallback(position){
@@ -396,15 +390,6 @@ function initMap(){
           switch(error.code) {
             case error.PERMISSION_DENIED:
               $('#myModal').modal();
-              break;
-            case error.POSITION_UNAVAILABLE:
-              x.innerHTML = "Location information is unavailable.";
-              break;
-            case error.TIMEOUT:
-              x.innerHTML = "The request to get user location timed out.";
-              break;
-            case error.UNKNOWN_ERROR:
-              x.innerHTML = "An unknown error occurred.";
               break;
           }
         }
@@ -421,11 +406,8 @@ function geocodeAddress(geocoder) {
         var address = document.getElementById('address').value;
         geocoder.geocode({'address': address}, function(results, status) {
           if (status === 'OK') {
-            // console.log(results[0].geometry.location.lat());
-            // console.log(results[0].geometry.location.lng());
             var lat = results[0].geometry.location.lat();
             var long = results[0].geometry.location.lng();
-            // resultsMap.setCenter(results[0].geometry.location);
             showCards(lat,long);
           } else {
             console.log('Geocode was not successful for the following reason: ' + status);
@@ -440,7 +422,7 @@ $this->registerJs($script);
 $this->registerCssFile('@backendAssets/global/css/components-md.min.css');
 $this->registerCssFile('@eyAssets/css/perfect-scrollbar.css');
 $this->registerJsFile('@backendAssets/global/scripts/app.min.js');
-$this->registerJsFile('@eyAssets/js/perfect-scrollbar.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
-$this->registerJsFile('https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.min.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
+$this->registerJsFile('@eyAssets/js/perfect-scrollbar.js', ['depends' => [JqueryAsset::className()]]);
+$this->registerJsFile('https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.min.js', ['depends' => [JqueryAsset::className()]]);
 ?>
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDYtKKbGvXpQ4xcx4AQcwNVN6w_zfzSg8c"></script>
