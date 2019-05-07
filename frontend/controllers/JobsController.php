44ab134a57ab42fd893c2cdb3b09636b72ea0d17
Ravinder Saini <ravindersaini15697@gmail.com>
2019-05-07 15:31:27 +0530

    Testing

--- frontend/controllers/JobsController.php
+++ frontend/controllers/JobsController.php
@@ -52,6 +52,7 @@ class JobsController extends Controller
 
     public function actionIndex()
     {
+
         if (Yii::$app->request->isAjax && Yii::$app->request->isPost) {
             Yii::$app->response->format = Response::FORMAT_JSON;
             $type = Yii::$app->request->post('type');
