358d933c7d1092c4283442bb2a2f3f924d63f8be
Tarandeep Singh <er.tarandeep@yahoo.com>
2019-05-14 13:21:45 +0530

    Fixed featured image issue in related blogs seciton

--- frontend/views/blog/detail.php
+++ frontend/views/blog/detail.php
@@ -124,7 +124,7 @@ $this->params['seo_tags'] = [
                         foreach ($similar_posts as $related) {
                             $path = Yii::$app->params->upload_directories->posts->featured_image . $related['featured_image_location'];
                             $image = $path . DIRECTORY_SEPARATOR . $related['featured_image'];
-                            if (!empty($related['featured_image'])) {
+                            if (empty($related['featured_image'])) {
                                 $image = '//placehold.it/250x200';
                             }
                             ?>
