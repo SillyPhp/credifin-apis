bf7d3df12b8d9147a8591d058cb51f24c412ae55
Ajay juneja <ajayjuneja@dsbedutech.in>
2019-05-27 12:49:35 +0530

    Mailing templates responsive in gmail.

--- common/mail/verification-email.php
+++ common/mail/verification-email.php
@@ -2,16 +2,16 @@
 
 use yii\helpers\Html;
 use yii\helpers\Url;
-?>
 
-<style type="text/css" media="screen">
+$this->registerCss('
   * {
     margin:0; 
     padding:0;
   }
   body {
     font-family:open sans, arial; 
-    font-size:13px; color:#4d4d4d;
+    font-size:13px;
+    color:#4d4d4d;
     font-family:Times New Roman, "Hoefler Text", "Liberation Serif", Times, "Times New Roman", serif;
   }
   img + div{
@@ -29,7 +29,7 @@ use yii\helpers\Url;
   .inner-wrapper{ 
     background:#fff; 
     border-radius:10px; 
-    text-align:center; 
+    text-align:center;
     box-shadow: 0px 0px 10px rgba(68, 67, 67, 0.4);
   }
   .logo img{
@@ -44,11 +44,13 @@ use yii\helpers\Url;
     padding:10px 0 0 0; 
     font-weight:bold; 
     font-size:28px;
+    color:#4d4d4d;
   }
   .welcome-text{
     padding:20px 80px 0 80px; 
     font-size:24px; 
     line-height:30px;
+    color:#4d4d4d;
   }
   .activate-button{
     padding:45px 0 20px 0;
@@ -70,6 +72,7 @@ use yii\helpers\Url;
   .copyright{
     padding:10px 0 0 0; 
     font-size:15px;
+    color:#4d4d4d;
   }
   .last-list{
     padding:5px 0 10px 0; 
@@ -83,8 +86,8 @@ use yii\helpers\Url;
     color:#00a0e3; 
     text-decoration:none;
   }
-</style>
-<style media="only screen and (max-device-width: 500px), only screen and (max-width: 380px)" type="text/css">
+', ['media' => 'screen']);
+$this->registerCss('
 @media only screen and (max-width: 500px){
     .welcome-text{
         padding:20px 30px 0 30px;
@@ -93,6 +96,7 @@ use yii\helpers\Url;
 @media only screen and (max-width: 380px){
     .wrapper { 
         padding:30px 10px; 
+        font-size:20px;
     } 
     .activate-button a{ 
         padding:15px 20px; 
@@ -103,8 +107,8 @@ use yii\helpers\Url;
         margin-bottom:20px;
     }
 }
-  </style>
-
+', ['media' => 'only screen and (max-device-width: 500px), only screen and (max-width: 380px)']);
+?>
 <div class="wrapper">
     <div class="inner-wrapper">
         <div class="logo">
@@ -124,8 +128,8 @@ use yii\helpers\Url;
             <?= Html::a(Yii::t('app', 'Activate Account'), $data['link']); ?>
         </div>
         <div class="link-text">
-            <p style="padding: 8px 60px;font-size: 16px;">If you’re having trouble clicking the button, copy and paste the URL below into your web browser.</p>
-            <?= Html::a(Yii::t('app', $data['link']), $data['link'],['style' => 'font-size: 17px;text-decoration: none;word-break: break-word;']); ?>
+            <p style="padding: 8px 60px;font-size: 16px;color:#4d4d4d;">If you’re having trouble clicking the button, copy and paste the URL below into your web browser.</p>
+            <?= Html::a(Yii::t('app', $data['link']), $data['link'],['style' => 'padding: 2px 10px;font-size: 17px;text-decoration: none;word-break: break-word;']); ?>
         </div>
         <div class="ey-team">
             <img src="<?= Url::to('@commonAssets/email_service/email-eyteam.png', true); ?>">
