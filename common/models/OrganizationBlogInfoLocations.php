8ccfd7e84ca2c4ebafbf7355a65d8d6533c0dbaf
Sneh Kant <sneh@dsbedutech.in>
2019-05-29 12:53:46 +0530

    auto blog genrater

--- common/models/OrganizationBlogInfoLocations.php
+++ common/models/OrganizationBlogInfoLocations.php
@@ -5,27 +5,27 @@ namespace common\models;
 use Yii;
 
 /**
- * This is the model class for table "{{%organization_blog_information_images}}".
+ * This is the model class for table "{{%organization_blog_info_locations}}".
  *
  * @property int $id
- * @property string $image_enc_id
+ * @property string $blog_information_location_enc_id
  * @property string $blog_information_enc_id
- * @property string $image
- * @property string $image_location
+ * @property string $city_enc_id
  * @property string $created_on
  * @property string $created_by
  *
  * @property OrganizationBlogInformation $blogInformationEnc
  * @property Users $createdBy
+ * @property Cities $cityEnc
  */
-class OrganizationBlogInformationImages extends \yii\db\ActiveRecord
+class OrganizationBlogInfoLocations extends \yii\db\ActiveRecord
 {
     /**
      * @inheritdoc
      */
     public static function tableName()
     {
-        return '{{%organization_blog_information_images}}';
+        return '{{%organization_blog_info_locations}}';
     }
 
     /**
@@ -34,12 +34,13 @@ class OrganizationBlogInformationImages extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['image_enc_id', 'blog_information_enc_id', 'image', 'image_location'], 'required'],
+            [['blog_information_location_enc_id', 'blog_information_enc_id', 'city_enc_id'], 'required'],
             [['created_on'], 'safe'],
-            [['image_enc_id', 'blog_information_enc_id', 'image', 'image_location', 'created_by'], 'string', 'max' => 100],
-            [['image_enc_id'], 'unique'],
+            [['blog_information_location_enc_id', 'blog_information_enc_id', 'city_enc_id', 'created_by'], 'string', 'max' => 100],
+            [['blog_information_location_enc_id'], 'unique'],
             [['blog_information_enc_id'], 'exist', 'skipOnError' => true, 'targetClass' => OrganizationBlogInformation::className(), 'targetAttribute' => ['blog_information_enc_id' => 'blog_information_enc_id']],
             [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => Users::className(), 'targetAttribute' => ['created_by' => 'user_enc_id']],
+            [['city_enc_id'], 'exist', 'skipOnError' => true, 'targetClass' => Cities::className(), 'targetAttribute' => ['city_enc_id' => 'city_enc_id']],
         ];
     }
 
@@ -61,5 +62,13 @@ class OrganizationBlogInformationImages extends \yii\db\ActiveRecord
     public function getCreatedBy()
     {
         return $this->hasOne(Users::className(), ['user_enc_id' => 'created_by']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCityEnc()
+    {
+        return $this->hasOne(Cities::className(), ['city_enc_id' => 'city_enc_id']);
     }
 }
--- common/models/OrganizationBlogInformationApplications.php
+++ common/models/OrganizationBlogInformationApplications.php
@@ -6,7 +6,7 @@ namespace common\models;
  * This is the model class for table "{{%organization_blog_information_applications}}".
  *
  * @property int $id
- * @property string $information_application_enc_id
+ * @property string $blog_information_applications_enc_id
  * @property string $application_enc_id
  * @property string $blog_information_enc_id
  * @property string $created_on
@@ -32,10 +32,10 @@ class OrganizationBlogInformationApplications extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['information_application_enc_id', 'application_enc_id', 'blog_information_enc_id', 'created_by'], 'required'],
+            [['blog_information_applications_enc_id', 'application_enc_id', 'blog_information_enc_id'], 'required'],
             [['created_on'], 'safe'],
-            [['information_application_enc_id', 'application_enc_id', 'blog_information_enc_id', 'created_by'], 'string', 'max' => 100],
-            [['information_application_enc_id'], 'unique'],
+            [['blog_information_applications_enc_id', 'application_enc_id', 'blog_information_enc_id', 'created_by'], 'string', 'max' => 100],
+            [['blog_information_applications_enc_id'], 'unique'],
             [['blog_information_enc_id'], 'exist', 'skipOnError' => true, 'targetClass' => OrganizationBlogInformation::className(), 'targetAttribute' => ['blog_information_enc_id' => 'blog_information_enc_id']],
             [['application_enc_id'], 'exist', 'skipOnError' => true, 'targetClass' => EmployerApplications::className(), 'targetAttribute' => ['application_enc_id' => 'application_enc_id']],
             [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => Users::className(), 'targetAttribute' => ['created_by' => 'user_enc_id']],
