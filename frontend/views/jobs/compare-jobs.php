dc72b4923de8be0d983a3722e8067dcafcb445d9
Aditya Bansal <eadityabansal@gmail.com>
2019-06-14 12:58:12 +0530

    Testing

--- frontend/views/jobs/compare-jobs.php
+++ frontend/views/jobs/compare-jobs.php
@@ -906,8 +906,8 @@ $script = <<<JS
                 }
                 
                 if(!dropped.includes(data['message']['application_enc_id'])){
-                    $('[data-id='+data['message']['application_enc_id']+']').draggable({disabled:true});
-                    $('[data-id='+data['message']['application_enc_id']+']').addClass('b-li-card');
+                    $('[data-id="'+data['message']['application_enc_id']+'"]').draggable({disabled:true});
+                    $('[data-id="'+data['message']['application_enc_id']+'"]').addClass('b-li-card');
                     dropped.push(data['message']['application_enc_id']);
                     console.log(data['message']['application_enc_id']);
                     console.log(dropped);
