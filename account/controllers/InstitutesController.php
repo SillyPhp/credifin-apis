51145fd2d249b14d8b05f0f406f8e31ac6bf12c3
Ajay juneja <ajayjuneja@dsbedutech.in>
2019-07-22 16:13:57 +0530

    Manage candidate.

--- account/controllers/InstitutesController.php
+++ account/controllers/InstitutesController.php
@@ -4,7 +4,6 @@ namespace account\controllers;
 
 use common\models\ReferralJobAppliedTracking;
 use Yii;
-use common\models\Users;
 use yii\web\Controller;
 
 class InstitutesController extends Controller
@@ -30,19 +29,14 @@ class InstitutesController extends Controller
                     $d->select(['e.application_enc_id','e.organization_enc_id','e.slug as application_slug','i.name as application_title','h.assigned_to']);
                 }]);
                 $c->joinWith(['createdBy g'], false);
-//                $c->groupBy(['g.user_enc_id', 'c.applied_application_enc_id']);
                 $c->select(['c.applied_application_enc_id','c.application_enc_id','c.current_round','c.status','c.created_by',' CONCAT(g.first_name, " ", g.last_name) name','g.image','g.image_location','g.initials_color','g.username']);
                 $c->onCondition(['c.is_deleted' => 0]);
             }])
             ->distinct()
             ->where(['b.code' => Yii::$app->user->identity->referral->code, 'a.is_deleted' => 0])
-//            ->groupBy(['g.user_enc_id', 'c.applied_application_enc_id'])
             ->asArray()
             ->limit(20)
             ->all();
-
-//        print_r($data);
-//        exit();
 
         return $this->render('manage-candidates',[
             'users' => $data,
