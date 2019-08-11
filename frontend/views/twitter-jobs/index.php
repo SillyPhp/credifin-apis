443213f61f80f9b75b2a082a02ff007e90a07e78
Sneh Kant <sneh@dsbedutech.in>
2019-08-12 00:02:05 +0530

    epic jobs

--- frontend/views/twitter-jobs/index.php
+++ frontend/views/twitter-jobs/index.php
@@ -2,14 +2,19 @@
 $this->params['grid_size'] = 'col-md-12';
 ?>
 <div class="row">
-    <div class="col-md-6">
-        <blockquote class="twitter-tweet"><p lang="en" dir="ltr">Jobs of the week in Ireland; <br><br>🔷Senior C# .Net Software Developer - Limerick <br>🔷Senior Data Analyst - Dublin <br>🔷Senior Software Developer - Dublin <br>🔷Senior Technical Programme/Project Manager - Dublin <br>🔷PHP Engineer - Waterford <br>🔷Junior C# .Net Developer - Dublin<a href="https://twitter.com/hashtag/jobsearch?src=hash&amp;ref_src=twsrc%5Etfw">#jobsearch</a> <a href="https://t.co/JHXMaoj8XK">pic.twitter.com/JHXMaoj8XK</a></p>&mdash; Reperio HC (@reperiorecruit) <a href="https://twitter.com/reperiorecruit/status/1159788499467493376?ref_src=twsrc%5Etfw">August 9, 2019</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <div class="col-md-4">
+        <blockquote class="twitter-tweet" data-width="320"><p lang="en" dir="ltr">Jobs of the week in Ireland; <br><br>🔷Senior C# .Net Software Developer - Limerick <br>🔷Senior Data Analyst - Dublin <br>🔷Senior Software Developer - Dublin <br>🔷Senior Technical Programme/Project Manager - Dublin <br>🔷PHP Engineer - Waterford <br>🔷Junior C# .Net Developer - Dublin<a href="https://twitter.com/hashtag/jobsearch?src=hash&amp;ref_src=twsrc%5Etfw">#jobsearch</a> <a href="https://t.co/JHXMaoj8XK">pic.twitter.com/JHXMaoj8XK</a></p>&mdash; Reperio HC (@reperiorecruit) <a href="https://twitter.com/reperiorecruit/status/1159788499467493376?ref_src=twsrc%5Etfw">August 9, 2019</a></blockquote>
     </div>
-    <div class="col-md-6">
-        <blockquote class="twitter-tweet"><p lang="en" dir="ltr">We are hiring, Join our team! Job Apply on hr@iihglobal.com<br><br>- Business Development Executive (1-3/Years)<br>- PHP Developer (1-3/Years)<br><br>Job Location: <a href="https://twitter.com/hashtag/Ahmedabad?src=hash&amp;ref_src=twsrc%5Etfw">#Ahmedabad</a> <a href="https://t.co/ad4YiVTsfL">pic.twitter.com/ad4YiVTsfL</a></p>&mdash; IIH Global (@IIH_Global) <a href="https://twitter.com/IIH_Global/status/1159702212341145600?ref_src=twsrc%5Etfw">August 9, 2019</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <div class="col-md-4">
+        <blockquote class="twitter-tweet" data-width="320"><p lang="en" dir="ltr">Jobs of the week in Ireland; <br><br>🔷Senior C# .Net Software Developer - Limerick <br>🔷Senior Data Analyst - Dublin <br>🔷Senior Software Developer - Dublin <br>🔷Senior Technical Programme/Project Manager - Dublin <br>🔷PHP Engineer - Waterford <br>🔷Junior C# .Net Developer - Dublin<a href="https://twitter.com/hashtag/jobsearch?src=hash&amp;ref_src=twsrc%5Etfw">#jobsearch</a> <a href="https://t.co/JHXMaoj8XK">pic.twitter.com/JHXMaoj8XK</a></p>&mdash; Reperio HC (@reperiorecruit) <a href="https://twitter.com/reperiorecruit/status/1159788499467493376?ref_src=twsrc%5Etfw">August 9, 2019</a></blockquote>
+    </div>
+    <div class="col-md-4">
+        <blockquote class="twitter-tweet" data-width="320"><p lang="en" dir="ltr">Jobs of the week in Ireland; <br><br>🔷Senior C# .Net Software Developer - Limerick <br>🔷Senior Data Analyst - Dublin <br>🔷Senior Software Developer - Dublin <br>🔷Senior Technical Programme/Project Manager - Dublin <br>🔷PHP Engineer - Waterford <br>🔷Junior C# .Net Developer - Dublin<a href="https://twitter.com/hashtag/jobsearch?src=hash&amp;ref_src=twsrc%5Etfw">#jobsearch</a> <a href="https://t.co/JHXMaoj8XK">pic.twitter.com/JHXMaoj8XK</a></p>&mdash; Reperio HC (@reperiorecruit) <a href="https://twitter.com/reperiorecruit/status/1159788499467493376?ref_src=twsrc%5Etfw">August 9, 2019</a></blockquote>
     </div>
 </div>
 <?php
 $script = <<< JS
 JS;
+$this->registerJs($script);
+$this->registerJsFile("https://platform.twitter.com/widgets.js");
 ?>
