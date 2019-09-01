52796319564fa3479876905f27f5a380336b5a37
Ravinder Singh <ravindersaini15697@gmail.com>
2019-09-01 12:45:11 +0530

    minor change in query

--- api/modules/v2/controllers/CandhomeController.php
+++ api/modules/v2/controllers/CandhomeController.php
@@ -9,9 +9,14 @@ use common\models\ShortlistedApplications;
 use Yii;
 use yii\helpers\Url;
 
-class CandhomeController extends ApiBaseController{
+class CandhomeController extends ApiBaseController
+{
 
-    public function actionCounts($id){
+    public function actionCounts()
+    {
+
+        $id = Yii::$app->request->post('id');
+
         $applied_count = AppliedApplications::find()
             ->alias('a')
             ->select(['COUNT(a.applied_application_enc_id) applied_count'])
@@ -48,10 +53,11 @@ class CandhomeController extends ApiBaseController{
         return $this->response(200, $counts);
     }
 
-    public function actionGetCompanies(){
+    public function actionGetCompanies()
+    {
         $q = Organizations::find()
             ->alias('a')
-            ->select(['a.organization_enc_id', 'a.name', 'CONCAT("'. Url::to('/', true). '", a.slug) profile_link', 'd.business_activity', 'CASE WHEN a.logo IS NOT NULL THEN CONCAT("' . Url::to(Yii::$app->params->upload_directories->organizations->logo, true) . '", a.logo_location, "/", a.logo) ELSE CONCAT("https://ui-avatars.com/api/?name=", a.name, "&size=200&rounded=false&background=", REPLACE(a.initials_color, "#", ""), "&color=ffffff") END logo'])
+            ->select(['a.organization_enc_id', 'a.name', 'CONCAT("' . Url::to('/', true) . '", a.slug) profile_link', 'd.business_activity', 'CASE WHEN a.logo IS NOT NULL THEN CONCAT("' . Url::to(Yii::$app->params->upload_directories->organizations->logo, true) . '", a.logo_location, "/", a.logo) ELSE CONCAT("https://ui-avatars.com/api/?name=", a.name, "&size=200&rounded=false&background=", REPLACE(a.initials_color, "#", ""), "&color=ffffff") END logo'])
             ->distinct()
             ->innerJoinWith(['employerApplications b' => function ($x) {
                 $x
@@ -81,18 +87,23 @@ class CandhomeController extends ApiBaseController{
         return $this->response(200, $q);
     }
 
-    public function actionAppliedApplications($id){
+    public function actionAppliedApplications()
+    {
+
+        $id = Yii::$app->request->post('id');
+
         $q = AppliedApplications::find()
             ->alias('a')
             ->select(['a.application_enc_id', 'a.current_round'])
-            ->joinWith(['applicationEnc b' => function($x){
+            ->joinWith(['applicationEnc b' => function ($x) {
                 $x->joinWith(['organizationEnc d'], false);
-                $x->joinWith(['title h' => function($y){
+                $x->joinWith(['title h' => function ($y) {
                     $y->joinWith(['parentEnc i']);
+                    $y->joinWith(['categoryEnc j']);
                 }], false);
-                $x->joinWith(['applicationPlacementLocations e' => function($y) use($x){
-                    $x->select(['b.application_enc_id', 'b.title', 'i.category_enc_id', 'i.name parent_name', 'b.organization_enc_id', 'd.name organization_name', 'CONCAT("'. Url::to('/', true). '", d.slug) profile_link', 'CASE WHEN d.logo IS NOT NULL THEN CONCAT("' . Url::to(Yii::$app->params->upload_directories->organizations->logo, true) . '", d.logo_location, "/", d.logo) ELSE CONCAT("https://ui-avatars.com/api/?name=", d.name, "&size=200&rounded=false&background=", REPLACE(d.initials_color, "#", ""), "&color=ffffff") END logo', 'e.placement_location_enc_id', 'COUNT(e.placement_location_enc_id) cnt']);
-                    $y->joinWith(['locationEnc f' => function($z){
+                $x->joinWith(['applicationPlacementLocations e' => function ($y) use ($x) {
+                    $x->select(['b.application_enc_id', 'b.title', 'i.category_enc_id', 'g.name city', 'j.name profile', 'i.name parent_name', 'b.organization_enc_id', 'd.name organization_name', 'CONCAT("' . Url::to('/', true) . '", d.slug) profile_link', 'CASE WHEN d.logo IS NOT NULL THEN CONCAT("' . Url::to(Yii::$app->params->upload_directories->organizations->logo, true) . '", d.logo_location, "/", d.logo) ELSE CONCAT("https://ui-avatars.com/api/?name=", d.name, "&size=200&rounded=false&background=", REPLACE(d.initials_color, "#", ""), "&color=ffffff") END logo', 'e.placement_location_enc_id', 'COUNT(e.placement_location_enc_id) cnt']);
+                    $y->joinWith(['locationEnc f' => function ($z) {
                         $z->joinWith(['cityEnc g']);
                     }], false);
                     $x->groupBy(['e.placement_location_enc_id']);
@@ -106,20 +117,24 @@ class CandhomeController extends ApiBaseController{
             ->andWhere([
                 '<>', 'a.status', 'Cancelled'
             ])
+            ->limit(6)
             ->asArray()
             ->all();
-        return $q;
-//        return $this->response(200, $q);
+//        return $q;
+        return $this->response(200, $q);
     }
 
-    public function actionFollowedCompanies($id){
+    public function actionFollowedCompanies()
+    {
+        $id = Yii::$app->request->post('id');
+
         $f = FollowedOrganizations::find()
             ->alias('a')
-            ->select(['a.followed_enc_id','a.organization_enc_id'])
-            ->joinWith(['organizationEnc b' => function($x){
+            ->select(['a.followed_enc_id', 'a.organization_enc_id'])
+            ->innerJoinWith(['organizationEnc b' => function ($x) {
                 $x->joinWith(['businessActivityEnc e'], false);
-                $x->joinWith(['employerApplications c' => function($y) use($x){
-                    $x->select(['b.organization_enc_id', 'b.name organization_name', 'CONCAT("'. Url::to('/', true). '", b.slug) profile_link', 'e.business_activity', 'CASE WHEN b.logo IS NOT NULL THEN CONCAT("' . Url::to(Yii::$app->params->upload_directories->organizations->logo, true) . '", b.logo_location, "/", b.logo) ELSE CONCAT("https://ui-avatars.com/api/?name=", b.name, "&size=200&rounded=false&background=", REPLACE(b.initials_color, "#", ""), "&color=ffffff") END logo', 'c.application_enc_id','COUNT(c.application_enc_id) application_type', 'd.name']);
+                $x->joinWith(['employerApplications c' => function ($y) use ($x) {
+                    $x->select(['b.organization_enc_id', 'b.name organization_name', 'CONCAT("' . Url::to('/', true) . '", b.slug) profile_link', 'e.business_activity', 'CASE WHEN b.logo IS NOT NULL THEN CONCAT("' . Url::to(Yii::$app->params->upload_directories->organizations->logo, true) . '", b.logo_location, "/", b.logo) ELSE CONCAT("https://ui-avatars.com/api/?name=", b.name, "&size=200&rounded=false&background=", REPLACE(b.initials_color, "#", ""), "&color=ffffff") END logo', 'c.application_enc_id', 'COUNT(c.application_enc_id) application_type', 'd.name']);
                     $y
                         ->joinWith(['applicationTypeEnc d'], false)
                         ->onCondition([
