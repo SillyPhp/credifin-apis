04c8d1d48bbdce1738383abc96fa3d66d16955f7
Aditya Bansal <eadityabansal@gmail.com>
2019-09-10 18:30:11 +0530

    Applying authenticaation

---
+++ api/modules/v2/controllers/TestController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace api\modules\v2\controllers;
+
+use api\modules\v1\models\Candidates;
+use common\models\UserAccessTokens;
+use Yii;
+use yii\web\Response;
+use yii\filters\Cors;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use yii\filters\ContentNegotiator;
+use yii\filters\auth\HttpBearerAuth;
+
+class TestController extends ApiBaseController
+{
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        $behaviors['verbs'] = [
+            'class' => VerbFilter::className(),
+            'actions' => [
+                'demo' => ['OPTIONS', 'POST'],
+            ]
+        ];
+
+        $behaviors['corsFilter'] = [
+            'class' => Cors::className(),
+            'cors' => [
+                'Origin' => ['http://127.0.0.1:5500'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Max-Age' => 86400,
+                'Access-Control-Expose-Headers' => [],
+            ],
+        ];
+        return $behaviors;
+    }
+
+    public function actionDemo()
+    {
+        if($this->isAuthorized()){
+            return 1;
+        }else{
+            return 2;
+        }
+    }
+}
