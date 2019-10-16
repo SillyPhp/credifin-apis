88e9c10a1290225ef88f47da03f164d5b51f8350
Ajay juneja <ajayjuneja@dsbedutech.in>
2019-10-16 11:22:08 +0530

    Bug Fixed.

--- frontend/views/widgets/employer_applications/top-banner.php
+++ frontend/views/widgets/employer_applications/top-banner.php
@@ -1,5 +1,7 @@
 <?php
+
 use yii\helpers\Url;
+
 ?>
     <section class="overlape dark-color">
         <div data-velocity="-.1"
@@ -13,25 +15,25 @@ use yii\helpers\Url;
                             <img src="/assets/common/categories/profile/<?= $icon_png; ?>"/>
                         </div>
                         <h3><?= $job_title; ?></h3>
-                    <?php if ($shortlist_btn_display): ?>
                         <div class="job-statistic">
-                            <?php
-                            if (!Yii::$app->user->isGuest && !Yii::$app->user->identity->organization) {
-                                if (!empty($shortlist) && $shortlist['shortlisted'] == 1) {
-                                    ?>
-                                    <span class="hover-change col_pink"><a href="#" class="shortlist_job"><i
-                                                    class="far fa-heart"></i> Shortlisted</a></span>
-                                    <?php
-                                } else {
-                                    ?>
-                                    <span class="hover-change"><a href="#" class="shortlist_job"><i
-                                                    class="far fa-heart"></i> Shortlist</a></span>
-                                    <?php
+                            <?php if ($shortlist_btn_display): ?>
+                                <?php
+                                if (!Yii::$app->user->isGuest && !Yii::$app->user->identity->organization) {
+                                    if (!empty($shortlist) && $shortlist['shortlisted'] == 1) {
+                                        ?>
+                                        <span class="hover-change col_pink"><a href="#" class="shortlist_job"><i
+                                                        class="far fa-heart"></i> Shortlisted</a></span>
+                                        <?php
+                                    } else {
+                                        ?>
+                                        <span class="hover-change"><a href="#" class="shortlist_job"><i
+                                                        class="far fa-heart"></i> Shortlist</a></span>
+                                        <?php
+                                    }
                                 }
-                            }
-                            ?>
+                                ?>
+                            <?php endif; ?>
                         </div>
-            <?php endif; ?>
                     </div>
                 </div>
             </div>
