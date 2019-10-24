903609e46a8f11c7c7441833821fe8c90eda5153
Ajay Juneja <ajayjuneja52@gmail.com>
2019-10-24 18:16:42 +0530

    trainings.

--- account/models/training_program/InviteCandidatesForm.php
+++ account/models/training_program/InviteCandidatesForm.php
@@ -26,8 +26,20 @@ class InviteCandidatesForm extends Model
 
     public function send()
     {
-//        $mail = Yii::$app->mail;
-//        $mail->
+        $all_mails = [];
+        for($i=0;$i<count($this->email);$i++){
+            $mail = [];
+            if(!empty($this->email[$i])) {
+                $mail['email'] = $this->email[$i];
+                $mail['name'] = $this->name[$i];
+                array_push($all_mails, $mail);
+            }
+        }
+        $mail = Yii::$app->mail;
+        $mail->receivers = [];
+        $mail->receivers = $all_mails;
+        $mail->subject = 'Educational Institute has invited you to join on Empower Youth';
+        $mail->template = 'invitation-email';
     }
 
 }
