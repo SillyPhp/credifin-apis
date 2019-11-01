fcb8cb08a214494e4fda9e10f8e304b3143b2e13
Shshank <vasishtshshank@gmail.com>
2019-11-01 14:08:33 +0530

    featured jobs widget

--- frontend/views/widgets/featured-jobs.php
+++ frontend/views/widgets/featured-jobs.php
@@ -3,7 +3,9 @@ use yii\helpers\Url;
 ?>
  <div class="row">
         <div class="col-md-12">
-            <div class="widget-heading">Developer Jobs</div>
+            <div class="widget-heading">Developer Jobs
+                <span></span>
+            </div>
         </div>
     </div>
     <div class="row">
@@ -13,10 +15,68 @@ use yii\helpers\Url;
             ])
         ?>
     </div>
+    <div class="fj-form">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="fj-sub-heading">Get Latest Updates in you inbox</div>
+            </div>
+            <div class="col-md-6">
+                <div class="fj-sub-form">
+                    <form id="subs_news">
+                        <div class="row">
+                            <div class="col-md-9">
+                                <input type="text" class="fj-input" placeholder="Your Email">
+                            </div>
+                            <div class="col-md-3">
+                                <button type="submit" class="fj-btn">Notify Me</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php
 $this->registerCss('
 .widget-heading{
     text-align:center;
 }
-')
+.fj-sub-heading{
+    font-size: 18px;
+    text-transform: capitalize;
+    font-family: roboto;
+    padding:8px 10px;    
+}
+
+.fj-form {
+    border:1px solid #eee;
+    padding:20px 10px 15px;
+    margin:20px 0;
+    box-shadow:0 0 10px rgba(0,0,0,.2)
+}
+.fj-input{
+    width:100%;
+    padding:10px;
+    border:1px solid #eee;
+    font-size:15px;
+}
+.fj-input::placeholder{
+    color:#999;
+}
+.fj-btn{
+    color:#fff;
+    background:#00a0e3;
+    padding:10px 10px;
+    border:none;
+}
+');
+$script = <<<JS
+$('#subs_news').submit(function(event) {
+    event.preventDefault();
+  $.ajax({
+    url: ''
+  })
+})
+JS;
+$this->registerJS($script);
 ?>
