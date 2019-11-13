fac60f8787b64acf58c2cc0be957fa3369ff9ee8
Ajay Juneja <ajayjuneja52@gmail.com>
2019-11-13 13:49:43 +0530

    ads

--- frontend/views/govt-jobs/detail.php
+++ frontend/views/govt-jobs/detail.php
@@ -176,16 +176,7 @@ $this->params['seo_tags'] = [
                         </button>
                     </div>
                 </div>
-                <script async src=“https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js“></script>
-                <ins class=“adsbygoogle”
-                     style=“display:block”
-                     data-ad-client=“ca-pub-2186770765824304”
-                     data-ad-slot=“6687290527"
-                     data-ad-format=“auto”
-                     data-full-width-responsive=“true”></ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
+                <?= $this->render("/widgets/square_ads");?>
             </div>
         </div>
     </div>
