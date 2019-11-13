9eef8879f715777475efefca761ddd0fa6d5983e
Ajay juneja <ajayjuneja@dsbedutech.in>
2019-11-13 16:28:03 +0530

    ads

--- frontend/views/widgets/square_ads.php
+++ frontend/views/widgets/square_ads.php
@@ -5,14 +5,17 @@
      data-ad-slot="6687290527"
      data-ad-format="auto"
      data-full-width-responsive="true"></ins>
-<br/>
-<ins class=“adsbygoogle”
-     style=“display:block”
-     data-ad-client=“ca-pub-2186770765824304”
-     data-ad-slot=“9944488187"
-     data-ad-format=“auto”
-     data-full-width-responsive=“true”></ins>
-<br/>
 <script>
     (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
+<br/>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2186770765824304"
+     data-ad-slot="9944488187"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<br/>
