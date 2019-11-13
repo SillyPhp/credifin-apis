ad56cbae86baab89b8a7abc694ec0bf41da5b367
Ajay juneja <ajayjuneja@dsbedutech.in>
2019-11-13 15:12:45 +0530

    ads

--- frontend/views/govt-jobs/detail.php
+++ frontend/views/govt-jobs/detail.php
@@ -340,7 +340,6 @@ margin:auto;
     margin-top: 67px;
     border-radius: 5px;
     box-shadow: 0 0 10px 0px #eee;
-    float:left;
     width:100%;
 }
 .agency{
@@ -542,9 +541,9 @@ margin:auto;
     margin-top: 20px;
     border-radius: 5px;
     box-shadow: 0 0 10px 0px #eee;
-    float:left;
     width:100%;
     background-color:#1d759a;
+    margin-bottom: 20px;
 }
 .fb-btn, .li-btn, .tw-btn, .wa-btn, .mail-btn {
     padding: 10px 0;
