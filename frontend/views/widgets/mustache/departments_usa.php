3d4fce07cb644fc0bbe6eefe4dd378919ae08b7d
Sneh <snehkant93@gmail.com>
2019-12-02 18:24:34 +0530

    all departments page and widget

--- frontend/views/widgets/mustache/departments_usa.php
+++ frontend/views/widgets/mustache/departments_usa.php
@@ -10,7 +10,7 @@ use yii\helpers\Url;
                         <img src="{{logo}}" alt="{{Value}}" title="{{Value}}">
                         {{/logo}}
                         {{^logo}}
-                        <canvas class="user-icon" name="{{Value}}" width="80" height="80"
+                        <canvas class="user-icon" name="{{Value}}" width="100" height="100"
                                 color="{{color}}" font="35px"></canvas>
                         {{/logo}}
                     </div>
@@ -24,19 +24,19 @@ use yii\helpers\Url;
 </script>
 <?php
 $script = <<< JS
-function fetchDepartments(template,limit) {
+function fetchDepartments(template,limit,offset) {
   $.ajax({
   url:'/usa-jobs/get-departments',
   method:'Post',
   datatype:"json",
   data:{
       'limit':limit,
+      'offset':offset,
   },
   beforeSend: function(){
      
       },
   success:function(response) {
-      console.log(response);
       if(response.status === 200) {
           template.append(Mustache.render($('#departments-card').html(),response.cards));
           utilities.initials();
@@ -45,6 +45,6 @@ function fetchDepartments(template,limit) {
   })
 }
 JS;
-
+$this->registerJsFile('https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.min.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
 $this->registerJs($script);
 ?>
