718e77e76819d171d0be05d0b520665ad29133e7
Kulwinder Sohal <kulwindersohal2@icloud.com>
2019-12-03 11:33:12 +0530

    responsive fix

--- frontend/views/usa-jobs/index.php
+++ frontend/views/usa-jobs/index.php
@@ -247,33 +247,33 @@ Yii::$app->view->registerJs('var keywords = "' . $keywords . '"', \yii\web\View:
 <section>
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <div class="col-md-6 col-sm-6 col-xs-6">
-                    <div class="heading-style">Departments</div>
-                </div>
-                <div class="col-md-6 col-sm-6 col-xs-6">
-                    <div class="type-1">
-                        <div>
-                            <a href="/usa-jobs/departments" class="btn btn-3">
-                                <span class="txt-cate">View all</span>
-                                <span class="round"><i class="fas fa-chevron-right"></i></span>
-                            </a>
-                        </div>
+            <div class="col-md-6 col-sm-6 col-xs-6">
+                <div class="heading-style">Departments</div>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-6">
+                <div class="type-1">
+                    <div>
+                        <a href="/usa-jobs/departments" class="btn btn-3">
+                            <span class="txt-cate">View all</span>
+                            <span class="round"><i class="fas fa-chevron-right"></i></span>
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
         <div class="row">
-           <div id="departments_cards">
+            <div id="departments_cards">
 
-           </div>
+            </div>
         </div>
     </div>
 </section>
 <section>
     <div class="container">
         <div class="row">
-            <div class="heading-style">Jobs</div>
+            <div class="col-md-12">
+                <div class="heading-style">Jobs</div>
+            </div>
         </div>
         <div class="row">
             <div class="loader_screen">
