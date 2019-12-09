2a7f2a13b5190f02a2126d5bb4ccc5fd55fe5c68
Sneh Kant <sneh@dsbedutech.in>
2019-12-09 13:09:25 +0530

    department vise list jobs

--- frontend/views/govt-jobs/ind-department-detail.php
+++ frontend/views/govt-jobs/ind-department-detail.php
@@ -12,9 +12,14 @@ use yii\helpers\Url;
             <div class="row">
                 <div class="department">
                     <div class="depart-logo">
+                        <?php if ($data['logo']): ?>
                         <img src="<?= Url::to('@eyAssets/images/pages/blog/articles.png'); ?>" class="img_load">
+                        <?php else: ?>
+                            <canvas class="user-icon" name="<?= $data['Value'] ?>" width="100" height="100"
+                                    color="" font="60px"></canvas>
+                        <?php endif; ?>
                     </div>
-                    <div class="depart-name">sbi</div>
+                    <div class="depart-name"><?= $data['Value'] ?></div>
                 </div>
             </div>
         </div>
@@ -29,14 +34,27 @@ use yii\helpers\Url;
             </div>
 
             <div class="col-md-12">
-                <div class="blogbox"></div>
+                <div id="cards">
+                </div>
+                <div class="align_btn">
+                    <button id="loader" class="btn btn-success">Load More</button>
+                </div>
             </div>
         </div>
     </section>
-
+<input type="hidden" name="dept_id" id="dept_id" value="<?= $data['dept_enc_id']; ?>">
 <?php
-echo $this->render('/widgets/mustache/application-card');
+echo $this->render('/widgets/mustache/application-card-bk');
 $this->registerCss('
+.application-card-main
+{
+height:210px;
+}
+.align_btn
+{
+text-align:center;
+clear:both;
+}
 .head-img{
     background: url(\'/assets/themes/ey/images/pages/blog/articles.png\');
     background-repeat: repeat;
@@ -69,3 +87,16 @@ $this->registerCss('
     text-transform: uppercase;
 }
 ');
+echo $this->render('/widgets/mustache/govt-jobs-card');
+$script = <<< JS
+var dept_id = $('#dept_id').val();
+$(document).on('click','#loader',function(e) {
+  e.preventDefault();
+  fetchDeptData(template=$('#cards'),limit,offset+12,dept_id,loader=false,loader_btn=true);
+})
+var limit =12;
+var offset = 0;
+fetchDeptData(template=$('#cards'),limit,offset,dept_id,loader=true,loader_btn=false);
+JS;
+
+$this->registerJs($script);
