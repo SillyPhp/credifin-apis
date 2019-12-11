fa6d566d7f3504a6c0cd102e39ddc2cceb752c0c
Ajay Juneja <ajayjuneja52@gmail.com>
2019-12-11 17:32:07 +0530

    application Templates(completed).

--- account/controllers/JobsController.php
+++ account/controllers/JobsController.php
@@ -4,6 +4,7 @@ namespace account\controllers;
 
 use account\models\applications\ApplicationDataProvider;
 use account\models\applications\ApplicationForm;
+use account\models\applications\ApplicationTemplateDataProvider;
 use account\models\applications\ExtendsJob;
 use account\models\applications\ShortJobs;
 use account\models\applications\UserAppliedApplication;
@@ -736,16 +737,52 @@ class JobsController extends Controller
         }
     }
 
-//    public function actionCloneTemplate($aidk){
-//        if(Yii::$app->user->identity->organization){
-//            $model = new ApplicationForm();
-//            return $this->render('/employer-applications/form', [
-//                'model' => $model,
-//                ]);
-//        } else{
-//            throw new HttpException(404, Yii::t('account', 'Page not found'));
-//        }
-//    }
+    public function actionCloneTemplate($aidk){
+        if(Yii::$app->user->identity->organization){
+            $model = new ApplicationForm();
+            $type = 'Clone_Jobs';
+            $primary_cat = $model->getPrimaryFields();
+            $questionnaire = $model->getQuestionnnaireList();
+            $industry = $model->getndustry();
+            $benefits = $model->getBenefits();
+            $process = $model->getInterviewProcess();
+            $placement_locations = $model->getOrganizationLocations();
+            $interview_locations = $model->getOrganizationLocations(2);
+            if ($model->load(Yii::$app->request->post())) {
+                Yii::$app->response->format = Response::FORMAT_JSON;
+                $session_token = Yii::$app->request->post('n');
+                if ($application_id = $model->saveValues($type)) {
+                    $session = Yii::$app->session;
+                    if (!empty($session->get($session_token))) {
+                        $session->remove($session_token);
+                    }
+                    return $response = [
+                        'status' => 200,
+                        'title' => 'Success',
+                        'app_id' => $application_id,
+                    ];
+                } else {
+                    return false;
+                }
+            } else {
+                $obj = new ApplicationTemplateDataProvider();
+                $model = $obj->setValues($model, $aidk);
+                return $this->render('/employer-applications/form', [
+                    'model' => $model,
+                    'primary_cat' => $primary_cat,
+                    'industry' => $industry,
+                    'placement_locations' => $placement_locations,
+                    'interview_locations' => $interview_locations,
+                    'benefits' => $benefits,
+                    'process' => $process,
+                    'questionnaire' => $questionnaire,
+                    'type' => $type,
+                ]);
+            }
+        } else{
+            throw new HttpException(404, Yii::t('account', 'Page not found'));
+        }
+    }
 
     public function actionPreview()
     {
