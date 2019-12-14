0970b6a26f83949ab1ec2dfad7b6af3f0d2f93f9
Nitesh Sharma <rs.nitesh01@gmail.com>
2019-12-14 13:06:16 +0530

    Get user internship & jobs preference data in an array and use widget for prefered jobs in the site/index page above the top city widget

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -45,7 +45,8 @@ $this->params['header_dark'] = false;
                                 </div><!-- Job Search 2 -->
                                 <span class="feature-links">Search For: <a href="/jobs">Jobs</a>,
                                     <a href="/internships">Internships</a>, <a href="/training-programs">Training Courses</a>,
-                                    <a href="/reviews">Reviews</a>, <a href="/learning">Learning Content</a>, <a href="/blog">Blogs</a>
+                                    <a href="/reviews">Reviews</a>, <a href="/learning">Learning Content</a>, <a
+                                            href="/blog">Blogs</a>
                                 </span>
                             </div>
                         </div>
@@ -192,7 +193,7 @@ $this->params['header_dark'] = false;
     </div>
 </section>
 <!---->
-<?= $this->render('/widgets/usa_and_govt_jobs');?>
+<?= $this->render('/widgets/usa_and_govt_jobs'); ?>
 
 <section>
     <div class="block">
@@ -528,7 +529,18 @@ $this->params['header_dark'] = false;
         </div>
     </div>
 </section>
-<?= $this->render('/widgets/whatsapp-widget')?>
+
+<section>
+    <div class="container">
+        <?=
+        $this->render('/widgets/featured-jobs', [
+            'featured_jobs' => $featured_jobs
+        ])
+        ?>
+    </div>
+</section>
+
+<?= $this->render('/widgets/whatsapp-widget') ?>
 <section>
     <div class="container ">
         <div class="cat-padding">
@@ -561,82 +573,81 @@ $this->params['header_dark'] = false;
 <!--use to show stats like Job count , profile/title count of jobs, location count of jobs and conpanies count for jobs-->
 <div id="stats_cards"></div>
 
-<?= $this->render('/widgets/top-cities',[
+<?= $this->render('/widgets/top-cities', [
     'cities_jobs' => $cities_jobs
-])?>
+]) ?>
 <!---------------how it works-------------->
 <?php
-    if(Yii::$app->user->isGuest) {
-?>
-        <section class="how-it-works">
-            <div class="container">
-                <div class="hiw-heading">Take your career to the next level. <p>Join Empower Youth Today.</p></div>
-                <div class="row ">
-                    <div class="col-md-3">
-                        <div class="fade-in one">
-                            <div class="how-icon">
-                                <img src="<?= Url::to('@eyAssets/images/pages/index2/create-profile.png') ?>"
-                                     title="Create your Exclusive Profile" alt="Create your Exclusive Profile"/>
-                            </div>
-                            <div class="how-text-box">
-                                <div class="how-heading">Create your Exclusive Profile</div>
-                            </div>
+if (Yii::$app->user->isGuest) {
+    ?>
+    <section class="how-it-works">
+        <div class="container">
+            <div class="hiw-heading">Take your career to the next level. <p>Join Empower Youth Today.</p></div>
+            <div class="row ">
+                <div class="col-md-3">
+                    <div class="fade-in one">
+                        <div class="how-icon">
+                            <img src="<?= Url::to('@eyAssets/images/pages/index2/create-profile.png') ?>"
+                                 title="Create your Exclusive Profile" alt="Create your Exclusive Profile"/>
                         </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="fade-in two">
-                            <div class="how-icon">
-                                <img src="<?= Url::to('@eyAssets/images/pages/index2/discover.png') ?>"
-                                     title="Get discovered by top employers" alt="Get discovered by top employers"/>
-                            </div>
-                            <div class="how-text-box">
-                                <div class="how-heading"> Get discovered by top employers</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="fade-in three">
-                            <div class="how-icon">
-                                <img src="<?= Url::to('@eyAssets/images/pages/index2/evaluate.png') ?>"
-                                     title="Evaluate Offer"
-                                     alt="Evaluate Offer"/>
-                            </div>
-                            <div class="how-text-box">
-                                <div class="how-heading">Evaluate Offer</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="fade-in four">
-                            <div class="how-icon">
-                                <img src="<?= Url::to('@eyAssets/images/pages/index2/recive.png') ?>"
-                                     title="Receive Custom Job Notifications" alt="Receive Custom Job Notifications">
-                            </div>
-                            <div class="how-text-box">
-                                <div class="how-heading">Receive Custom Job Notifications</div>
-                            </div>
+                        <div class="how-text-box">
+                            <div class="how-heading">Create your Exclusive Profile</div>
                         </div>
                     </div>
                 </div>
-                <?php
-                if (Yii::$app->user->isGuest) {
-                    ?>
-                    <div class="row">
-                        <div class="signupbttns">
-                            <a href="/login" class="login-bttn" title="Login">Login</a>
-                            <a href="/signup/individual" class="sign-up" title="Sign Up">Sign Up</a>
+                <div class="col-md-3">
+                    <div class="fade-in two">
+                        <div class="how-icon">
+                            <img src="<?= Url::to('@eyAssets/images/pages/index2/discover.png') ?>"
+                                 title="Get discovered by top employers" alt="Get discovered by top employers"/>
+                        </div>
+                        <div class="how-text-box">
+                            <div class="how-heading"> Get discovered by top employers</div>
                         </div>
                     </div>
-                    <?php
-                }
-                ?>
+                </div>
+                <div class="col-md-3">
+                    <div class="fade-in three">
+                        <div class="how-icon">
+                            <img src="<?= Url::to('@eyAssets/images/pages/index2/evaluate.png') ?>"
+                                 title="Evaluate Offer"
+                                 alt="Evaluate Offer"/>
+                        </div>
+                        <div class="how-text-box">
+                            <div class="how-heading">Evaluate Offer</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="fade-in four">
+                        <div class="how-icon">
+                            <img src="<?= Url::to('@eyAssets/images/pages/index2/recive.png') ?>"
+                                 title="Receive Custom Job Notifications" alt="Receive Custom Job Notifications">
+                        </div>
+                        <div class="how-text-box">
+                            <div class="how-heading">Receive Custom Job Notifications</div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </section>
-        <?php
+            <?php
+            if (Yii::$app->user->isGuest) {
+                ?>
+                <div class="row">
+                    <div class="signupbttns">
+                        <a href="/login" class="login-bttn" title="Login">Login</a>
+                        <a href="/signup/individual" class="sign-up" title="Sign Up">Sign Up</a>
+                    </div>
+                </div>
+                <?php
+            }
+            ?>
+        </div>
+    </section>
+    <?php
 }
 ?>
 <!--how it works ends-->
-
 
 
 <!--new section starts-->
@@ -745,7 +756,7 @@ echo $this->render('/widgets/info-stats');
 echo $this->render('/widgets/mustache/learning-categories');
 $this->registerCss('
 .j-tweets{
-    background:url('. Url::to('@eyAssets/images/backgrounds/p6.png') .');  
+    background:url(' . Url::to('@eyAssets/images/backgrounds/p6.png') . ');  
     background-attachment: fixed;
     padding-bottom:20px;
 }
@@ -820,7 +831,7 @@ $this->registerCss('
     max-height:300px;
 }
 .moble-bg{
-background:url('. Url::to('@eyAssets/images/backgrounds/p6.png') .'); 
+background:url(' . Url::to('@eyAssets/images/backgrounds/p6.png') . '); 
 background-attachment:fixed;
 //    background:url(' . Url::to('@eyAssets/images/pages/index2/mobile-app-bg.png') . ');
 //    background-repeat:no-repeat;
@@ -1115,21 +1126,21 @@ background-attachment:fixed;
     color:#5E4795;
 }
 .ser-box-yellow{
-    background: url('.Url::to('@eyAssets/images/pages/index2/learningbg.png').');
+    background: url(' . Url::to('@eyAssets/images/pages/index2/learningbg.png') . ');
 }
 .ser-box-yellow:hover{
     border-top: 5px solid #f8b321;
     color:#f8b321
 }
 .ser-box-maroon{
-    background: url('.Url::to('@eyAssets/images/pages/index2/trainingbg.png').');
+    background: url(' . Url::to('@eyAssets/images/pages/index2/trainingbg.png') . ');
 }
 .ser-box-maroon:hover{
     border-top: 5px solid #c76692;
     color:#c76692
 }
 .ser-box-green{
-    background: url('.Url::to('@eyAssets/images/pages/index2/careerAdviceBg.png').');
+    background: url(' . Url::to('@eyAssets/images/pages/index2/careerAdviceBg.png') . ');
 }
 .ser-box-green:hover{
     border-top: 5px solid #047c7d;
@@ -1953,7 +1964,8 @@ $this->registerJsFile('https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/
         // var i = 0;
         // i += 5;
         // var k = 4;
-        var ll = 0;``
+        var ll = 0;
+        ``
         var zz = 0;
         var tt = 0;
         var f = true;
