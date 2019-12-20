2fd4e3337e98d3ed8cf27f00ad57fc25da49be89
kulwindersohal <kulwindersohal2@iclud.com>
2019-12-20 11:04:24 +0530

    ui final cources

--- frontend/views/courses/courses-detail-page.php
+++ frontend/views/courses/courses-detail-page.php
@@ -29,7 +29,12 @@ use yii\helpers\Url;
                 <div class="col-md-8 col-sm-8">
                     <div class="about-course">
                         <div class="course-heading">About this course</div>
-                        <div class="course-detail">This course introduces you to important concepts and terminology for working with Google Cloud Platform (GCP). You learn about, and compare, many of the computing and storage services available in Google Cloud Platform, including Google App Engine, Google Compute Engine, Google Kubernetes Engine, Google Cloud Storage, Google Cloud SQL, and BigQuery. You learn about important resource and policy management tools, </div>
+                        <div class="course-detail">This course introduces you to important concepts and terminology for
+                            working with Google Cloud Platform (GCP). You learn about, and compare, many of the
+                            computing and storage services available in Google Cloud Platform, including Google App
+                            Engine, Google Compute Engine, Google Kubernetes Engine, Google Cloud Storage, Google Cloud
+                            SQL, and BigQuery. You learn about important resource and policy management tools,
+                        </div>
                     </div>
                     <div class="learn-box">
                         <h3>What you will learn</h3>
@@ -51,7 +56,9 @@ use yii\helpers\Url;
                         <h3>Requirements</h3>
                         <div class="req-points">
                             <ul>
-                                <li>Absolutely no experience is required. We will start from the basics and gradually build up your knowledge. Everything is in the course.</li>
+                                <li>Absolutely no experience is required. We will start from the basics and gradually
+                                    build up your knowledge. Everything is in the course.
+                                </li>
                                 <li>You will need Microsoft Excel 2010, 2013, or 2016</li>
                                 <li>You will need Microsoft PowerPoint 2010, 2013, or 2016</li>
                             </ul>
@@ -67,14 +74,29 @@ use yii\helpers\Url;
                         <div class="buy-btn">
                             <button class="new-btn-set">Buy Now</button>
                         </div>
+                        <div class="discount-set">
+                            <button onclick="myFunction()" class="coupon-code">Apply Promo Code</button>
+                        </div>
+                        <div class="coupon-modal" id="coupon">
+                            <input class="form-control set-marg" type="text">
+                            <button class="coupon-btn-set">Apply</button>
+                        </div>
+                        <div class="get-coupon input-group">
+                            <input type="text" id="value-save" class="form-control set-form" value="">
+                            <div class="input-group-btn">
+                                <button class="clipboard btn btn-default get-btn"  onClick="valueSave();">
+                                    <i class="fa fa-clipboard" aria-hidden="true"></i>Copy to Clipboard
+                                </button>
+                            </div>
+                        </div>
                         <div class="c-includes">
                             <div class="include-head">This Course Includes</div>
                             <div class="include-inner">
                                 <ul>
-                                    <li>17 hours on-demand video </li>
-                                    <li>2 articles </li>
+                                    <li>17 hours on-demand video</li>
+                                    <li>2 articles</li>
                                     <li>462 downloadable resources</li>
-                                    <li>Full lifetime access </li>
+                                    <li>Full lifetime access</li>
                                     <li>Certificate of Completion</li>
                                 </ul>
                             </div>
@@ -85,6 +107,18 @@ use yii\helpers\Url;
         </div>
     </section>
 
+    <script>
+        function myFunction() {
+            document.getElementById("coupon").style.display = "block";
+        }
+        function valueSave() {
+            var copyText = document.getElementById("value-save");
+            copyText.select();
+            copyText.setSelectionRange(0, 99999)
+            document.execCommand("copy");
+            //alert("Copied the text: " + copyText.value);
+        }
+    </script>
 <?php
 $this->registerCss('
 .bg-set-clr {
@@ -267,5 +301,75 @@ $this->registerCss('
     list-style: disc;
     font-size: 15px;
     font-family: roboto;
+}
+.discount-set {
+    text-align: right;
+}
+.discount-set a{
+    font-size: 13px;
+    color:#bd6666;
+    font-family: roboto;
+}
+.btn-primary{
+    width:100%;
+}
+.coupon-modal{
+    display:none;
+}
+.coupon-code {
+    border: none;
+    background-color: #fff;
+    color:#ce3c3c;
+    font-size: 12px;
+    font-family: roboto;
+}
+.set-marg {
+    margin: 10px 0px;
+}
+.coupon-btn-set {
+    width: 100%;
+    color: #fff;
+    background-color:#ff7803;
+    border: 1px solid transparent;
+    padding: 11px 12px;
+    font-size: 20px;
+    border-radius: 4px;
+    font-weight: 500;
+    font-family: roboto;
+}
+.set-form{
+    height: 50px;
+    font-size: 20px;
+    line-height: 30px;
+    font-weight: bold;
+    border: 1px dashed;
+    border-color: #5677fc;
+}
+.get-btn{
+    height: 50px;
+    background-color: #00a0e3;
+    color:#ffffff;
+    border-color:#00a0e3;
+    -webkit-transition: all 0.2s ease-out;
+    -moz-transition: all 0.2s ease-out;
+    -o-transition: all 0.2s ease-out;
+    transition: all 0.2s ease-out;
+}
+.get-btn > i{
+    margin-right:5px;
+}
+.get-btn:hover {
+    color:#00a0e3;
+    background-color:#ffffff;
+    border-color:#00a0e3;
+}
+.clipboard.btn.btn-default.get-btn:focus {
+    background-color: #fff;
+}
+.clipboard.btn.btn-default.get-btn:active{
+    background-color: #fff;
+}
+.get-coupon{
+    margin: 10px 0px;
 }
 ');
--- frontend/views/courses/institute-detail-page.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: SoHaL
- * Date: 12/19/2019
- * Time: 11:09 AM
- */
