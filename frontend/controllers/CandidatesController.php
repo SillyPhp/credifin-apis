bc4c4b720ceafb7aaf39ff56f69a1e75f8dd1096
Nitesh Sharma <rs.nitesh01@gmail.com>
2019-12-28 12:05:51 +0530

    Candidate cards displayed for organizations..

--- frontend/controllers/CandidatesController.php
+++ frontend/controllers/CandidatesController.php
@@ -10,6 +10,7 @@ use common\models\UserWorkExperience;
 use Yii;
 use yii\web\Controller;
 use yii\web\Response;
+use yii\helpers\Url;
 
 class CandidatesController extends Controller
 {
@@ -25,17 +26,15 @@ class CandidatesController extends Controller
         return $this->render('features');
     }
 
-    public function actionGitCandidate()
+    public function actionGitCandidate($offset = null)
     {
-        $users = Users::find()
+        $data = Users::find()
             ->alias('a')
             ->select([
                 'a.user_enc_id',
                 'a.city_enc_id',
                 'a.user_type_enc_id',
                 'CONCAT(a.first_name, " ", a.last_name) fullname',
-//                'a.first_name',
-//                'a.last_name',
                 'a.image',
                 'a.image_location',
                 'a.initials_color',
@@ -67,16 +66,66 @@ class CandidatesController extends Controller
             ->andWhere(['a.is_deleted' => 0])
             ->groupBy('a.user_enc_id')
             ->orderBy(['exp_count' => SORT_DESC, 'sk_count' => SORT_DESC, 'e.company' => SORT_ASC, 'e.title' => SORT_ASC])
+            ->limit(20)
+            ->offset($offset)
             ->distinct()
             ->asArray()
             ->all();
 
+        $users = [];
+        $j = 0;
+        foreach ($data as $u) {
+            if($u['image']){
+                $icon = '<a href="/'.$u['username'].'"><img src="'. Url::to(Yii::$app->params->upload_directories->users->image . $u['image_location'] . '/' . $u['image']) .'" alt="'.$u['fullname'].'"></a>';
+            } else {
+                $icon = '<canvas class="user-icon img-circle img-responsive" name="'.$u['fullname'].'" color="'.$u['initials_color'].'" width="140" height="140" font="70px"></canvas>';
+            }
+            array_push($users, [
+                'fullname' => $u['fullname'],
+                'image' => $u['image'],
+                'image_location' => $u['image_location'],
+                'initials_color' => $u['initials_color'],
+                'username' => $u['username'],
+                'sk_count' => $u['sk_count'],
+                'exp_count' => $u['exp_count'],
+                'city_name' => ($u['city_name']) ? $u['city_name'] : 'N/A',
+                'userWorkExperiences' => ($u['userWorkExperiences']) ? [
+                    'company' => $u['userWorkExperiences'][0]['company'],
+                    'title' => $u['userWorkExperiences'][0]['title']
+                ] : '',
+                'icon' => $icon,
+                'skills' => [],
+            ]);
+            if ($u['userSkills']) {
+                $plus_count = '';
+                if (count($u['userSkills']) > 3) {
+                    $count = 3;
+                    $c = count($u['userSkills']) - $count;
+                    $plus_count = '<li class="more-skill bg-primary">+'.$c.'</li>';
+                } else {
+                    $count = count($u['userSkills']);
+                }
+                for ($i = 0; $i < $count; $i++) {
+                    array_push($users[$j]['skills'], '<li>' . $u['userSkills'][$i]['skill'] . '</li>');
+                }
+                if ($plus_count) {
+                    array_push($users[$j]['skills'], $plus_count);
+                }
+            }
+            $j++;
+        }
+
 //        print_r($users);
 //        exit();
 //        print_r(count($users));exit();
-        return $this->render('git-candidate', [
-            'users' => $users
-        ]);
+        if (Yii::$app->request->isPost) {
+            Yii::$app->response->format = Response::FORMAT_JSON;
+            return $users;
+        } else {
+            return $this->render('git-candidate', [
+                'users' => $users
+            ]);
+        }
     }
 
 }
--- frontend/views/candidates/git-candidate.php
+++ frontend/views/candidates/git-candidate.php
@@ -3,148 +3,470 @@ $this->params['header_dark'] = true;
 
 use yii\helpers\Html;
 use yii\helpers\Url;
-
+echo $this->render('/widgets/mustache/git-candidates');
 ?>
     <section>
         <div class="container">
-            <div class="row">
-                <?php
-                foreach ($users as $u) {
-                    ?>
-                    <div class="col-md-3 col-sm-6">
-                        <div class="cand-box">
-                            <div class="cand-img">
-                                <?php
-                                if (!empty($u['image'])) {
-                                    $path = Yii::$app->params->upload_directories->users->image . $u['image_location'] . '/' . $u['image'];
-                                    ?>
-                                    <img src="<?= Url::to($path) ?>">
-                                    <?php
-                                } else {
-                                    ?>
-                                    <canvas class="user-icon" color="<?= $u['initials_color'] ?>"
-                                            name="<?= $u['fullname'] ?>>" width="100" height="100" font="35px"></canvas>
-                                    <?php
-                                }
-                                ?>
-                            </div>
-                            <div class="cand-detail">
-                                <h3><?= $u['fullname'] ?></h3>
-                                <div class="cand-position"><?= (!empty($u['userWorkExperiences'][0]['company']) && !empty($u['userWorkExperiences'][0]['title'])) ? $u['userWorkExperiences'][0]['title'] . ' at ' . $u['userWorkExperiences'][0]['company'] : '' ?></div>
-                                <div class="cand-skills">
-                                    <ul>
-                                        <?php
-                                        for ($i = 0; $i < 3; $i++) {
-                                            if (!empty($u['userSkills'][$i])) {
-                                                ?>
-                                                <li><?= $u['userSkills'][$i]['skill'] ?></li>
-                                                <?php
-                                            }
-                                        }
-                                        ?>
-                                        <?= ($u['sk_count'] > 3) ? '+' . ($u['sk_count'] - 3) : '' ?>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="cand-location"><i
-                                        class="fas fa-map-marker-alt"></i> <?= ($u['city_name']) ? $u['city_name'] : '' ?>
-                            </div>
-                            <div class="view-btn">
-                                <a href="/<?= $u['username'] ?>">View Profile</a>
-                            </div>
-                        </div>
-                    </div>
-                    <?php
-                }
-                ?>
+            <div class="row" id="user_cards">
+
+            </div>
+            <div class="col-md-12 col-sm-12">
+                <div id="cardBlock" class="row work-load blogbox border-top-set m-0 mb-20"></div>
+                <a href="#" id="loadMore"
+                   class="ajax-paginate-link btn btn-border btn-more btn--primary load-more loading_more">
+                    <span class="load-more-text">Load More</span>
+                    <svg class="load-more-spinner" viewBox="0 0 57 57" xmlns="http://www.w3.org/2000/svg"
+                         stroke="currentColor">
+                        <g fill="none" fill-rule="evenodd">
+                            <g transform="translate(1 1)" stroke-width="2">
+                                <circle cx="8.90684" cy="50" r="5">
+                                    <animate attributeName="cy" begin="0s" dur="2.2s" values="50;5;50;50"
+                                             calcMode="linear" repeatCount="indefinite"></animate>
+                                    <animate attributeName="cx" begin="0s" dur="2.2s" values="5;27;49;5"
+                                             calcMode="linear" repeatCount="indefinite"></animate>
+                                </circle>
+                                <circle cx="25.0466" cy="8.99563" r="5">
+                                    <animate attributeName="cy" begin="0s" dur="2.2s" from="5" to="5"
+                                             values="5;50;50;5" calcMode="linear"
+                                             repeatCount="indefinite"></animate>
+                                    <animate attributeName="cx" begin="0s" dur="2.2s" from="27" to="27"
+                                             values="27;49;5;27" calcMode="linear"
+                                             repeatCount="indefinite"></animate>
+                                </circle>
+                                <circle cx="47.0466" cy="46.0044" r="5">
+                                    <animate attributeName="cy" begin="0s" dur="2.2s" values="50;50;5;50"
+                                             calcMode="linear" repeatCount="indefinite"></animate>
+                                    <animate attributeName="cx" from="49" to="49" begin="0s" dur="2.2s"
+                                             values="49;5;27;49" calcMode="linear"
+                                             repeatCount="indefinite"></animate>
+                                </circle>
+                            </g>
+                        </g>
+                    </svg>
+                </a>
             </div>
         </div>
     </section>
 
 <?php
 $this->registerCss('
-.cand-box {
-    border: 1px solid #eee;
-    border-radius: 5px;
-    padding: 10px;
-    box-shadow: 0px 0px 3px 0px #eee;
-    transition: all 250ms ease-out, transform 250ms ease-out, -webkit-transform 250ms ease-out;
-    cursor:pointer;
-    margin-bottom:20px;
+//.cand-box {
+//    border: 1px solid #eee;
+//    border-radius: 5px;
+//    padding: 10px;
+//    box-shadow: 0px 0px 3px 0px #eee;
+//    transition: all 250ms ease-out, transform 250ms ease-out, -webkit-transform 250ms ease-out;
+//    cursor:pointer;
+//    margin-bottom:20px;
+//}
+//.cand-box:hover {
+//    box-shadow: 0px 7px 13px rgba(0, 0, 0, 0.14);
+//    transform: translate3d(0, -3px, 0);
+//}
+//.cand-img {
+//    height: 100px;
+//    width: 100px;
+//    margin: 0 auto;
+//    border-radius: 50px;
+//    border: 1px solid #ddd;
+//    overflow: hidden;
+//}
+//.cand-img img {
+//    height: auto;
+//    width: auto;
+//    border: 3px solid #fff;
+//    border-radius: 50px;
+//}
+//.cand-detail {
+//    height: 140px;
+//}
+//.cand-detail h3 {
+//    text-align: center;
+//    margin: 10px 0px 0px 0;
+//    font-family: roboto;
+//    text-transform: capitalize;
+//    font-size: 20px;
+//}
+//.cand-position {
+//    text-align: center;
+//    font-size: 14px;
+//    font-family: roboto;
+//    position: relative;
+//    display: -webkit-box;
+//    -webkit-line-clamp: 1;
+//    -webkit-box-orient: vertical;
+//    overflow: hidden;
+//    text-overflow: ellipsis;
+//}
+//.cand-skills {
+//    text-align: center;
+//    margin: 5px 0px;
+//}
+//.cand-skills > ul > li {
+//    border: 1px solid #ddd;
+//    padding: 1px 10px;
+//    display: inline-block;
+//    border-radius: 15px;
+//    font-family: roboto;
+//    font-size: 13px;
+//    margin-right: 4px;
+//    margin-bottom:5px;
+//}
+//.cand-location {
+//    text-align: center;
+//    margin: 5px 0px;
+//    font-size:13px;
+//    height: 22px;
+//}
+//.view-btn {
+//    text-align: center;
+//    margin: 10px 0px 3px 0px;
+//}
+//.view-btn a {
+//    background-color: #fff;
+//    color:#00a0e3;
+//    padding: 4px 15px;
+//    border-radius: 3px;
+//    border: 1px solid #00a0e3;
+//    transition: ease-out 0.3s;
+//}
+//.view-btn a:hover {
+//    background-color:#00a0e3;
+//    color:#fff;
+//}
+
+.tab-empty{
+    padding:20px;
 }
-.cand-box:hover {
-    box-shadow: 0px 7px 13px rgba(0, 0, 0, 0.14);
-    transform: translate3d(0, -3px, 0);
+.tab-empty-icon img{
+    max-width:250px; 
+    margin:0 auto;
 }
-.cand-img {
-    height: 100px;
-    width: 100px;
-    margin: 0 auto;
-    border-radius: 50px;
-    border: 1px solid #ddd;
-    overflow: hidden;
+.tab-empty-text{
+    text-align:center; 
+    font-size:35px; 
+    font-family:lobster; 
+    color:#999999; 
+    padding-top:20px;
 }
-.cand-img img {
-    height: auto;
-    width: auto;
-    border: 3px solid #fff;
-    border-radius: 50px;
+.shortlist-strip{
+    position:absolute;
+    top:0;
+    left:0;
 }
-.cand-detail {
-    height: 140px;
-}
-.cand-detail h3 {
-    text-align: center;
-    margin: 10px 0px 0px 0;
-    font-family: roboto;
-    text-transform: capitalize;
-    font-size: 20px;
-}
-.cand-position {
-    text-align: center;
-    font-size: 14px;
-    font-family: roboto;
-    position: relative;
-    display: -webkit-box;
-    -webkit-line-clamp: 1;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-    text-overflow: ellipsis;
-}
-.cand-skills {
-    text-align: center;
-    margin: 5px 0px;
-}
-.cand-skills > ul > li {
-    border: 1px solid #ddd;
-    padding: 1px 10px;
-    display: inline-block;
-    border-radius: 15px;
-    font-family: roboto;
-    font-size: 13px;
-    margin-right: 4px;
-    margin-bottom:5px;
-}
-.cand-location {
-    text-align: center;
-    margin: 5px 0px;
-    font-size:13px;
-    height: 22px;
-}
-.view-btn {
-    text-align: center;
-    margin: 10px 0px 3px 0px;
-}
-.view-btn a {
-    background-color: #fff;
-    color:#00a0e3;
-    padding: 4px 15px;
-    border-radius: 3px;
-    border: 1px solid #00a0e3;
-    transition: ease-out 0.3s;
-}
-.view-btn a:hover {
-    background-color:#00a0e3;
+.s-strip{
+    padding:5px 10px;
+    border:1px solid #00a0e3;
+    border-radius:0 0 10px 0;
+    background:#00a0e3;
     color:#fff;
 }
+ button.viewall-jobs{
+    border:none;
+}   
+ *:focus{
+    outline:none !important;
+}
+#btn-group1{
+    display:hidden;
+}
+#btn-group2{
+    display:none;
+}
+.paid-candidate-container{
+    background: #ffffff;
+    border-radius: 6px !important;
+    overflow: hidden;
+	text-align:center;
+    margin-bottom:30px;
+	position:relative;
+	transition: .4s;
+    border:1px solid #eaeff5;
+}
+.paid-candidate-container:hover, .paid-candidate-container:focus{
+    transform: translateY(-5px);
+    -webkit-transform: translateY(-5px);
+	cursor:pointer;
+}
+.paid-candidate-box-thumb img{
+    height:100%;
+}
+.com-load-more-btn{
+    max-width:150px;
+    margin:0 auto;
+    color:#fff;
+    font-size:14px;
+}
+.paid-candidate-box{
+    text-align: center;
+    padding:20px 10px 15px;
+}
+.paid-candidate-status {
+    position: absolute;
+    left:32px;
+    top: 25px;
+    background:#01c73d;
+    color: #ffffff;
+    padding: 4px 18px;
+    border-radius: 50px;
+    font-weight: 500;
+}
+.paid-candidate-box-thumb {
+    margin-bottom: 30px;
+    width: 120px;
+	height:120px;
+    margin: 0 auto 25px auto;
+	border-radius:50% !important;
+	overflow:hidden;
+	box-shadow: 0 0px 14px 0 rgba(0, 0, 0, 0.08);
+	-webkit-box-shadow: 0 0px 14px 0 rgba(0, 0, 0, 0.08);
+	-moz-box-shadow: 0 0px 14px 0 rgba(0, 0, 0, 0.08);
+}
+.paid-candidate-box-detail h4{
+	margin-bottom:4px;
+	font-size:20px;
+	text-overflow: ellipsis;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    display: -webkit-inline-box;
+    overflow: hidden;
+}
+.paid-candidate-box-exp{
+    display: flex;
+    justify-content: center;
+}
+.paid-candidate-box-detail .desination, .paid-candidate-box-detail .location,
+.paid-candidate-box-exp .desination{
+	font-weight:500;
+	font-size:15px;
+	color:#677484;
+	height:27px;
+    padding:5px 20px 0;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    display: -webkit-box;
+    overflow: hidden;
+        
+}
+.paid-candidate-box-extra ul {
+    margin: 10px 0;
+	padding:0;
+	min-height:74px;
+	height: 100px;
+}
+.paid-candidate-box-extra ul li {
+    list-style: none;
+    padding:3px 15px;
+    border: 1px solid #b9c5ce;
+    border-radius: 50px !important;
+    margin: 2px 0;
+    font-weight: 500;
+    color: #657180;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    display: -webkit-inline-box;
+    overflow: hidden;
+}
+.paid-candidate-box-extra ul li.more-skill{
+	color:#ffffff;
+	border-color:#1194f7;
+}
+a.btn.btn-paid-candidate {
+    padding: 10px !important;
+    display: inline-block;
+    width: 100%;
+    font-size: 16px;
+    font-weight: 500;
+    border-radius: 0;
+}
+a.btn.btn-paid-candidate:hover, a.btn.btn-paid-candidate:focus{
+	background:#00a0e3; 
+	color:#ffffff;
+	-webkit-transition: all .3s ease-in-out;
+    -moz-transition: all .3s ease-in-out;
+    -o-transition: all .3s ease-in-out;
+    -ms-transition: all .3s ease-in-out;
+    transition: all .3s ease-in-out;
+}
+.paid-candidate-box .dropdown{
+	position:absolute;
+	right:30px;
+	top:25px;
+}
+.btn-trans {
+    background: transparent;
+    border: none;
+	font-size:20px;
+    color:#99abb9;
+}
+.dropdown-menu.pull-right {
+    right: 0;
+    left: auto !important;
+    top: 90% !important;
+}
+.dropdown-menu.pull-right {
+    right: 0;
+	border-color: #ebf2f7;
+	padding: 0;
+    left: auto !important;
+    top: 90% !important;
+}
+.dropdown-menu>a, .dropdown-menu>button {
+    display: block;
+    padding: 14px 12px 14px 12px; 
+    clear: both;
+    font-weight: 300; 
+    line-height: 1.42857143;
+    color: #67757c;
+    border-bottom: 1px solid #f1f6f9;
+    background:transparent;
+}
+.dropdown-menu>button {
+    text-align: left;
+    border: none;
+    width: 100%;
+}
+.bt-1 {
+    border-top: 1px solid #eaeff5!important;
+}
+.custom-buttons{
+    width:100%;
+    font-size: 10px !important;
+    padding: 8px 0px !important;
+    margin-bottom:20px;
+}
+.dashboard-button a, .dashboard-button button{    
+    margin-left:10px !important;
+}
+/*----------------------*/
+.checkbox-input {
+  display: none;
+}
+.checkbox-label {
+  vertical-align: top;
+  width: 100%;
+  cursor: pointer;
+  font-weight: 400;
+  margin-bottom:0px;
+}
+.p-category-main:hover .checkbox-label:before {
+    top:-5px !important;
+} 
+.checkbox-label:before {
+  content: "";
+  position: absolute;
+  top: 80px;
+  left: 15px;
+  width: 35px;
+  height: 35px;
+  opacity: 0;
+  background-color: #2196F3;
+  background-repeat: no-repeat; 
+  background-size: 30px;
+  border-radius: 8px 0;
+//  -webkit-transform: translate(0%, -50%);
+//  transform: translate(0%, -50%);
+  transition: all 0.4s ease;
+  z-index:999;
+  
+}
+.checkbox-input:checked + .checkbox-label:before {
+  top: 0;
+  opacity: 1;
+}
+.checkox-input:checked + .checkbox-label{
+   transform: translateY(-5px);
+    -webkit-transform: translateY(-5px);
+	cursor:pointer;
+}
+.checkbox-input:checked + .checkbox-label .checkbox-text span {
+  -webkit-transform: translate(0, -8px);
+  transform: translate(0, -8px);
+}
+.radio_questions {
+    max-width: 100%;
+    font-size: 18px;
+    font-weight: 600;
+    line-height: 36px;
+    position: relative;
+}
+.inputGroup {
+    background-color: #fff;
+    display: block;
+    margin: 10px 0;
+    position: relative;
+}
+.inputGroup input {
+    width: 32px;
+    height: 32px;
+    order: 1;
+    z-index: 2;
+    position: absolute;
+    right: 30px;
+    top: 50%;
+    transform: translateY(-50%);
+    cursor: pointer;
+    visibility: hidden;
+}
+.inputGroup input:checked ~ label {
+    color: #fff;
+    box-shadow: 0 0 10px rgba(0,0,0,.3) !important;
+}
+.inputGroup label {
+    padding: 6px 75px 10px 25px;
+    width: 96%;
+    display: block;
+    margin: auto;
+    text-align: left;
+    color: #3C454C !important;
+    cursor: pointer;
+    position: relative;
+    z-index: 2;
+    transition: color 1ms ease-out;
+    overflow: hidden;
+    border-radius: 8px;
+    border: 1px solid #eee;
+}
+.inputGroup input:checked ~ label:before {
+    transform: translate(-50%, -50%) scale3d(56, 56, 1);
+    opacity: 1;
+}
+.inputGroup input:checked ~ label:after {
+    background-color: #00a0e3;
+    border-color: #00a0e3;
+}
+
 ');
+
+$script = <<<JS
+
+    var load_more_cards = true;
+    var loading = true;
+    var offset = 0;
+    
+    $(document).ready(function() {
+        loading = false;
+        getUserCards(0);
+    });
+    
+    $(document).on('click', '#loadMore', function(event) {
+        event.preventDefault();
+        if(load_more_cards && loading){
+            loading = false;
+            getUserCards(offset);
+        }
+    });
+    
+    $(window).scroll(function() { //detact scroll
+        if($(window).scrollTop() + $(window).height() >= $(document).height() - ($('#footer').height() + 335)){ //scrolled to bottom of the page
+            if(load_more_cards && loading){
+                loading = false;
+                getUserCards(offset);
+            }
+        }
+    });
+JS;
+$this->registerJs($script);
+
