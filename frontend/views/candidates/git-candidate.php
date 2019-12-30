9d9ad40116a698cfd6b489b852dfa08b1e0ef54c
Nitesh Sharma <rs.nitesh01@gmail.com>
2019-12-30 13:21:42 +0530

    Candidate profile detail page update and dynamic show data..

--- frontend/views/candidates/git-candidate.php
+++ frontend/views/candidates/git-candidate.php
@@ -3,6 +3,7 @@ $this->params['header_dark'] = true;
 
 use yii\helpers\Html;
 use yii\helpers\Url;
+
 echo $this->render('/widgets/mustache/git-candidates');
 ?>
     <section>
@@ -47,94 +48,12 @@ echo $this->render('/widgets/mustache/git-candidates');
             </div>
         </div>
     </section>
-
 <?php
+echo $this->render('/widgets/users/preloaders/git-candidates');
 $this->registerCss('
-//.cand-box {
-//    border: 1px solid #eee;
-//    border-radius: 5px;
-//    padding: 10px;
-//    box-shadow: 0px 0px 3px 0px #eee;
-//    transition: all 250ms ease-out, transform 250ms ease-out, -webkit-transform 250ms ease-out;
-//    cursor:pointer;
-//    margin-bottom:20px;
-//}
-//.cand-box:hover {
-//    box-shadow: 0px 7px 13px rgba(0, 0, 0, 0.14);
-//    transform: translate3d(0, -3px, 0);
-//}
-//.cand-img {
-//    height: 100px;
-//    width: 100px;
-//    margin: 0 auto;
-//    border-radius: 50px;
-//    border: 1px solid #ddd;
-//    overflow: hidden;
-//}
-//.cand-img img {
-//    height: auto;
-//    width: auto;
-//    border: 3px solid #fff;
-//    border-radius: 50px;
-//}
-//.cand-detail {
-//    height: 140px;
-//}
-//.cand-detail h3 {
-//    text-align: center;
-//    margin: 10px 0px 0px 0;
-//    font-family: roboto;
-//    text-transform: capitalize;
-//    font-size: 20px;
-//}
-//.cand-position {
-//    text-align: center;
-//    font-size: 14px;
-//    font-family: roboto;
-//    position: relative;
-//    display: -webkit-box;
-//    -webkit-line-clamp: 1;
-//    -webkit-box-orient: vertical;
-//    overflow: hidden;
-//    text-overflow: ellipsis;
-//}
-//.cand-skills {
-//    text-align: center;
-//    margin: 5px 0px;
-//}
-//.cand-skills > ul > li {
-//    border: 1px solid #ddd;
-//    padding: 1px 10px;
-//    display: inline-block;
-//    border-radius: 15px;
-//    font-family: roboto;
-//    font-size: 13px;
-//    margin-right: 4px;
-//    margin-bottom:5px;
-//}
-//.cand-location {
-//    text-align: center;
-//    margin: 5px 0px;
-//    font-size:13px;
-//    height: 22px;
-//}
-//.view-btn {
-//    text-align: center;
-//    margin: 10px 0px 3px 0px;
-//}
-//.view-btn a {
-//    background-color: #fff;
-//    color:#00a0e3;
-//    padding: 4px 15px;
-//    border-radius: 3px;
-//    border: 1px solid #00a0e3;
-//    transition: ease-out 0.3s;
-//}
-//.view-btn a:hover {
-//    background-color:#00a0e3;
-//    color:#fff;
-//}
-
+#loadMore {
+    display : none;
+}
 .tab-empty{
     padding:20px;
 }
@@ -441,14 +360,20 @@ a.btn.btn-paid-candidate:hover, a.btn.btn-paid-candidate:focus{
 ');
 
 $script = <<<JS
-
     var load_more_cards = true;
     var loading = true;
     var offset = 0;
-    
     $(document).ready(function() {
         loading = false;
-        getUserCards(0);
+            getUserCards(0);
+        setTimeout(
+            function(){
+            $('.loading-main').css('display','none');
+        }, 1000);
+        setTimeout(
+            function(){
+            $('#loadMore').css('display','block');
+        }, 2000);
     });
     
     $(document).on('click', '#loadMore', function(event) {
