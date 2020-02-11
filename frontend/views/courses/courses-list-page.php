6ea3b3166b4f185a40e0edc96f4d4ad8c1065a37
Ajay Juneja <ajayjuneja52@gmail.com>
2020-02-11 18:09:31 +0530

    courses almost complete

--- frontend/views/courses/courses-list-page.php
+++ frontend/views/courses/courses-list-page.php
@@ -11,8 +11,8 @@ use yii\helpers\Url;
             <div class="c-heading">Search All type of Courses which you want to do</div>
         </div>
         <div class="search-box1">
-            <form action="<?= Url::to('#') ?>">
-                <input type="text" placeholder="Search" name="keyword">
+            <form action="<?= Url::to('/courses/courses-list') ?>">
+                <input type="text" placeholder="Search" name="keyword" value="<?= $_GET['keyword'];?>"/>
                 <button type="submit"><i class="fas fa-search"></i></button>
             </form>
         </div>
@@ -24,33 +24,33 @@ use yii\helpers\Url;
         <div class="row">
             <div class="heading-style">Courses</div>
         </div>
-        <div class="row">
-            <div class="col-md-4 col-sm-6">
-                <a href="#">
-                    <div class="course-box">
-                        <div class="course-upper">
-                            <div class="course-logo">
-                                <img src="<?= Url::to('@eyAssets/images/pages/learning-corner/element-image.png'); ?>"/>
-                            </div>
-                            <div class="course-provider">udemy</div>
-                            <div class="course-description">
-                                <div class="course-name">html</div>
-                                <div class="course-duration"><i class="far fa-clock"></i>3 months</div>
-                                <div class="course-fees"><i class="fas fa-rupee-sign"></i>15000</div>
-                                <div class="course-start"><i class="far fa-calendar-check"></i>15/10/12</div>
-                            </div>
-                        </div>
-                        <div class="course-skills">
-                            <div class="skills-set">html</div>
-                        </div>
-                    </div>
-                </a>
-            </div>
+        <div class="row" id="list-main">
+<!--            <div class="col-md-4 col-sm-6">-->
+<!--                <a href="#">-->
+<!--                    <div class="course-box">-->
+<!--                        <div class="course-upper">-->
+<!--                            <div class="course-logo">-->
+<!--                                <img src="--><?//= Url::to('@eyAssets/images/pages/learning-corner/element-image.png'); ?><!--"/>-->
+<!--                            </div>-->
+<!--                            <div class="course-provider">udemy</div>-->
+<!--                            <div class="course-description">-->
+<!--                                <div class="course-name">html</div>-->
+<!--                                <div class="course-duration"><i class="far fa-clock"></i>3 months</div>-->
+<!--                                <div class="course-fees"><i class="fas fa-rupee-sign"></i>15000</div>-->
+<!--                                <div class="course-start"><i class="far fa-calendar-check"></i>15/10/12</div>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                        <div class="course-skills">-->
+<!--                            <div class="skills-set">html</div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </a>-->
+<!--            </div>-->
         </div>
     </div>
 </section>
-
 <?php
+echo $this->render('/widgets/mustache/courses-card');
 $this->registerCss('
 .head-search {
     background-color: #60969f;
@@ -103,88 +103,30 @@ $this->registerCss('
 .search-box1 button:hover {
     color: #ff7803; 
 }
-.course-box {
-    position: relative;
-    box-shadow: 0 1px 3px 0px #797979;
-    background-color:#fff;
-    margin-bottom: 15px;
-    border-radius: 5px;
-    overflow: hidden;
-    color:#000;
-}
-.course-upper{
-    padding:5px 10px;
-    display:flex;
-}
-.course-provider {
-    position: absolute;
-    text-align: center;
-    text-transform: uppercase;
-    right: 0;
-    top: 0;
-    color: #fff;
-    padding: 4px 15px;
-    background: #FF4500;
-    font-size: 13px;
-    font-weight: 500;
-    font-family: roboto;
-}
-.course-logo {
-    height: 80px;
-    border-radius: 50%;
-    box-shadow: 0px 2px 20px 1px #bbbbbb8c;
-    width: 80px;
-    margin-top: 25px;
-    margin-bottom: 5px;
-    overflow:hidden;
-}
-.course-description {
-    display:inline-block;
-    margin: 22px 10px 10px 23px;
-    font-family:roboto;
-}
-.course-duration > i, .course-fees > i, .course-start > i{
-    margin-right:5px;
-}
-.course-name{
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    text-transform: capitalize;
-    font-size: 20px;
-    font-weight: 400;
-}
-.course-duration, .course-fees, .course-start {
-    text-transform: capitalize;
-    font-size: 15px;
-    font-weight: 400;
-}
-.course-skills {
-    border-top: 2px solid #eee;
-    padding: 10px 15px;
-}
-.skills-set {
-    background: #eee;
-    border-radius: 3px 0 0 3px;
-    color:#777;
-    display: inline-block;
-    height: 26px;
-    line-height: 25px;
-    padding: 0 21px 0 11px;
-    position: relative;
-    margin: 0 9px 0px 0;
-    text-decoration: none;
-    -webkit-transition: color 0.2s;
-}
-
-.skills-set::after {
-    background: #fff;
-    border-bottom: 13px solid transparent;
-    border-left: 10px solid #eee;
-    border-top: 13px solid transparent;
-    content: "";
-    position: absolute;
-    right: 0;
-    top: 0;
-}
 ');
+$script = <<<JS
+$.ajax({
+    method: "POST",
+    url : window.location.href,
+    success: function(response) {
+            response = JSON.parse(response);
+        if(response.detail == "Not found.") {
+            console.log('cards not found');
+        } else{
+            var template = $('#course-card').html();
+            var rendered = Mustache.render(template,response.results);
+            $('#list-main').append(rendered);
+            $('.c-author').each(function() {
+                var strVal = $.trim($(this).text());
+                var lastChar = strVal.slice(-1);
+                if (lastChar == ',') { // check last character is string
+                    strVal = strVal.slice(0, -1); // trim last character
+                    $(this).text(strVal);
+                }
+            });
+        }
+    }
+});
+JS;
+$this->registerJs($script);
+$this->registerJsFile('https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.min.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
