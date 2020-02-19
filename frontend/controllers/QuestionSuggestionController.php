69087d4ef36e59a53e75c7a80a59a210d0e1e07f
Sneh Kant <sneh@dsbedutech.in>
2020-02-20 01:14:31 +0530

    suggestion box

--- frontend/controllers/QuestionSuggestionController.php
+++ frontend/controllers/QuestionSuggestionController.php
@@ -1,19 +1,35 @@
 <?php
 
 namespace frontend\controllers;
+use account\models\questionnaire\QuestionnaireViewForm;
+use common\models\OrganizationQuestionnaire;
+use common\models\QuestionnaireFieldOptions;
+use common\models\QuestionnaireFields;
 use common\models\SuggestionGroup;
 use common\models\SuggestionQuestionnaire;
+use common\models\SuggestionQuestionnaireFields;
 use common\models\Utilities;
 use Yii;
 use yii\web\Controller;
 use yii\helpers\Url;
 use yii\web\Response;
 use yii\web\HttpException;
+use yii\db\Expression;
 
 class QuestionSuggestionController extends Controller
 {
-    public function actionIndex()
+    public function actionSaveResponse()
     {
-        return $this->render('index');
+        $model = new QuestionnaireViewForm();
+        if (Yii::$app->request->isPost)
+        {
+            $data = Yii::$app->request->post('data');
+            $qidk = Yii::$app->request->post('id');
+            if ($model->saveResponse($data,$qidk)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
