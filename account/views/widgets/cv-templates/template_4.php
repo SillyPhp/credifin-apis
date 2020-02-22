2dd197d326f9afc17e15c0a2bac881bfbc0bb3b4
kulwindersohal <kulwindersohal2@iclud.com>
2020-02-22 11:40:11 +0530

    set 4 resume templates 5th template pending

--- account/views/widgets/cv-templates/template_4.php
+++ account/views/widgets/cv-templates/template_4.php
@@ -1,93 +1,91 @@
 <?php
 
 use yii\helpers\Url;
+
 ?>
 <script id="template_4" type="text/template">
-    <div class="r-parent">
-        <div class="r-head">
-            <div class="r-head-data">
-                <div class="user-name">{{name}}</div>
-                <div class="user-work">{{title}}</div>
+    <!-- Begin Paper-->
+    <div id="paper">
+        <div id="paper-mid">
+            <!-- Begin Personal Information -->
+            <div class="self">
+                <h1 class="name">{{name}}<br/>
+                    <span>Interactive Designer</span></h1>
+                <ul>
+                    {{#address}}
+                    <li class="ad">{{address}}</li>
+                    {{/address}}
+                    <li class="mail">{{email}}</li>
+                    <li class="tel">{{phone}}</li>
+                </ul>
             </div>
-        </div>
-        <div class="r-inner">
-            <div class="r-left">
-                <div class="user-photo">
-                    <img src="<?= Url::to('@eyAssets/images/pages/world-job/hong-kong.png') ?>">
+            <!-- End Personal Information -->
+            <div class="entry">
+                <h2>OBJECTIVE</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dignissim viverra nibh sed varius.
+                    Proin bibendum nunc in sem ultrices posuere. Aliquam ut aliquam lacus.</p>
+            </div>
+            <!-- End 1st Row -->
+            <!-- Begin 2nd Row -->
+            <div class="entry">
+                <h2>EDUCATION</h2>
+                {{#userEducations}}
+                <div class="content">
+                    <h3>{{from_date}} / {{to_date}}</h3>
+                    <p>{{institute}}<br/>
+                        <em>{{degree}} {{field}}</em></p>
                 </div>
-                <div class="user-profile hed">Profile</div>
-                <div class="user-detail u-txt">Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since
-                    the 1500s
+                {{/userEducations}}
+            </div>
+            <!-- End 2nd Row -->
+            <!-- Begin 3rd Row -->
+            <div class="entry">
+                <h2>EXPERIENCE</h2>
+                <div class="content">
+                    {{#userWorkExperiences}}
+                    <h3><span>{{from_date}}</span> {{#is_current}} / <span> Current</span>{{/is_current}}{{^is_current}}/{{to_date}}{{/is_current}}
+                    </h3>
+                    <p>{{company}}<br/>
+                        <em>{{title}}</em>
+                    </p>
+                    <ul class="info">
+                        <li>{{description}}</li>
+                    </ul>
+                    {{/userWorkExperiences}}
                 </div>
-                <div class="user-contact hed">contact</div>
-                <div class="user-detail">
-                    <div class="user-email u-txt">{{email}}</div>
-                    <div class="user-num u-txt">{{phone}}</div>
-                    <div class="user-address u-txt">{{address}}</div>
-                </div>
-                <div class="user-skills hed">skills</div>
-                <div class="user-detail">
+            </div>
+            <!-- End 3rd Row -->
+            <!-- Begin 4th Row -->
+            <div class="entry">
+                <h2>SKILLS</h2>
+                <div class="content">
+                    <h3>Skill set</h3>
+                    <ul class="skills">
                         {{#userSkills}}
-                        <div>{{skill}}</div>
+                        <li>{{skill}}</li>
                         {{/userSkills}}
+                    </ul>
                 </div>
-                <div class="user-interest hed">interest</div>
-                <div class="user-detail">
-                        {{#userInterests}}
-                        <div>{{interest}}</div>
-                        {{/userInterests}}
-                </div>
-                <div class="user-hobbies hed">hobbies</div>
-                <div class="user-detail">
+                <div class="content">
+                    <h3>Hobbies</h3>
+                    <ul class="skills">
                         {{#userHobbies}}
-                        <div>{{hobby}}</div>
+                        <li>{{hobby}}</li>
                         {{/userHobbies}}
+                    </ul>
+                </div>
+                <div class="content">
+                    <h3>Interest</h3>
+                    <ul class="skills">
+                        {{#userInterests}}
+                        <li>{{interest}}</li>
+                        {{/userInterests}}
+                    </ul>
                 </div>
             </div>
-            <div class="r-right">
-                <div class="user-working right-head">
-                    <span>Work Experience</span>
-                    <div class="user-inner-d">
-                        {{#userWorkExperiences}}
-                        <div class="user-year">
-                            <span>{{from_date}}</span> {{#is_current}} - <span> Current</span>{{/is_current}}{{^is_current}}-{{to_date}}{{/is_current}}|
-                            <span>{{title}}</span>|
-                            <span>{{company}}</span>
-                        </div>
-                        <div class="user-desc">{{description}}</div>
-                        {{/userWorkExperiences}}
-                    </div>
-                </div>
-                <div class="user-education right-head">
-                    <span>education</span>
-                    <div class="user-inner-d">
-                        {{#userEducations}}
-                        <div class="user-stdy">
-                            <span>{{from_date}} - {{to_date}}</span>/
-                            <span>{{degree}} {{field}}</span>
-                        </div>
-                        <div class="user-uni">{{institute}}</div>
-                        {{/userEducations}}
-                    </div>
-                </div>
-                <div class="user-languages right-head">
-                    <span>archievements</span>
-                    <div class="user-inner-d">
-                        {{#userAchievements}}
-                            <div class="langs">{{achievement}}</div>
-                        {{/userAchievements}}
-                    </div>
-                </div>
-                <div class="user-languages right-head">
-                    <span>languages</span>
-                    <div class="user-inner-d">
-                        {{#userLanguages}}
-                        <div class="langs">{{Language}}</div>
-                        {{/userLanguages}}
-                    </div>
-                </div>
-            </div>
+            <!-- End 4th Row -->
         </div>
+        <div class="clear"></div>
     </div>
+    <!-- End Paper -->
 </script>
