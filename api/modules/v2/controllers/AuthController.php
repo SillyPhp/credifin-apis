c11f8b51324d92651c33cbdd02c973e266bcb405
Ravinder Singh <ravindersaini15697@gmail.com>
2020-03-13 16:59:08 +0530

    set cors

--- api/modules/v2/controllers/AuthController.php
+++ api/modules/v2/controllers/AuthController.php
@@ -32,6 +32,15 @@ class AuthController extends ApiBaseController
                 'save-other-detail' => ['POST','OPTIONS'],
             ]
         ];
+        $behaviors['corsFilter'] = [
+            'class' => Cors::className(),
+            'cors' => [
+                'Origin' => ['https://www.myecampus.in/'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Max-Age' => 86400,
+                'Access-Control-Expose-Headers' => [],
+            ],
+        ];
         return $behaviors;
     }
 
