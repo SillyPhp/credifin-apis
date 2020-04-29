4b51657404efab7d1d0fd0fa11c83b103d8d2fb6
kulwindersohal <kulwindersohal2@iclud.com>
2020-04-29 17:08:01 +0530

    responsive issue fix

--- frontend/views/widgets/sharing-widget-new.php
+++ frontend/views/widgets/sharing-widget-new.php
@@ -3,30 +3,35 @@
 use yii\helpers\Url;
 
 ?>
-
+    <div class="sh-heading">Share :</div>
     <div class="share-social">
         <div class="facebook-share basis">
             <a href="https://www.facebook.com/sharer/sharer.php?u=" target="_blank" class="share-elem-main">
-                <span class="fb-btn"><i class="fab fa-facebook-f marg"></i> Facebook</span>
+                <span><i class="fab fa-facebook-f marg"></i> Facebook</span>
             </a>
         </div>
         <div class="twitter-share basis">
             <a href="https://twitter.com/home?status=" target="_blank" class="share-elem-main">
-                <span class="tw-btn"><i class="fab fa-twitter marg"></i> Twitter</span>
+                <span><i class="fab fa-twitter marg"></i> Twitter</span>
             </a>
         </div>
         <div class="linked-share basis">
             <a href="https://www.linkedin.com/shareArticle?mini=true&url=" target="_blank" class="share-elem-main">
-                <span class="li-btn"><i class="fab fa-linkedin-in marg"></i> LinkedIn</span>
+                <span><i class="fab fa-linkedin-in marg"></i> LinkedIn</span>
             </a>
         </div>
     </div>
 <?php
 $this->registercss('
+.sh-heading{
+	font-size: 18px;
+	padding-top: 10px;
+	font-family: roboto;
+}
 .share-social {
 	display: flex;
 	align-items: stretch;
-	margin:10px 0;
+	margin:5px 0 10px 0;
 }
 .basis{
     flex-basis:50%;
