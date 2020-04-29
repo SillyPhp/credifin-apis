e39b4adcf32cab28d8ed7031c8d6ac46409ff0d6
kulwindersohal <kulwindersohal2@iclud.com>
2020-04-29 18:04:33 +0530

    pending quiz landing page

--- frontend/views/quizzes/quiz-landing-page.php
+++ frontend/views/quizzes/quiz-landing-page.php
@@ -18,49 +18,6 @@ $this->params['header_dark'] = false;
         </div>
     </section>
 
-    <!--    <section>-->
-    <!--        <div class="container mt-20">-->
-    <!--            <div class="row">-->
-    <!--                <div class="col-md-6 col-sm-12">-->
-    <!--                    <div class="box1">-->
-    <!--                        <div class="heading-text-1">You're Welcome To Challenge Yourself</div>-->
-    <!--                        <div class="inner-text">Take a quiz from different categories to test your knowledge</div>-->
-    <!--                    </div>-->
-    <!--                </div>-->
-    <!--                <div class="col-md-6 col-sm-12">-->
-    <!--                    <div class="row mar-top">-->
-    <!--                        --><?php
-//                        $next = 0;
-//                        for ($i = 0; $i < 2; $i++) {
-//                            ?>
-    <!--                            <div class="col-md-6 col-sm-6 col-xs-6 top-categories pr-0">-->
-    <!--                                --><?php
-//                                for ($j = 0; $j < 2; $j++) {
-//                                    ?>
-    <!--                                    <div class="edu mb-15 top-categories-list">-->
-    <!--                                        <a href="--><?//= Url::to('/quizzes?type=' . $data[$next]['slug']); ?><!--">-->
-    <!--                                            <div class="newset">-->
-    <!--                                                <div class="imag">-->
-    <!--                                                    <img src="--><?//= $data[$next]['icon']; ?><!--"/>-->
-    <!--                                                </div>-->
-    <!--                                                <div class="txt-name">--><?//= $data[$next]['name']; ?><!--</div>-->
-    <!--                                            </div>-->
-    <!--                                        </a>-->
-    <!--                                    </div>-->
-    <!--                                    --><?php
-//                                    $next++;
-//                                }
-//                                ?>
-    <!--                            </div>-->
-    <!--                            --><?php
-//                        }
-//                        ?>
-    <!---->
-    <!--                    </div>-->
-    <!--                </div>-->
-    <!--            </div>-->
-    <!--    </section>-->
-
     <section>
         <div class="container">
             <div class="row">
@@ -145,6 +102,62 @@ $this->params['header_dark'] = false;
     <!--        </div>-->
     <!--    </section>-->
 
+<!--    <section>-->
+<!--        <div class="container">-->
+<!--            <div class="row">-->
+<!--                <div class="col-md-6 col-sm-6">-->
+<!--                    <div class="heading-style">Popular quiz</div>-->
+<!--                </div>-->
+<!--                <div class="col-md-6 col-sm-6 col-xs-12">-->
+<!--                    <div class="type-1">-->
+<!--                        <div>-->
+<!--                            <a href="--><?//= Url::to('/quizzes/all'); ?><!--" class="btn btn-3">-->
+<!--                                <span class="txt">--><?//= Yii::t('frontend', 'View all'); ?><!--</span>-->
+<!--                                <span class="round"><i class="fas fa-chevron-right"></i></span>-->
+<!--                            </a>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <div class="row">-->
+<!--                --><?php
+//                foreach ($quiz as $q) {
+//                    ?>
+<!--                    <div class="col-md-3 col-sm-6">-->
+<!--                        <a href="--><?//= Url::to($q['slug']); ?><!--" title="--><?//= $q['name']; ?><!--" class="quiz-box">-->
+<!--                            <div class="quiz-icon">-->
+<!--                                <img src="--><?//= Yii::$app->params->upload_directories->quiz->sharing->image . "/" . $q['sharing_image_location'] . "/" . $q['sharing_image'] ?><!--">-->
+<!--                            </div>-->
+<!--                            <div class="quiz-overlay">-->
+<!--                                <div class="quiz-text">-->
+<!--                                    <div class="quiz-creater">-->
+<!--                                        <div class="quiz-c-image"><img-->
+<!--                                                    src="--><?//= Url::to('@eyAssets/images/pages/candidate-profile/Girls2.jpg') ?><!--"-->
+<!--                                                    alt=""></div>-->
+<!--                                        <div class="quiz-c-name">alice</div>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                            <div class="quiz-inner">-->
+<!--                                <div class="quiz-title">-->
+<!--                                    --><?//= $q['name']; ?>
+<!--                                </div>-->
+<!--                                <div class="quiz-ques">-->
+<!--                                    Total Questions : --><?//= $q['cnt']; ?>
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                            <div class="take-quiz">-->
+<!--                                <span>Take Quiz</span>-->
+<!--                            </div>-->
+<!--                        </a>-->
+<!--                    </div>-->
+<!--                    --><?php
+//                }
+//                ?>
+<!--            </div>-->
+<!--        </div>-->
+<!--    </section>-->
+
     <section>
         <div class="container">
             <div class="row">
@@ -163,47 +176,6 @@ $this->params['header_dark'] = false;
                 </div>
             </div>
             <div class="row">
-                <?php
-                foreach ($quiz as $q) {
-                    ?>
-                    <div class="col-md-3 col-sm-6">
-                        <a href="<?= Url::to($q['slug']); ?>" title="<?= $q['name']; ?>" class="quiz-box">
-                            <div class="quiz-icon">
-                                <img src="<?= Yii::$app->params->upload_directories->quiz->sharing->image . "/" . $q['sharing_image_location'] . "/" . $q['sharing_image'] ?>">
-                            </div>
-                            <div class="quiz-overlay">
-                                <div class="quiz-text">
-                                    <div class="quiz-creater">
-                                        <div class="quiz-c-image"><img
-                                                    src="<?= Url::to('@eyAssets/images/pages/candidate-profile/Girls2.jpg') ?>"
-                                                    alt=""></div>
-                                        <div class="quiz-c-name">alice</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="quiz-inner">
-                                <div class="quiz-title">
-                                    <?= $q['name']; ?>
-                                </div>
-                                <div class="quiz-ques">
-                                    Total Questions : <?= $q['cnt']; ?>
-                                </div>
-                            </div>
-                            <div class="take-quiz">
-                                <span>Take Quiz</span>
-                            </div>
-                        </a>
-                    </div>
-                    <?php
-                }
-                ?>
-            </div>
-        </div>
-    </section>
-
-    <section>
-        <div class="container">
-            <div class="row">
                 <div class="col-md-4">
                     <div class="qz-main">
                         <div class="qz-logo">
@@ -218,7 +190,8 @@ $this->params['header_dark'] = false;
                             <div class="qz-creater">
                                 <div class="creator-name">
                                     <span class="creator-img">
-                                        <img src="<?= Url::to('@eyAssets/images/pages/candidate-profile/Girls2.jpg') ?>" alt="">
+                                        <img src="<?= Url::to('@eyAssets/images/pages/candidate-profile/Girls2.jpg') ?>"
+                                             alt="">
                                     </span>
                                     <span>Pooja</span>
                                 </div>
@@ -228,8 +201,11 @@ $this->params['header_dark'] = false;
                                 </div>
                             </div>
                             <div class="qz-border"></div>
-                            <div class="qz-level">Category : Marketing</div>
-                            <div class="qz-level">Price : $12</div>
+                            <div class="qz-flex">
+                                <div class="qz-cat"><span>Category :</span> Marketing</div>
+                                <div class="qz-duration"><span>Duration :</span> 5min</div>
+                            </div>
+                            <div class="qz-price"><span>Price :</span> $12</div>
                             <div class="take-quiz">
                                 <span>Take Quiz</span>
                             </div>
@@ -281,8 +257,10 @@ $this->registerCss('
 	font-family: roboto;
 }
 .qz-name {
-	font-size: 20px;
+	font-size: 22px;
 	text-transform: capitalize;
+	font-weight: 500;
+	padding-bottom: 5px;
 }
 .qz-creater {
 	display: flex;
@@ -297,8 +275,16 @@ $this->registerCss('
 	font-size: 16px;
 	text-transform: capitalize;
 }
-.qz-level {
+.qz-flex {
+    display: flex;
+    justify-content: space-between;
+}
+.qz-cat, .qz-duration, .qz-price {
 	font-size: 15px;
+}
+.qz-cat span, .qz-duration span, .qz-price span{
+    font-size: 16px;
+    font-weight: 500;
 }
 .quiz-overlay {
 	position: absolute;
@@ -333,17 +319,6 @@ $this->registerCss('
 	text-transform: capitalize;
 	font-weight: 500;
 }
-//.quiz-text {
-//  color: white;
-//  font-size: 20px;
-//  position: absolute;
-//  top: 50%;
-//  left: 50%;
-//  -webkit-transform: translate(-50%, -50%);
-//  -ms-transform: translate(-50%, -50%);
-//  transform: translate(-50%, -50%);
-//  text-align: center;
-//}
 .class-box {
     box-shadow: 0 0 7px 1px #eee;
     text-align: center;
@@ -363,12 +338,7 @@ $this->registerCss('
     font-size: 40px;
     font-family: lora;
 }
-.top-categories:nth-child(2){
-    margin-top:10px;
-}
-.top-categories:nth-child(1) .top-categories-list{
-    float:right;
-}
+
 .quiz-header{
      background:url(' . Url::to('@eyAssets/images/pages/quiz/quiz-header1.png') . ');
      min-height:450px;
@@ -434,13 +404,15 @@ $this->registerCss('
     padding: 20px 0 6px 0;
     text-align:center;
 }
-.take-quiz span{
-    border:1px solid #eee;
-    padding: 8px 13px;
-    border-radius:5px 5px 0 0;
-    font-size:13px;    
+.take-quiz span {
+	border: 1px solid #eee;
+	padding: 10px 19px;
+	border-radius: 5px 5px 0 0;
+	font-size: 15px;
+	font-family:roboto;
+	font-weight:500;
 }
-.take-quiz span:hover, .quiz-box:hover .take-quiz span{
+.take-quiz span:hover, .qz-main:hover .take-quiz span{
     color:#fff;
     background:#00a0e3;
     border-color:#00a0e3;
@@ -499,49 +471,11 @@ $this->registerCss('
     border-color:#00a0e3;
     transition:.3s ease;
 }
-.edu{
-//    text-align:center;
-//    box-shadow: 0 0 10px rgba(0,0,0,.3);
-//    width: 160px;
-    min-height: 245px;  
-//    line-height: 210px;
-    position: relative;
-    clear:both;
-}
+
 .imag{
     text-align: right;
 }
-.txt-name {
-    position: absolute;
-    line-height: 17px;
-    bottom: 10px;
-    left: 0px;
-    font-weight: 400;
-    color: #222;
-    font-family: roboto;
-    text-transform: capitalize;
-    background-color: #fff;
-    padding: 0px 5px;
-}
-.top-categories-list{
-    line-height: 30px;
-    bottom: 10px;
-    left: 10px;
-    font-weight: 400;
-    font-family:roboto;
-    text-transform:uppercase;
- }
-.popular-cate{
-    text-align:center;
-}
-.newset{
-    text-align:center;
-    max-width: 160px;
-    line-height: 210px;
-    position: relative;
-    width:100%;
-    margin-bottom:20px;
-}
+
 @media only screen and (max-width:992px){
     .mar-top{
         margin-top:20px;
