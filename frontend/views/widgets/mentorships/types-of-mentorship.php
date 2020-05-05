959811f35df1ce71f9163befdcc1b34842fe417e
kulwindersohal <kulwindersohal2@iclud.com>
2020-05-05 18:00:42 +0530

    working on types of mentorship

--- frontend/views/widgets/mentorships/types-of-mentorship.php
+++ frontend/views/widgets/mentorships/types-of-mentorship.php
@@ -12,52 +12,68 @@ use yii\helpers\Url;
             </div>
             <div class="row">
                 <div class="col-md-3 col-sm-4">
-                    <div class="ment-main">
-                        <img src="<?= Url::to('@eyAssets/images/pages/mentor/startup.png') ?>">
-                        <p class="ment-heading">startup mentorship</p>
-                    </div>
+                    <a href="/site/all-mentors">
+                        <div class="ment-main">
+                            <img src="<?= Url::to('@eyAssets/images/pages/mentor/startup.png') ?>">
+                            <p class="ment-heading">startup mentorship</p>
+                        </div>
+                    </a>
                 </div>
                 <div class="col-md-3 col-sm-4">
-                    <div class="ment-main">
-                        <img src="<?= Url::to('@eyAssets/images/pages/mentor/coding.png') ?>">
-                        <p class="ment-heading">coding mentorship</p>
-                    </div>
+                    <a href="/site/all-mentors">
+                        <div class="ment-main">
+                            <img src="<?= Url::to('@eyAssets/images/pages/mentor/coding.png') ?>">
+                            <p class="ment-heading">coding mentorship</p>
+                        </div>
+                    </a>
                 </div>
                 <div class="col-md-3 col-sm-4">
-                    <div class="ment-main">
-                        <img src="<?= Url::to('@eyAssets/images/pages/mentor/design.png') ?>">
-                        <p class="ment-heading">Design mentorship</p>
-                    </div>
+                    <a href="/site/all-mentors">
+                        <div class="ment-main">
+                            <img src="<?= Url::to('@eyAssets/images/pages/mentor/learning-education.png') ?>">
+                            <p class="ment-heading">learning & education mentorship</p>
+                        </div>
+                    </a>
                 </div>
                 <div class="col-md-3 col-sm-4">
-                    <div class="ment-main">
-                        <img src="<?= Url::to('@eyAssets/images/pages/mentor/blogging.png') ?>">
-                        <p class="ment-heading">blogging mentorship</p>
-                    </div>
+                    <a href="/site/all-mentors">
+                        <div class="ment-main">
+                            <img src="<?= Url::to('@eyAssets/images/pages/mentor/design.png') ?>">
+                            <p class="ment-heading">Design mentorship</p>
+                        </div>
+                    </a>
                 </div>
                 <div class="col-md-3 col-sm-4">
-                    <div class="ment-main">
-                        <img src="<?= Url::to('@eyAssets/images/pages/mentor/career-choice.png') ?>">
-                        <p class="ment-heading">career choice mentorship</p>
-                    </div>
+                    <a href="/site/all-mentors">
+                        <div class="ment-main">
+                            <img src="<?= Url::to('@eyAssets/images/pages/mentor/blogging.png') ?>">
+                            <p class="ment-heading">blogging mentorship</p>
+                        </div>
+                    </a>
                 </div>
                 <div class="col-md-3 col-sm-4">
-                    <div class="ment-main">
-                        <img src="<?= Url::to('@eyAssets/images/pages/mentor/learning-education.png') ?>">
-                        <p class="ment-heading">learning & education mentorship</p>
-                    </div>
+                    <a href="/site/all-mentors">
+                        <div class="ment-main">
+                            <img src="<?= Url::to('@eyAssets/images/pages/mentor/career-choice.png') ?>">
+                            <p class="ment-heading">career choice mentorship</p>
+                        </div>
+                    </a>
                 </div>
                 <div class="col-md-3 col-sm-4">
-                    <div class="ment-main">
-                        <img src="<?= Url::to('@eyAssets/images/pages/mentor/companies.png') ?>">
-                        <p class="ment-heading">companies mentorship</p>
-                    </div>
+                    <a href="/site/all-mentors">
+                        <div class="ment-main">
+                            <img src="<?= Url::to('@eyAssets/images/pages/mentor/companies.png') ?>">
+                            <p class="ment-heading">companies mentorship</p>
+                        </div>
+                    </a>
                 </div>
                 <div class="col-md-3 col-sm-4">
-                    <div class="ment-main">
-                        <img src="<?= Url::to('@eyAssets/images/pages/mentor/digital-mentorship.png') ?>">
-                        <p class="ment-heading">digital marketing mentorship</p>
-                    </div>
+                    <a href="/site/all-mentors">
+                        <div class="ment-main">
+                            <img src="<?= Url::to('@eyAssets/images/pages/mentor/digital-mentorship.png') ?>">
+                            <p class="ment-heading">digital marketing mentorship</p>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
@@ -85,6 +101,7 @@ $this->registerCss('
     font-family: lora;
     padding: 5px;
     margin:0;
+    height:65px;
 }
 .ment-main p{
     text-transform: capitalize;
