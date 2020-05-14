07fac6d95d42a733bfb7ecd4ed1c1c17da081c07
Sneh Kant <sneh@dsbedutech.in>
2020-05-14 15:43:23 +0530

    quiz topics

--- account/controllers/QuizController.php
+++ account/controllers/QuizController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace account\controllers;
-
+use account\models\quiz\QuizModel;
+use common\models\Categories;
+use common\models\QuizTopics;
+use common\models\Topics;
 use Yii;
 use yii\helpers\Url;
 use yii\web\Controller;
+use yii\web\Response;
+use common\models\Utilities;
 class QuizController extends Controller
 {
     public function actionDashboard(){
@@ -35,4 +40,64 @@ class QuizController extends Controller
             }
         }
     }
+
+    public function actionAddSubject()
+    {
+        if (Yii::$app->request->isAjax && Yii::$app->request->isPost) {
+            Yii::$app->response->format = Response::FORMAT_JSON;
+            $d = Yii::$app->request->post('data');
+            $model = new QuizModel();
+            $res = $model->addSub($d);
+            if ($res)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 }
+
+    private function _data($type)
+    {
+       return Categories::find()
+            ->alias('a')
+            ->select(['a.name', 'b.assigned_category_enc_id as id'])
+            ->joinWith(['assignedCategories b'],false)
+            ->andWhere([
+                'b.assigned_to' => $type,
+                'b.parent_enc_id'=>null
+            ])
+            ->andWhere([
+                'or',
+                ['=', 'b.status', 'Approved'],
+                ['b.created_by' => Yii::$app->user->identity->user_enc_id]
+            ])
+            ->asArray()
+            ->all();
+    }
+
+    private function _topics($is_rec)
+    {
+        return Topics::find()
+            ->where(['assigned_to'=>'quiz','is_deleted'=>0])
+            ->andWhere([
+                'or',
+                ['=', 'status', 1],
+                ['created_by' => Yii::$app->user->identity->user_enc_id]
+            ])
+            ->andWhere(['is_recommend'=>$is_rec])
+            ->asArray()->all();
+    }
+
+    public function actionFaker()
+    {
+        $model = new Topics();
+        $utilitiesModel = new Utilities();
+        $utilitiesModel->variables['string'] = time() . rand(100, 100000);
+        $model->topic_enc_id = $utilitiesModel->encrypt();
+    }
+
+}
+
