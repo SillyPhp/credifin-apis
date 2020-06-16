2238d4d89eb23887fb59d6737dc70fee7c152496
Pritam Kumar <mr.pritamkumar786@gmail.com>
2020-06-16 18:08:35 +0530

    show images for view templates

--- account/controllers/JobsController.php
+++ account/controllers/JobsController.php
@@ -1557,7 +1557,7 @@ class JobsController extends Controller
         if (!empty(Yii::$app->user->identity->organization)) {
             $application = \common\models\ApplicationTemplates::find()
                 ->alias('a')
-                ->select(['a.application_enc_id', 'a.title', 'zz.name as cat_name'])
+                ->select(['a.application_enc_id', 'a.title', 'zz.name as cat_name','zz.icon_png'])
                 ->joinWith(['title0 z' => function ($z) {
                     $z->joinWith(['categoryEnc zz']);
                 }], false)
