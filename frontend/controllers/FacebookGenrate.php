1a36d0a3f4f0eb1c3b2e0a46688f0e3b1063389b
Sneh Kant <sneh@dsbedutech.in>
2020-06-25 12:50:27 +0530

    fb auth login

---
+++ frontend/controllers/FacebookGenrate.php
@@ -0,0 +1,14 @@
+<?php
+namespace frontend\controllers;
+use Yii;
+use yii\web\Controller;
+use yii\helpers\Url;
+use yii\web\Response;
+
+class FacebookGenrate  extends  Controller
+{
+    public function actionTest()
+    {
+        return 12;
+    }
+}
