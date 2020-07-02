6f33f2593173e68642bb512fdb1ccf08148b7f52
Tarandeep Singh <er.tarandeep@yahoo.com>
2020-07-02 14:06:10 +0530

    Changed URL to https

--- frontend/views/accounts/login.php
+++ frontend/views/accounts/login.php
@@ -70,7 +70,7 @@ $loginForm->field($loginFormModel, 'password')->passwordInput([
     </div>
     <div class="form-group mt-10">
         <?=
-        \yii\authclient\widgets\AuthChoice::widget([ 'baseAuthUrl' => [Url::to('site/auth', 'https')], 'popupMode' => false, ])
+        \yii\authclient\widgets\AuthChoice::widget([ 'baseAuthUrl' => ['site/auth'], 'popupMode' => false, ])
         ?>
     </div>
     <div class="separator pb-10 text-black">
