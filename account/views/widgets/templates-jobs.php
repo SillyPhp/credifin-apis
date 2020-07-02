577e5b77a89fd74e9a0fbb963c17a2e15be9e5da
kulwindersohal <kulwindersohal2@iclud.com>
2020-07-02 16:38:33 +0530

    template cards

--- account/views/widgets/templates-jobs.php
+++ account/views/widgets/templates-jobs.php
@@ -5,6 +5,7 @@ use yii\helpers\Url;
 <div class="temp-job-main">
     <div class="temp-head">Job Templates</div>
     <div class="temp-card">
+        <i class="fa fa-files-o" title="clone"></i>
         <img src="<?= Url::to('@eyAssets/images/pages/dashboard/quick-job-icon1.png') ?>">
         <h3>Web developer</h3>
     </div>
@@ -25,6 +26,22 @@ $this->registercss('
 	box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
 	padding: 14px 5px;
 	border-radius: 8px;
+	margin-bottom:20px;
+	position:relative;
+}
+.temp-card i {
+	position: absolute;
+	left: 5px;
+	top: 0px;
+	font-weight: bold;
+	color: #8e8e8e;
+	padding: 5px;
+	border-radius: 0 0px 6px 6px;
+	transition: all .3s;
+}
+.temp-card:hover.temp-card > i{
+    background-color:#00a0e3;
+    color:#fff;
 }
 .temp-card img{
 	height: 70px;
