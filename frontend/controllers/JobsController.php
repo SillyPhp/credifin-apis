0cd02ec4798cee0e3d2039fe4c750943371c717a
Tarandeep Singh <er.tarandeep@yahoo.com>
2020-07-22 15:43:18 +0530

    XML Feeds function

--- frontend/controllers/JobsController.php
+++ frontend/controllers/JobsController.php
@@ -450,21 +450,21 @@ class JobsController extends Controller
             ->orderBy(new Expression('rand()'))
             ->limit(6);
 
-        $popular_videos =  $related_videos
-               ->joinWith(['assignedCategoryEnc a'=>function($a){
-                   $a->joinWith(['parentEnc a1'],false);
-                   $a->joinWith(['categoryEnc a2'],false);
-                   $a->joinWith(['employerApplications b' => function($b){
-                       $b->joinWith(['designationEnc c'],false);
-                   }],false);
-               }],false)
-             ->andFilterWhere(['or',
-                 ['like','c.designation',$desi_name],
-                 ['like','a1.name',$pro_name],
-                 ['like','a2.name',$cat_name],
-             ])
-               ->asArray()->all();
-        if(count($popular_videos) < 6) {
+        $popular_videos = $related_videos
+            ->joinWith(['assignedCategoryEnc a' => function ($a) {
+                $a->joinWith(['parentEnc a1'], false);
+                $a->joinWith(['categoryEnc a2'], false);
+                $a->joinWith(['employerApplications b' => function ($b) {
+                    $b->joinWith(['designationEnc c'], false);
+                }], false);
+            }], false)
+            ->andFilterWhere(['or',
+                ['like', 'c.designation', $desi_name],
+                ['like', 'a1.name', $pro_name],
+                ['like', 'a2.name', $cat_name],
+            ])
+            ->asArray()->all();
+        if (count($popular_videos) < 6) {
             $limit = 6 - count($popular_videos);
             $xyz = LearningVideos::find()
                 ->alias('z')
@@ -475,16 +475,15 @@ class JobsController extends Controller
             $xz = $xyz->asArray()->all();
             $popular_videos = array_merge($popular_videos, $xz);
         }
-           if (empty($popular_videos) )
-           {
-               $xyz = LearningVideos::find()
-                   ->alias('z')
-                   ->where(['z.is_deleted' => 0,
-                       'z.status' => 1])
-                   ->orderBy(new Expression('rand()'))
-                   ->limit(6);
-               $popular_videos = $xyz->asArray()->all();
-           }
+        if (empty($popular_videos)) {
+            $xyz = LearningVideos::find()
+                ->alias('z')
+                ->where(['z.is_deleted' => 0,
+                    'z.status' => 1])
+                ->orderBy(new Expression('rand()'))
+                ->limit(6);
+            $popular_videos = $xyz->asArray()->all();
+        }
 
         return $this->render('/employer-applications/detail', [
             'application_details' => $application_details,
@@ -1403,6 +1402,56 @@ class JobsController extends Controller
                 }
             }
         }
+    }
+
+    public function actionXmlFeeds($offset = 0, $limit = 3000, $type = 'Jobs')
+    {
+        $params = [];
+        $type = 'Jobs';
+        $params['limit'] = $limit;
+        $params['offset'] = $offset;
+        $params['type'] = $type;
+        $obj = new ApplicationFeeds();
+        $objects = $obj->getApplications($params);
+        $dom = new \DOMDocument();
+        $dom->encoding = 'utf-8';
+        $dom->xmlVersion = '1.0';
+        $dom->formatOutput = true;
+        $base_path = Url::to('@rootDirectory/files/xml');
+        $xml_file_name = $type . '-Feeds.xml';
+        $root = $dom->createElement('jobs');
+        $i = time() . rand(100, 100000);
+        foreach ($objects as $object) {
+            $node = $dom->createElement('job');
+            $attr_node_id = new \DOMAttr('id', $i++);
+            $node->setAttributeNode($attr_node_id);
+            $child_node_slug = $dom->createElement('link', '<![CDATA[' . $object['link'] . ']]>');
+            $node->appendChild($child_node_slug);
+            $child_node_slug = $dom->createElement('name', '<![CDATA[' . $object['name'] . ']]>');
+            $node->appendChild($child_node_slug);
+            $child_node_slug = $dom->createElement('region', '<![CDATA[' . $object['city'] . ', ' . $object['country'] . ']]>');
+            $node->appendChild($child_node_slug);
+            $child_node_slug = $dom->createElement('salary', '<![CDATA[' . $object['salary'] . ']]>');
+            $node->appendChild($child_node_slug);
+            $child_node_slug = $dom->createElement('description', '<![CDATA[' . $object['description'] . '<br>' . $object['education_req'] . ']]>');
+            $node->appendChild($child_node_slug);
+            $child_node_slug = $dom->createElement('apply_url', '<![CDATA[' . $object['link'] . ']]>');
+            $node->appendChild($child_node_slug);
+            $child_node_slug = $dom->createElement('company', '<![CDATA[' . $object['organization_name'] . ']]>');
+            $node->appendChild($child_node_slug);
+            $child_node_slug = $dom->createElement('pubdate', '<![CDATA[' . $object['pubdate'] . ']]>');
+            $node->appendChild($child_node_slug);
+            $child_node_slug = $dom->createElement('updated', '<![CDATA[' . $object['updated'] . ']]>');
+            $node->appendChild($child_node_slug);
+            $child_node_slug = $dom->createElement('expire', '<![CDATA[' . $object['expire'] . ']]>');
+            $node->appendChild($child_node_slug);
+            $child_node_slug = $dom->createElement('type', '<![CDATA[' . $object['type'] . ']]>');
+            $node->appendChild($child_node_slug);
+            $root->appendChild($node);
+        }
+        $dom->appendChild($root);
+        $dom->save($base_path . DIRECTORY_SEPARATOR . $xml_file_name);
+        echo "$xml_file_name has been successfully created";
     }
 
 }
