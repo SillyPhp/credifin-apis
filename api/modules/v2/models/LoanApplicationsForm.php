c218f873153e724411215761fd0a4295b5886369
Ravinder Singh <ravindersaini15697@gmail.com>
2020-07-28 12:08:56 +0530

    minor change in quiz controller and save loan application

--- api/modules/v2/models/LoanApplicationsForm.php
+++ api/modules/v2/models/LoanApplicationsForm.php
@@ -33,7 +33,7 @@ class LoanApplicationsForm extends LoanApplications
     {
         $application_fee = OrganizationFeeAmount::find()
             ->select(['application_fee_amount_enc_id', 'amount', 'gst'])
-            ->where(['organization_enc_id' => $college_id, 'loan_type_enc_id' => $this->loan_type_enc_id, 'status' => 1])
+            ->where(['organization_enc_id' => $college_id, 'loan_type_enc_id' => 'Y682Wx8amy3qnRdPAeddl1JKzpXQPb', 'status' => 1])
             ->asArray()
             ->one();
 
@@ -43,9 +43,9 @@ class LoanApplicationsForm extends LoanApplications
             $utilitiesModel->variables['string'] = time() . rand(100, 100000);
             $this->loan_app_enc_id = $utilitiesModel->encrypt();
             $this->college_enc_id = $college_id;
-            if (!empty($application_fee)) {
-                $this->status = 4;
-            }
+//            if (!empty($application_fee)) {
+//                $this->status = 4;
+//            }
             $this->source = 'Mec';
             $this->created_by = $userId;
             $this->created_on = date('Y-m-d H:i:s');
@@ -94,50 +94,50 @@ class LoanApplicationsForm extends LoanApplications
                 }
             }
             if (!empty($application_fee)) {
-
                 $amount = $application_fee['amount'];
                 $gst = $application_fee['gst'];
                 $percentage = ($amount * $gst) / 100;
                 $total_amount = $amount + $percentage;
-                $application_fee['total_amount'] = $total_amount;
+            } else {
+                $total_amount = 500;
+                $gst = 0;
+            }
 
-                $args = [];
-                $args['amount'] = $application_fee['total_amount'];
-                $args['currency'] = "INR";
-                $args['email'] = $this->email;
-                $args['contact'] = $this->phone;
 
-                $request = curl_init('http://www.sneh.eygb.me/api/v3/payment-request/get-token');
-                curl_setopt($request, CURLOPT_POST, true);
-                curl_setopt($request, CURLOPT_POSTFIELDS, $args);
-                curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
-                $response = json_decode(curl_exec($request), true);
+            $args = [];
+            $args['amount'] = $total_amount;
+            $args['currency'] = "INR";
+            $args['email'] = $this->email;
+            $args['contact'] = $this->phone;
 
-                if (isset($response['status']) && $response['status'] == 'created') {
-                    $token = $response['id'];
-                    $loan_payment = new EducationLoanPayments();
-                    $utilitiesModel->variables['string'] = time() . rand(100, 100000);
-                    $loan_payment->education_loan_payment_enc_id = $utilitiesModel->encrypt();
-                    $loan_payment->college_enc_id = $college_id;
-                    $loan_payment->loan_app_enc_id = $this->loan_app_enc_id;
-                    $loan_payment->payment_token = $token;
-                    $loan_payment->payment_amount = $application_fee['total_amount'];
-                    $loan_payment->payment_gst = $application_fee['gst'];
-                    $loan_payment->created_by = $userId;
-                    $loan_payment->created_on = date('Y-m-d H:i:s');
-                    if (!$loan_payment->save()) {
-                        print_r($loan_payment->getErrors());
-                        die();
-                        $transaction->rollBack();
-                        return false;
-                    } else {
-                        $transaction->commit();
-                        $data = [];
-                        $data['loan_app_enc_id'] = $this->loan_app_enc_id;
-                        $data['education_loan_payment_enc_id'] = $loan_payment->education_loan_payment_enc_id;
-                        $data['payment_id'] = $loan_payment->payment_token;
-                        return $data;
-                    }
+            $request = curl_init('http://www.sneh.eygb.me/api/v3/payment-request/get-token');
+            curl_setopt($request, CURLOPT_POST, true);
+            curl_setopt($request, CURLOPT_POSTFIELDS, $args);
+            curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+            $response = json_decode(curl_exec($request), true);
+
+            if (isset($response['status']) && $response['status'] == 'created') {
+                $token = $response['id'];
+                $loan_payment = new EducationLoanPayments();
+                $utilitiesModel->variables['string'] = time() . rand(100, 100000);
+                $loan_payment->education_loan_payment_enc_id = $utilitiesModel->encrypt();
+                $loan_payment->college_enc_id = $college_id;
+                $loan_payment->loan_app_enc_id = $this->loan_app_enc_id;
+                $loan_payment->payment_token = $token;
+                $loan_payment->payment_amount = $total_amount;
+                $loan_payment->payment_gst = $gst;
+                $loan_payment->created_by = $userId;
+                $loan_payment->created_on = date('Y-m-d H:i:s');
+                if (!$loan_payment->save()) {
+                    $transaction->rollBack();
+                    return false;
+                } else {
+                    $transaction->commit();
+                    $data = [];
+                    $data['loan_app_enc_id'] = $this->loan_app_enc_id;
+                    $data['education_loan_payment_enc_id'] = $loan_payment->education_loan_payment_enc_id;
+                    $data['payment_id'] = $loan_payment->payment_token;
+                    return $data;
                 }
             }
             if ($this->_flag) {
