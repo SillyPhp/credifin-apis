0e9d6c7a098a55a2022f33e1ab031632c24d1685
Nitesh Sharma <rs.nitesh01@gmail.com>
2020-08-05 18:02:06 +0530

    Webinar speakers list api done..and status update on condition for Erexx

--- api/modules/v2/controllers/WebinarsController.php
+++ api/modules/v2/controllers/WebinarsController.php
@@ -48,35 +48,37 @@ class WebinarsController extends ApiBaseController
     public function actionSpeakers()
     {
 //        if ($user = $this->isAuthorized()) {
-            $webSpeaker = Speakers::find()
-                ->alias('a')
-                ->select(['a.speaker_enc_id',
-                    'a.unclaimed_org_id',
-                    'a.designation_enc_id',
-                    'b.designation',
-                    'CONCAT(f.first_name, " ", f.last_name) fullname',
-                    'f.email', 'f.phone',
-                    'f.image','f.image_location',
-                    'f.description',
-                    'f.facebook','f.twitter','f.instagram','f.linkedin',
-                    'c.logo org_logo','c.logo_location org_logo_location',
-                    'REPLACE(c.name, "&amp;", "&") as org_name',
-                ])
-                ->where(['a.is_deleted' => 0])
-                ->joinWith(['designationEnc b'],false)
-                ->joinWith(['unclaimedOrg c'],false)
-                ->joinWith(['speakerExpertises d' => function($d){
-                    $d->select(['d.speaker_enc_id','d.skill_enc_id','e.skill']);
-                    $d->joinWith(['skillEnc e'],false);
-                }])
-                ->joinWith(['userEnc f'],false)
-                ->distinct()
-                ->asArray()
-                ->orderBy(['a.created_on' => SORT_DESC])
-                ->all();
+        $webSpeaker = Speakers::find()
+            ->alias('a')
+            ->select(['a.speaker_enc_id',
+                'a.unclaimed_org_id',
+                'a.designation_enc_id',
+                'b.designation',
+                'CONCAT(f.first_name, " ", f.last_name) fullname',
+                'f.email', 'f.phone',
+                'f.image', 'f.image_location',
+                'f.description',
+                'f.facebook', 'f.twitter', 'f.instagram', 'f.linkedin',
+                'c.logo org_logo', 'c.logo_location org_logo_location',
+                'REPLACE(c.name, "&amp;", "&") as org_name',
+            ])
+            ->where(['a.is_deleted' => 0])
+            ->joinWith(['designationEnc b'], false)
+            ->joinWith(['unclaimedOrg c'], false)
+            ->joinWith(['speakerExpertises d' => function ($d) {
+                $d->select(['d.speaker_enc_id', 'd.skill_enc_id', 'e.skill']);
+                $d->joinWith(['skillEnc e'], false);
+            }])
+            ->joinWith(['userEnc f'], false)
+            ->distinct()
+            ->asArray()
+            ->orderBy(['a.created_on' => SORT_DESC])
+            ->all();
 
-
-            return $this->response(200, $webSpeaker);
+        if ($webSpeaker) {
+            return $this->response(200, ['status' => 200, 'data' => $webSpeaker]);
+        }
+        return $this->response(404, ['status' => 404, 'message' => 'No detail found']);
 //        }
     }
 }
