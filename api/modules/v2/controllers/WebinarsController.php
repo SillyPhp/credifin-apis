0c3e000c5c008fe950de42e72734be6041b89bf6
Ravinder Singh <ravindersaini15697@gmail.com>
2020-08-06 16:01:52 +0530

    speaker detail api done

--- api/modules/v2/controllers/WebinarsController.php
+++ api/modules/v2/controllers/WebinarsController.php
@@ -21,7 +21,8 @@ class WebinarsController extends ApiBaseController
 
         $behaviors['authenticator'] = [
             'except' => [
-                'speakers'
+                'speakers',
+                'speaker-detail'
             ],
             'class' => HttpBearerAuth::className()
         ];
@@ -30,6 +31,7 @@ class WebinarsController extends ApiBaseController
             'class' => VerbFilter::className(),
             'actions' => [
                 'speakers' => ['POST', 'OPTIONS'],
+                'speaker-detail' => ['POST', 'OPTIONS'],
             ]
         ];
 
@@ -47,8 +49,8 @@ class WebinarsController extends ApiBaseController
 
     public function actionSpeakers()
     {
-//        if ($user = $this->isAuthorized()) {
         $webSpeaker = Speakers::find()
+            ->distinct()
             ->alias('a')
             ->select(['a.speaker_enc_id',
                 'a.unclaimed_org_id',
@@ -56,10 +58,10 @@ class WebinarsController extends ApiBaseController
                 'b.designation',
                 'CONCAT(f.first_name, " ", f.last_name) fullname',
                 'f.email', 'f.phone',
-                'f.image', 'f.image_location',
+                'CASE WHEN f.image IS NOT NULL THEN CONCAT("' . Url::to(Yii::$app->params->upload_directories->users->image, 'https') . '", f.image_location, "/", f.image) END image',
+                'CASE WHEN c.logo IS NOT NULL THEN CONCAT("' . Url::to(Yii::$app->params->upload_directories->organizations->logo, 'https') . '", c.logo_location, "/", c.logo) ELSE CONCAT("https://ui-avatars.com/api/?name=", c.name, "&size=200&rounded=false&background=", REPLACE(c.initials_color, "#", ""), "&color=ffffff") END logo',
                 'f.description',
                 'f.facebook', 'f.twitter', 'f.instagram', 'f.linkedin',
-                'c.logo org_logo', 'c.logo_location org_logo_location',
                 'REPLACE(c.name, "&amp;", "&") as org_name',
             ])
             ->where(['a.is_deleted' => 0])
@@ -70,15 +72,53 @@ class WebinarsController extends ApiBaseController
                 $d->joinWith(['skillEnc e'], false);
             }])
             ->joinWith(['userEnc f'], false)
-            ->distinct()
-            ->asArray()
             ->orderBy(['a.created_on' => SORT_DESC])
+            ->asArray()
             ->all();
 
         if ($webSpeaker) {
             return $this->response(200, ['status' => 200, 'data' => $webSpeaker]);
         }
         return $this->response(404, ['status' => 404, 'message' => 'No detail found']);
-//        }
+    }
+
+    public function actionSpeakerDetail()
+    {
+        $params = Yii::$app->request->post();
+        if (!isset($params['speaker_enc_id']) && empty($params['speaker_enc_id'])) {
+            return $this->response(422, ['status' => 422, 'message' => 'missing information']);
+        }
+
+        $speaker = Speakers::find()
+            ->alias('a')
+            ->select(['a.speaker_enc_id',
+                'a.unclaimed_org_id',
+                'a.designation_enc_id',
+                'b.designation',
+                'CONCAT(f.first_name, " ", f.last_name) fullname',
+                'f.email', 'f.phone',
+                'CASE WHEN f.image IS NOT NULL THEN CONCAT("' . Url::to(Yii::$app->params->upload_directories->users->image, 'https') . '", f.image_location, "/", f.image) END image',
+                'CASE WHEN c.logo IS NOT NULL THEN CONCAT("' . Url::to(Yii::$app->params->upload_directories->organizations->logo, 'https') . '", c.logo_location, "/", c.logo) ELSE CONCAT("https://ui-avatars.com/api/?name=", c.name, "&size=200&rounded=false&background=", REPLACE(c.initials_color, "#", ""), "&color=ffffff") END logo',
+                'f.description',
+                'f.facebook', 'f.twitter', 'f.instagram', 'f.linkedin',
+                'REPLACE(c.name, "&amp;", "&") as org_name',
+            ])
+            ->where(['a.is_deleted' => 0, 'a.speaker_enc_id' => $params['speaker_enc_id']])
+            ->joinWith(['designationEnc b'], false)
+            ->joinWith(['unclaimedOrg c'], false)
+            ->joinWith(['speakerExpertises d' => function ($d) {
+                $d->select(['d.speaker_enc_id', 'd.skill_enc_id', 'e.skill']);
+                $d->joinWith(['skillEnc e'], false);
+            }])
+            ->joinWith(['userEnc f'], false)
+            ->asArray()
+            ->one();
+
+        if ($speaker) {
+            return $this->response(200, ['status' => 200, 'data' => $speaker]);
+        }
+
+        return $this->response(404, ['status' => 404, 'message' => 'not found']);
+
     }
 }
