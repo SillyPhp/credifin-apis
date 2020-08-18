6aa2d69899d1e133b5b108e4933f6229cab78fc0
Sneh Kant <sneh@dsbedutech.in>
2020-08-18 16:39:07 +0530

    EducationLoans updated css changes and styles sheets , ui fixes

--- frontend/controllers/EducationLoansController.php
+++ frontend/controllers/EducationLoansController.php
@@ -2,9 +2,12 @@
 
 namespace frontend\controllers;
 
+use common\models\BusinessActivities;
 use common\models\Organizations;
+use common\models\UnclaimedOrganizations;
 use frontend\models\EducationalLoans;
 use Yii;
+use yii\helpers\Url;
 use yii\web\Controller;
 
 
@@ -26,14 +29,27 @@ class EducationLoansController extends Controller
         return $this->render("education-loan-index");
     }
     public function actionApply(){
-        $model = new EducationalLoans();
-        return $this->render('apply',[
-            'EducationalLoan' => $model,
-        ]);
+        $type = ['College','School','Educational Institute'];
+        $params1 = (new \yii\db\Query())
+            ->select(['REPLACE(name, "&amp;", "&") as name','a.organization_enc_id','b.business_activity'])
+            ->from(UnclaimedOrganizations::tableName() . 'as a')
+            ->leftJoin(BusinessActivities::tableName() . 'as b', 'b.business_activity_enc_id = a.organization_type_enc_id')
+            ->andWhere(['is_deleted' => 0])
+            ->andWhere(['in', 'business_activity', $type]);
+
+        $params2 = (new \yii\db\Query())
+            ->select(['REPLACE(name, "&amp;", "&") as name','a.organization_enc_id','b.business_activity'])
+            ->from(Organizations::tableName() . 'as a')
+            ->innerJoin(BusinessActivities::tableName() . 'as b', 'b.business_activity_enc_id = a.business_activity_enc_id')
+            ->andWhere(['is_deleted' => 0])
+            ->andWhere(['in', 'business_activity', $type]);
+
+        $data_collection = $params1->union($params2)->all();
+        return $this->render('apply-general-loan-form',['data_collection'=>$data_collection]);
     }
     public function actionApplyLoan($id)
-    {
-        $this->layout = 'blank-layout';
+    { 
+        $this->layout = 'widget-layout';
         $wid = Organizations::find()
             ->select(['organization_enc_id'])
             ->where(['organization_enc_id'=>$id])
