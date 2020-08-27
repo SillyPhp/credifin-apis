f8853a61f5c4a1f5ca41ffcdba6bc8d18439fab8
Sneh <snehkant93@gmail.com>
2020-08-28 00:23:31 +0530

    payment handler for status updates ,api and console 1 api to get information about payments ( u can use it this api anywhere to get information about a particular token)
    2 console commmands to automatically trigger the payments status aftr some intrvl of payment (made between 6-7 days to reduce repeative payment check)

--- frontend/controllers/TestCacheController.php
+++ frontend/controllers/TestCacheController.php
@@ -2,31 +2,11 @@
 
 namespace frontend\controllers;
 use common\models\EducationLoanPayments;
+use common\models\ReviewedApplications;
 use yii\web\Controller;
 use common\models\Utilities;
 
 class TestCacheController extends Controller
 {
-    public function actionStatusUpdate()
-    {
-        $paymentsModes = ['pending','failed','cancelled',NULL,''];
-        $applications = EducationLoanPayments::find()
-                        ->select(['education_loan_payment_enc_id','loan_app_enc_id','payment_status','created_on'])
-                        ->andWhere([
-                            'or',
-                            ['payment_status'=>'pending'],
-                            ['payment_status'=>'failed'],
-                            ['payment_status'=>'cancelled'],
-                            ['payment_status'=>NULL],
-                            ['payment_status'=>''],
-                        ])
-                        ->asArray()
-                        ->all();
-
-        foreach ($applications as $app)
-        {
-
-        }
-    }
 
 }
