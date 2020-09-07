e6d890e863280bc968230ceef40461a26aaa8f75
Nitesh Sharma <rs.nitesh01@gmail.com>
2020-09-07 21:04:11 +0530

    loan retry payment query test again...

--- api/modules/v3/controllers/EducationLoanController.php
+++ api/modules/v3/controllers/EducationLoanController.php
@@ -187,8 +187,6 @@ class EducationLoanController extends ApiBaseController
         $loan_payment->payment_status = $status;
         $loan_payment->payment_id = $payment_id;
         $loan_payment->payment_gst = $gst;
-        print_r($params);
-        print_r($loan_payment);exit();
         $loan_payment->created_on = date('Y-m-d H:i:s');
         if ($loan_payment->save()) {
             return $this->response(200, ['status' => 200, 'message' => 'success']);
