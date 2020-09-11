4f7dad19b40da0ecbe888dc070018a8d0187c544
Ajay Juneja <ajayjuneja52@gmail.com>
2020-09-11 13:10:57 +0530

    header and seo code

--- frontend/controllers/WabinarsController.php
+++ frontend/controllers/WabinarsController.php
@@ -10,6 +10,14 @@ use yii\httpclient\Client;
 
 class WebinarsController extends Controller
 {
+
+    public function beforeAction($action)
+    {
+        Yii::$app->view->params['sub_header'] = Yii::$app->header->getMenuHeader(Yii::$app->controller->id);
+        Yii::$app->seo->setSeoByRoute(ltrim(Yii::$app->request->url, '/'), $this);
+        return parent::beforeAction($action);
+    }
+
    public function actionSendMessage()
    {
        $ch = curl_init("https://rest.messengerpeople.com/api/v11/chat");
