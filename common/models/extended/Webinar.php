a66677d32a1f2dafc36b197a592f521b7c673ae2
Ravinder Singh <ravindersaini15697@gmail.com>
2020-09-15 12:03:03 +0530

    webinar code done

--- common/models/extended/Webinar.php
+++ common/models/extended/Webinar.php
@@ -4,6 +4,7 @@
 namespace common\models\extended;
 
 use common\models\WebinarEvents;
+use common\models\WebinarSpeakers;
 use yii\helpers\Url;
 use yii\helpers\ArrayHelper;
 use Yii;
@@ -60,42 +61,8 @@ class Webinar extends \common\models\Webinar
                 'a.seats',
                 'a.slug',
                 'a.availability',
-                'COUNT(DISTINCT(c3.assigned_speaker_enc_id)) speaker_count'
             ])
             ->joinWith(['assignedWebinarTos b'], false)
-            ->joinWith(['webinarEvents c' => function ($c) {
-                $c->select([
-                    'c.event_enc_id',
-                    'c.webinar_enc_id',
-                    'c.title event_title',
-                    'c.description event_description',
-                    'c.start_datetime',
-                    'c.duration',
-                    'c.status',
-                    'c.session_enc_id',
-                ]);
-                $c->joinWith(['webinarSpeakers c3' => function ($c2) {
-                    $c2->select([
-                        'c3.webinar_event_enc_id',
-                        'c3.speaker_enc_id',
-                        'c6.designation',
-                        'CONCAT(c5.first_name," ",c5.last_name) full_name',
-                        'CASE WHEN c5.image IS NOT NULL THEN CONCAT("' . Url::to(Yii::$app->params->upload_directories->users->image, 'https') . '", c5.image_location, "/", c5.image) ELSE CONCAT("https://ui-avatars.com/api/?name=", c5.first_name, "&size=200&rounded=false&background=", REPLACE(c5.initials_color, "#", ""), "&color=ffffff") END image',
-                        'c5.facebook',
-                        'c5.twitter',
-                        'c5.linkedin',
-                        'c5.instagram',
-                    ]);
-                    $c2->joinWith(['speakerEnc c4' => function ($c4) {
-                        $c4->joinWith(['userEnc c5']);
-                        $c4->joinWith(['designationEnc c6']);
-                    }], false);
-                    $c2->onCondition(['c3.is_deleted' => 0]);
-                }]);
-                $c->onCondition(['c.is_deleted' => 0]);
-                $c->orderBy(['c.start_datetime' => SORT_ASC]);
-                $c->groupBy(['c.event_enc_id']);
-            }])
             ->joinWith(['webinarRegistrations d' => function ($d) {
                 $d->select([
                     'd.webinar_enc_id',
@@ -127,17 +94,30 @@ class Webinar extends \common\models\Webinar
 
             $events = WebinarEvents::find()
                 ->alias('a')
-                ->select(["a.event_enc_id", 'a.duration', "DATE_FORMAT(a.start_datetime, '%d-%m-%Y') event_date", "DATE_FORMAT(a.start_datetime, '%H:%i') event_time", "ADDTIME(DATE_FORMAT(a.start_datetime, '%H:%i'), SEC_TO_TIME(a.duration*60)) endtime", "a.description"])
+                ->select(["a.event_enc_id",
+                    'a.duration',
+                    "DATE_FORMAT(a.start_datetime, '%d-%m-%Y') event_date",
+                    "DATE_FORMAT(a.start_datetime, '%H:%i:%s') event_time",
+                    "ADDTIME(DATE_FORMAT(a.start_datetime, '%H:%i:%s'), SEC_TO_TIME(a.duration*60)) endtime",
+                    "a.description event_description",
+                    'a.title event_title'
+                ])
                 ->joinWith(['webinarSpeakers d' => function ($d) {
                     $d->select([
                         'd.webinar_event_enc_id',
                         'd.speaker_enc_id',
                         'd1.user_enc_id',
                         'CONCAT(d2.first_name, " ", d2.last_name) as fullname',
-                        'CASE WHEN d2.image IS NOT NULL THEN CONCAT("' . Url::to(Yii::$app->params->upload_directories->users->image, 'https') . '", d2.image_location, "/", d2.image) ELSE CONCAT("https://ui-avatars.com/api/?name=", d2.first_name, "&size=200&rounded=false&background=", REPLACE(d2.initials_color, "#", ""), "&color=ffffff") END image'
+                        'CASE WHEN d2.image IS NOT NULL THEN CONCAT("' . Url::to(Yii::$app->params->upload_directories->users->image, 'https') . '", d2.image_location, "/", d2.image) ELSE CONCAT("https://ui-avatars.com/api/?name=", CONCAT(d2.first_name, " ", d2.last_name), "&size=200&rounded=false&background=", REPLACE(d2.initials_color, "#", ""), "&color=ffffff") END image',
+                        'd3.designation',
+                        'd2.facebook',
+                        'd2.twitter',
+                        'd2.linkedin',
+                        'd2.instagram',
                     ]);
                     $d->joinWith(['speakerEnc d1' => function ($d1) {
                         $d1->joinWith(['userEnc d2']);
+                        $d1->joinWith(['designationEnc d3']);
                     }], false);
                     $d->andWhere(['d.is_deleted' => 0]);
                 }])
@@ -148,6 +128,13 @@ class Webinar extends \common\models\Webinar
                 ->all();
             $dateEvents = ArrayHelper::index($events, null, 'event_date');
 
+            $speaker_count = WebinarSpeakers::find()
+                ->alias('a')
+                ->joinWith(['webinarEventEnc b'])
+                ->where(['b.webinar_enc_id' => $webinar_detail['webinar_enc_id']])
+                ->groupBy(['a.speaker_enc_id'])
+                ->count();
+
             $events = WebinarEvents::find()
                 ->select(['start_datetime'])
                 ->where(['webinar_enc_id' => $webinar_detail['webinar_enc_id'], 'status' => [0, 1]])
@@ -156,6 +143,7 @@ class Webinar extends \common\models\Webinar
                 ->one();
             $webinar_detail['event'] = $events;
             $webinar_detail['events'] = $dateEvents;
+            $webinar_detail['speaker_count'] = $speaker_count;
         }
 
         return $webinar_detail;
