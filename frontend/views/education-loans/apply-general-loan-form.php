d0b09b7a30ace036ce7c0097e978879b342108e8
silly <snehkant93@gmail.com>
2020-09-24 14:08:36 +0530

    images changed sharing

--- frontend/views/education-loans/apply-general-loan-form.php
+++ frontend/views/education-loans/apply-general-loan-form.php
@@ -17,7 +17,7 @@ if (Yii::$app->params->paymentGateways->mec->icici) {
 }
 $keywords = 'Interest Free Loans available for select colleges/Universities | Empower Youth';
 $description = 'Do Not let monetary constraints stop your from getting admission in your dream college/ university';
-$image = Yii::$app->urlManager->createAbsoluteUrl('/assets/common/images/sharing-loan-image.png');
+$image = Yii::$app->urlManager->createAbsoluteUrl('/assets/common/images/sharing-loan-images.png');
 $this->params['seo_tags'] = [
     'rel' => [
         'canonical' => Yii::$app->request->getAbsoluteUrl(),
