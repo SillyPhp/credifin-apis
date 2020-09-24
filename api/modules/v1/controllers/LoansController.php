6cc1239cc7bbd3acda054cb7032c9276ec686fe3
Ravinder Singh <ravindersaini15697@gmail.com>
2020-09-24 18:00:31 +0530

    update paymnet stwus

--- api/modules/v1/controllers/LoansController.php
+++ api/modules/v1/controllers/LoansController.php
@@ -10,6 +10,7 @@ use api\modules\v3\models\OrganizationList;
 use common\models\AssignedCollegeCourses;
 use common\models\CollegeCoursesPool;
 use common\models\Countries;
+use common\models\EducationLoanPayments;
 use common\models\LoanApplications;
 use common\models\LoanTypes;
 use common\models\OrganizationFeeComponents;
@@ -224,6 +225,47 @@ class LoansController extends ApiBaseController
         } else {
             return $this->response(404, ['status' => 404, 'message' => 'not found']);
         }
+    }
+
+    public function actionUpdateLoanApplication()
+    {
+        $params = Yii::$app->request->post();
+        if (isset($params['loan_app_id']) && !empty($params['loan_app_id'])) {
+            $loan_app_id = $params['loan_app_id'];
+        } else {
+            return $this->response(422, ['status' => 422, 'message' => 'missing information']);
+        }
+
+        if (isset($params['education_loan_payment_enc_id']) && !empty($params['education_loan_payment_enc_id'])) {
+            $loan_payment_id = $params['education_loan_payment_enc_id'];
+        } else {
+            return $this->response(422, ['status' => 422, 'message' => 'missing information']);
+        }
+
+        if ($params['status'] == 'captured') {
+            $loan_application = LoanApplications::find()
+                ->where(['loan_app_enc_id' => $loan_app_id])
+                ->one();
+            if ($loan_application) {
+                $loan_application->status = 0;
+                $loan_application->updated_by = $this->userId();
+                $loan_application->updated_on = date('Y-m-d H:i:s');
+                $loan_application->update();
+            }
+        }
+
+        $loan_payments = EducationLoanPayments::find()
+            ->where(['education_loan_payment_enc_id' => $loan_payment_id])
+            ->one();
+        if ($loan_payments) {
+            $loan_payments->payment_id = $params['payment_id'];
+            $loan_payments->payment_status = $params['status'];
+            $loan_payments->updated_by = $this->userId();
+            $loan_payments->updated_on = date('Y-m-d H:i:s');
+            $loan_payments->update();
+        }
+
+        return $this->response(200, ['status' => 200, 'message' => 'success']);
     }
 
 }
