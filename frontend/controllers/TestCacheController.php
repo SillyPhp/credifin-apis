33f433c35116ceed5ee322112dcd2daec25d150c
Sneh Kant <sneh@dsbedutech.in>
2020-10-03 15:06:54 +0530

    image script updates

--- frontend/controllers/TestCacheController.php
+++ frontend/controllers/TestCacheController.php
@@ -17,24 +17,30 @@ class TestCacheController extends Controller
         $client = Client::getInstance();
         $p = Url::to('@rootDirectory/bin/phantomjs');
         $client->getEngine()->setPath($p);
-        $client->getEngine()->debug(true);
-        /**
-         * @see JonnyW\PhantomJs\Http\Request
-         **/
         $request = $client->getMessageFactory()->createRequest('https://www.empoweryouth.com/education-loans/apply-loan/7y028kbWNRWwj8VGg3zORK4v9marEp', 'GET');
-
-        /**
-         * @see JonnyW\PhantomJs\Http\Response
-         **/
         $response = $client->getMessageFactory()->createResponse();
-        // Send the request
-       $client->send($request, $response);
-        echo  $client->getLog(); // Output log
-        //die();
+        $client->send($request, $response);
         print_r($response);
         if($response->getStatus() === 200) {
-            // Dump the requested page content
             echo $response->getContent();
         }
+    }
+
+    public function actionSharing()
+    {
+        $client = Client::getInstance();
+        $p = Url::to('@webroot/website/bin/phantomjs');
+        $client->getEngine()->setPath($p);
+        $width  = 1250;
+        $height = 650;
+        $top    = 0;
+        $left   = 0;
+        $request = $client->getMessageFactory()->createCaptureRequest('http://sneh.eygb.me/framed-widgets/application-sharing-image', 'GET');
+        $request->setOutputFile('sav-image.png');
+        $request->setViewportSize($width, $height);
+        $request->setCaptureDimensions($width, $height, $top, $left);
+        $request->setQuality(100);
+        $response = $client->getMessageFactory()->createResponse();
+        $client->send($request, $response);
     }
 }
