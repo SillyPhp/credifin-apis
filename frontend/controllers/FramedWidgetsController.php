f86d8aa5daa4d6cd5924219ec6daa7ced18d7dfd
Sneh Kant <sneh@dsbedutech.in>
2020-10-03 13:52:09 +0530

    image script widget frames

--- frontend/controllers/FramedWidgetsController.php
+++ frontend/controllers/FramedWidgetsController.php
@@ -4,13 +4,27 @@ namespace frontend\controllers;
 
 use common\models\Organizations;
 use Yii;
+use yii\filters\VerbFilter;
 use yii\web\Controller;
 use yii\helpers\Html;
 use yii\helpers\Url;
 
 class FramedWidgetsController extends Controller
 {
-  public $layout = 'widget-layout';
+    public $layout = 'widget-layout';
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        $behaviors['verbs'] = [
+            'class' => VerbFilter::className(),
+            'actions' => [
+                'sharing-graphic' => ['GET'],
+            ]
+        ];
+        return $behaviors;
+    }
   public function actionEducationsLoan($id)
    { 
        $wid = Organizations::find()
@@ -23,5 +37,19 @@ class FramedWidgetsController extends Controller
        else{
            return 'Unauthorized';
        }
+   }
+
+   public function actionApplicationSharingImage($content=null)
+   {
+       $content = [
+           'job_title'=>'(USA) Staff Pharmacist (Hourly) - Wm (Temporary)',
+           'company_name'=>'walmart',
+           'canvas'=>false,
+           'logo'=>'https://www.empoweryouth.com/images/unclaimed-organizations/eFVfBTYw3qeYDyU9sQfKriBUriahw1LdgxGkmLaXkjYXy8XkMo/WLXm0-ZKNKvj39wjcsLHle7u6ePZJ6ho/abvgrG4VyQNjO6zLzk27QpW30A9nXK.png',
+           'initial_color'=>'#286090',
+           'location'=>'Tepoztlán, Mexico',
+       ];
+       $this->layout = 'null-layout';
+       return $this->render('sharing-graphic',['content'=>$content]);
    }
 }
