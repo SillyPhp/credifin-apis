241f3ffdfebf1b4a87180e8b052128e907c6e293
Sneh Kant <sneh@dsbedutech.in>
2020-10-09 13:57:18 +0530

    move data from logos to digital ocean

--- frontend/controllers/TestCacheController.php
+++ frontend/controllers/TestCacheController.php
@@ -1,12 +1,51 @@
 <?php
 namespace frontend\controllers;
+use common\models\Organizations;
+use common\models\UnclaimedOrganizations;
 use Yii;
 use yii\web\Controller;
-use common\models\Spaces\Spaces;
+use common\models\spaces\Spaces;
 class TestCacheController extends Controller
 {
-   public function actionTest()
+   public function actionUnclaimedUpload($page,$limit=20)
    {
+       $offset = ($page - 1) * $limit;
+       $spaces = new Spaces(Yii::$app->params->digitalOcean->accessKey,Yii::$app->params->digitalOcean->secret);
+       $my_space = $spaces->space(Yii::$app->params->digitalOcean->sharingSpace);
+       $getData = UnclaimedOrganizations::find()
+           ->select(['logo','logo_location'])
+           ->limit($limit)
+           ->offset($offset)
+           ->asArray()
+           ->all();
 
+       foreach ($getData as $get)
+       {
+           if (!empty($get['logo'])){
+               $imageSourcePath = Yii::$app->params->upload_directories->unclaimed_organizations->logo_path.$get['logo_location'].'/'.$get['logo'];
+               $result = $my_space->uploadFile($imageSourcePath, "images/ey-logos/uncliamed-organizations/".$get['logo_location'].'/'.$get['logo'], "public");
+           }
+       }
    }
+
+    public function actionClaimedUpload($page,$limit=20)
+    {
+        $offset = ($page - 1) * $limit;
+        $spaces = new Spaces(Yii::$app->params->digitalOcean->accessKey,Yii::$app->params->digitalOcean->secret);
+        $my_space = $spaces->space(Yii::$app->params->digitalOcean->sharingSpace);
+        $getData = Organizations::find()
+            ->select(['logo','logo_location'])
+            ->limit($limit)
+            ->offset($offset)
+            ->asArray()
+            ->all();
+
+        foreach ($getData as $get)
+        {
+            if (!empty($get['logo'])){
+                $imageSourcePath = Yii::$app->params->upload_directories->organizations->logo_path.$get['logo_location'].'/'.$get['logo'];
+                $result = $my_space->uploadFile($imageSourcePath, "images/ey-logos/organizations/".$get['logo_location'].'/'.$get['logo'], "public");
+            }
+        }
+    }
 }
