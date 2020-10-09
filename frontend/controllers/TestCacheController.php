7fcc6c1751e260085a66787609d3d34f8fc23922
Sneh Kant <sneh@dsbedutech.in>
2020-10-09 17:10:08 +0530

    removed unused actions

--- frontend/controllers/TestCacheController.php
+++ frontend/controllers/TestCacheController.php
@@ -7,30 +7,5 @@ use yii\web\Controller;
 use common\models\spaces\Spaces;
 class TestCacheController extends Controller
 {
-    public function actionClaimedUpload($page,$limit=20)
-    {
-        $offset = ($page - 1) * $limit;
-        $getData = Organizations::find()
-            ->select(['logo','logo_location'])
-            ->where([
-                'or',
-                ['!=','logo',null],
-                ['!=','logo','']
-            ])
-            ->limit($limit)
-            ->offset($offset)
-            ->asArray()
-            ->all();
 
-        foreach ($getData as $get)
-        {
-            if (!empty($get['logo'])){
-                $spaces = new Spaces(Yii::$app->params->digitalOcean->accessKey,Yii::$app->params->digitalOcean->secret);
-                $my_space = $spaces->space(Yii::$app->params->digitalOcean->sharingSpace);
-                $imageSourcePath = Yii::$app->params->upload_directories->organizations->logo_path.$get['logo_location'].'/'.$get['logo'];
-                $result = $my_space->uploadFile($imageSourcePath, "images/ey-logos/organizations/".$get['logo_location'].'/'.$get['logo'], "public");
-            }
-        }
-        echo count($getData);
-    }
 }
