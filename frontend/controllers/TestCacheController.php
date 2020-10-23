7baca9216c168470d36a680424e5068179419ab1
Sneh Kant <sneh@dsbedutech.in>
2020-10-23 12:11:12 +0530

    loan widgets changes function changes for ,accrd genral form

--- frontend/controllers/TestCacheController.php
+++ frontend/controllers/TestCacheController.php
@@ -1,5 +1,6 @@
 <?php
 namespace frontend\controllers;
+use common\models\extended\PaymentsModule;
 use common\models\Organizations;
 use common\models\UnclaimedOrganizations;
 use Yii;
@@ -8,23 +9,5 @@ use common\models\spaces\Spaces;
 use Razorpay\Api\Api;
 class TestCacheController extends Controller
 {
-    public function actionTest()
-    {
-        $api_key = 'rzp_test_lzrrcON3EuW3nI';
-        $api_secret = 'caFrU097wpTYMi0xQgcCfonJ';
-        $api = new Api($api_key,$api_secret);
 
-        $order  = $api->order->create([
-            'receipt' => 'order_rcptid_11',
-            'amount'  => 50000,
-            'currency' => 'INR'
-        ]);
-
-        print_r($order);
-    }
-
-    public function actionPay()
-    {
-        return $this->render('pay');
-    }
 }
