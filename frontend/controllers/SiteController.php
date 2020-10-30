35f5558197b870808a93295937a0080581c7bf02
Nitesh Sharma <rs.nitesh01@gmail.com>
2020-10-31 00:31:08 +0530

    lead application for has now added field and radio button are now saving data on change and also change auto update ajax url with leads/update-application...please update url_rules file..

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -314,53 +314,6 @@ class SiteController extends Controller
 
     public function actionAboutUs()
     {
-        if (Yii::$app->request->post() && Yii::$app->request->isAjax) {
-            Yii::$app->response->format = Response::FORMAT_JSON;
-            $params = Yii::$app->request->post();
-            $field_name = $params['fieldName'];
-            $type = $params['type'];
-            $value = $params['value'];
-            $key = $params['lead_app_id'];
-            $seq = $params['sequence'];
-            $lead_app_id = $params['lead_app_id'];
-            $utilitiesModel = new Utilities();
-            if ($lead_app_id) {
-                if ($type == 'leadCollegePreference') {
-                    $model = LeadsCollegePreference::findOne(['application_enc_id' => $key, 'sequence' => $seq]);
-                    if (!$model) {
-                        $model = new LeadsCollegePreference();
-                        $utilitiesModel->variables['string'] = time() . rand(100, 100000);
-                        $model->preference_enc_id = $utilitiesModel->encrypt();
-                        $model->application_enc_id = $enc_id = $key;
-                        $model->sequence = $seq;
-                        if (!Yii::$app->user->isGuest) {
-                            $model->created_by = Yii::$app->user->identity->user_enc_id;
-                        }
-                    }
-                } else {
-                    $model = LeadsApplications::findOne(['application_enc_id' => $key]);
-                    $enc_id = $key;
-                }
-            } else {
-                $model = new LeadsApplications();
-                $utilitiesModel->variables['string'] = time() . rand(100, 100000);
-                $model->application_enc_id = $enc_id = $utilitiesModel->encrypt();
-                $model->application_number = date('ymd') . time();
-                if (!Yii::$app->user->isGuest) {
-                    $model->created_by = Yii::$app->user->identity->user_enc_id;
-                }
-            }
-            $model->$field_name = $value;
-            if ($model->save()) {
-                return [
-                    'status' => 200,
-                    'enc_id' => $enc_id,
-                ];
-            } else {
-                print_r($model->getErrors());
-                exit();
-            }
-        }
         return $this->render('about-us');
     }
 
