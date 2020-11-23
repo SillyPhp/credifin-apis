bb079bed644325a958edca8c89be44d8fb4fadd1
Sneh Kant <sneh@dsbedutech.in>
2020-11-23 12:15:40 +0530

    test

--- frontend/controllers/TestCacheController.php
+++ frontend/controllers/TestCacheController.php
@@ -1,7 +1,10 @@
 <?php
 namespace frontend\controllers;
 use common\models\Auth;
+use common\models\spaces\Spaces;
 use yii\web\Controller;
+use yii\helpers\Url;
+use Yii;
 class TestCacheController extends Controller
 {
   public function actionTest()
@@ -18,5 +21,17 @@ class TestCacheController extends Controller
       }catch (\Exception $exception) {
           return $exception->getMessage(); //final messege for user
       }
+  }
+
+  public function actionSpacesApi()
+  {
+      $spaces = new Spaces(Yii::$app->params->digitalOcean->accessKey,Yii::$app->params->digitalOcean->secret);
+      $my_space = $spaces->space(Yii::$app->params->digitalOcean->sharingSpace);
+      $result = $my_space->uploadFileMeta(
+          Url::to('@rootDirectory/files/temp/1nmUsMFvd4yV_hgaChik60teROlmNQcI.png'),
+          "images/sharing/sneh.png", "public",
+          ['Content-Disposition'=>'attachment']
+      );
+      print_r($result);
   }
 }
