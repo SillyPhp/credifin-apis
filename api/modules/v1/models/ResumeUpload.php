9bfd21beff34184aa1f6011c9f805d7194887962
Ravinder Singh <ravindersaini15697@gmail.com>
2020-11-28 12:36:42 +0530

    set print in android resumeupload api to error

--- api/modules/v1/models/ResumeUpload.php
+++ api/modules/v1/models/ResumeUpload.php
@@ -29,47 +29,57 @@ class ResumeUpload extends Model
 
     public function upload($resume, $resume_ext, $resume_name)
     {
-        $token_holder_id = UserAccessTokens::findOne([
-            'access_token' => explode(" ", Yii::$app->request->headers->get('Authorization'))[1]
-        ]);
-        $user = Candidates::findOne([
-            'user_enc_id' => $token_holder_id->user_enc_id
-        ]);
-        $utilitiesModel = new Utilities();
-        $utilitiesModel->variables['string'] = time() . rand(100, 100000);
-        $userResumeModel = new UserResume();
-        $userResumeModel->resume_enc_id = $utilitiesModel->encrypt();
-        $userResumeModel->user_enc_id = $user->user_enc_id;
-        $userResumeModel->resume_location = Yii::$app->getSecurity()->generateRandomString();
-        $base_path = Yii::$app->params->upload_directories->resume->file . $userResumeModel->resume_location . '/';
-        $utilitiesModel->variables['string'] = time() . rand(100, 100000);
+        $transaction = Yii::$app->db->beginTransaction();
+        try {
+            $token_holder_id = UserAccessTokens::findOne([
+                'access_token' => explode(" ", Yii::$app->request->headers->get('Authorization'))[1]
+            ]);
+            $user = Candidates::findOne([
+                'user_enc_id' => $token_holder_id->user_enc_id
+            ]);
+            $utilitiesModel = new Utilities();
+            $utilitiesModel->variables['string'] = time() . rand(100, 100000);
+            $userResumeModel = new UserResume();
+            $userResumeModel->resume_enc_id = $utilitiesModel->encrypt();
+            $userResumeModel->user_enc_id = $user->user_enc_id;
+            $userResumeModel->resume_location = Yii::$app->getSecurity()->generateRandomString();
+            $base_path = Yii::$app->params->upload_directories->resume->file . $userResumeModel->resume_location . '/';
+            $utilitiesModel->variables['string'] = time() . rand(100, 100000);
 
-        $encrypted_string = $utilitiesModel->encrypt();
-        if (substr($encrypted_string, -1) == '.') {
-            $encrypted_string = substr($encrypted_string, 0, -1);
-        }
-
-        $userResumeModel->resume = $encrypted_string . '.' . $resume_ext;
-        $userResumeModel->title = $resume_name . '.' . $resume_ext;
-        $userResumeModel->alt = $resume_name . '.' . $resume_ext;
-        $userResumeModel->created_on = date('Y-m-d h:i:s');
-        $userResumeModel->created_by = $user->user_enc_id;
-        $file = dirname(__DIR__, 4) . '/files/temp/' . $userResumeModel->resume;
-        $spaces = new Spaces(Yii::$app->params->digitalOcean->accessKey, Yii::$app->params->digitalOcean->secret);
-        $my_space = $spaces->space(Yii::$app->params->digitalOcean->sharingSpace);
-        if (file_put_contents($file, $resume)) {
-            $result = $my_space->uploadFile($file, Yii::$app->params->digitalOcean->rootDirectory . $base_path . $userResumeModel->resume, "private");
-            if (file_exists($file)) {
-                unlink($file);
+            $encrypted_string = $utilitiesModel->encrypt();
+            if (substr($encrypted_string, -1) == '.') {
+                $encrypted_string = substr($encrypted_string, 0, -1);
             }
-        }
-        if ($result) {
-            if ($userResumeModel->save()) {
-                return $userResumeModel->resume_enc_id;
+
+            $userResumeModel->resume = $encrypted_string . '.' . $resume_ext;
+            $userResumeModel->title = $resume_name . '.' . $resume_ext;
+            $userResumeModel->alt = $resume_name . '.' . $resume_ext;
+            $userResumeModel->created_on = date('Y-m-d h:i:s');
+            $userResumeModel->created_by = $user->user_enc_id;
+            $file = dirname(__DIR__, 4) . '/files/temp/' . $userResumeModel->resume;
+            $spaces = new Spaces(Yii::$app->params->digitalOcean->accessKey, Yii::$app->params->digitalOcean->secret);
+            $my_space = $spaces->space(Yii::$app->params->digitalOcean->sharingSpace);
+            if (file_put_contents($file, $resume)) {
+                $result = $my_space->uploadFile($file, Yii::$app->params->digitalOcean->rootDirectory . $base_path . $userResumeModel->resume, "private");
+                if (file_exists($file)) {
+                    unlink($file);
+                }
+            }
+            if ($result) {
+                if ($userResumeModel->save()) {
+                    return $userResumeModel->resume_enc_id;
+                }
+//                return false;
+                print_r($userResumeModel->getErrors());
+                die();
             }
             return false;
+        } catch (\Exception $exception) {
+            print_r($exception);
+            die();
+            $transaction->rollBack();
+            return false;
         }
-        return false;
     }
 
 
