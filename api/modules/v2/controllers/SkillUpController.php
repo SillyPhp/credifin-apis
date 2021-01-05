8bfa4054f34562d87fc13ba394df05488d0d6bdd
Ravinder Singh <ravindersaini15697@gmail.com>
2021-01-05 15:42:38 +0530

    working on skillup feeds

--- api/modules/v2/controllers/SkillUpController.php
+++ api/modules/v2/controllers/SkillUpController.php
@@ -3,6 +3,8 @@
 
 namespace api\modules\v2\controllers;
 
+use common\models\SkillsUpLikesDislikes;
+use common\models\SkillsUpPostComments;
 use common\models\SkillsUpPosts;
 use Yii;
 use \yii\db\Expression;
@@ -26,6 +28,8 @@ class SkillUpController extends ApiBaseController
             'class' => VerbFilter::className(),
             'actions' => [
                 'feed' => ['POST', 'OPTIONS'],
+                'post-like-dislike' => ['POST', 'OPTIONS'],
+                'post-comments' => ['POST', 'OPTIONS'],
             ]
         ];
 
@@ -45,14 +49,16 @@ class SkillUpController extends ApiBaseController
     {
         if ($user = $this->isAuthorized()) {
 
+            $param = Yii::$app->request->post();
+
             if (isset($param['limit']) && !empty($param['limit'])) {
-                $limit = $param['limit'];
+                $limit = (int)$param['limit'];
             } else {
                 $limit = 10;
             }
 
             if (isset($param['page']) && !empty($param['page'])) {
-                $page = $param['page'];
+                $page = (int)$param['page'];
             } else {
                 $page = 1;
             }
@@ -73,8 +79,11 @@ class SkillUpController extends ApiBaseController
                     'a.content_type'
                 ])
                 ->joinWith(['sourceEnc b'], false)
-                ->where(['a.is_deleted' => 0, 'a.status' => 'Active', 'b.is_deleted' => 0])
-                ->limit($limit)
+                ->where(['a.is_deleted' => 0, 'a.status' => 'Active', 'b.is_deleted' => 0]);
+            if (isset($param['content_type']) && !empty($param['content_type'])) {
+                $feeds->andWhere(['in', 'content_type', $param['content_type']]);
+            }
+            $feeds = $feeds->limit($limit)
                 ->offset(($page - 1) * $limit)
                 ->asArray()
                 ->all();
@@ -83,6 +92,78 @@ class SkillUpController extends ApiBaseController
             } else {
                 return $this->response(404, ['status' => 404, 'message' => 'not found']);
             }
+        } else {
+            return $this->response(401, ['status' => 401, 'message' => 'unauthorized']);
+        }
+    }
+
+    public function actionPostLikeDislike()
+    {
+        if ($user = $this->isAuthorized()) {
+            $params = Yii::$app->request->post();
+            if (!isset($params['action']) && empty($params['action'])) {
+                return $this->response(422, ['status' => 422, 'message' => 'missing information']);
+            }
+            if (!isset($params['post_id']) && empty($params['post_id'])) {
+                return $this->response(422, ['status' => 422, 'message' => 'missing information']);
+            }
+
+            $row = SkillsUpLikesDislikes::findOne(['post_enc_id' => $params['post_id'], 'created_by' => $user->user_enc_id]);
+
+            if ($row) {
+                $row->feedback_status = $params['action'];
+                $row->last_updated_by = $user->user_enc_id;
+                $row->last_updated_on = date('Y-m-d H:i:s');
+                if (!$row->update()) {
+                    return $this->response(500, ['status' => 500, 'message' => 'an error occurred']);
+                } else {
+                    return $this->response(200, ['status' => 200, 'message' => 'success']);
+                }
+            } else {
+                $model = new SkillsUpLikesDislikes();
+                $utilitiesModel = new \common\models\Utilities();
+                $utilitiesModel->variables['string'] = time() . rand(100, 100000);
+                $model->post_like_dislike_enc_id = $utilitiesModel->encrypt();
+                $model->post_enc_id = $params['post_id'];
+                $model->feedback_status = $params['action'];
+                $model->created_by = $user->user_enc_id;
+                $model->created_on = date('Y-m-d H:i:d');
+                if (!$model->save()) {
+                    return $this->response(500, ['status' => 500, 'message' => 'an error occurred']);
+                } else {
+                    return $this->response(200, ['status' => 200, 'message' => 'success']);
+                }
+            }
+
+        } else {
+            return $this->response(401, ['status' => 401, 'message' => 'unauthorized']);
+        }
+    }
+
+    public function actionPostComments()
+    {
+        if ($user = $this->isAuthorized()) {
+            $params = Yii::$app->request->post();
+
+            if (!isset($params['post_id']) && empty($params['post_id'])) {
+                return $this->response(422, ['status' => 422, 'message' => 'missing information']);
+            }
+
+            $comment = SkillsUpPostComments::findOne(['post_enc_id' => $params['post_id'], 'created_by' => $user->user_enc_id]);
+
+            if ($comment) {
+                $comment->comment = $params['comment'];
+                $comment->last_updated_by = $user->user_enc_id;
+                $comment->last_updated_on = date('Y-m-d H:i:s');
+                if ($comment->update()) {
+                    return $this->response(200, ['status' => 200, 'message' => 'success']);
+                } else {
+                    return $this->response(500, ['status' => 500, 'message' => 'an error occurred']);
+                }
+            } else {
+
+            }
+
         } else {
             return $this->response(401, ['status' => 401, 'message' => 'unauthorized']);
         }
