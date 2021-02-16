97d07fd3f8616e7a1e70bf82755bcc1c948a669a
Shshank <vasishtshshank@gmail.com>
2021-02-16 14:50:41 +0530

    coollege profile (pending)

--- frontend/views/widgets/college-widgets/college-overview.php
+++ frontend/views/widgets/college-widgets/college-overview.php
@@ -4,89 +4,92 @@ use yii\helpers\Url;
 
 ?>
 
-    <div class="container">
+<div class="container">
         <div class="col-md-12">
             <div class="set-sticky">
                 <h3 class="ou-head">Highlights</h3>
                 <div class="h-points">
-                    <div class="h-point1">
-                        <div class="fa-icon"><i class="fas fa-university"></i></div>
-                        <div class="fa-text">
-                            <h3>Established or University Type</h3>
-                            <p>AAA</p>
-                        </div>
-                    </div>
-                    <div class="h-point1">
-                        <div class="fa-icon"><i class="fab fa-affiliatetheme"></i></div>
-                        <div class="fa-text">
-                            <h3>Affiliated to</h3>
-                            <p>AAA</p>
-                        </div>
-                    </div>
-                    <div class="h-point1">
-                        <div class="fa-icon"><i class="fas fa-clipboard-check"></i></div>
-                        <div class="fa-text">
-                            <h3>Accredited to</h3>
-                            <p>AAA</p>
-                        </div>
-                    </div>
-                    <div class="h-point1">
-                        <div class="fa-icon"><i class="fas fa-scroll"></i></div>
-                        <div class="fa-text">
-                            <h3>Entrance Exam</h3>
-                            <p>AAA</p>
-                        </div>
-                    </div>
-                    <div class="h-point1">
-                        <div class="fa-icon"><i class="fas fa-microchip"></i></div>
-                        <div class="fa-text">
-                            <h3>Total Programs</h3>
-                            <p>AAA</p>
-                        </div>
-                    </div>
-                    <div class="h-point1">
-                        <div class="fa-icon"><i class="fas fa-list"></i></div>
-                        <div class="fa-text">
-                            <h3>Popular Courses</h3>
-                            <p>AAA</p>
-                        </div>
-                    </div>
-                    <div class="h-point1">
-                        <div class="fa-icon"><i class="fas fa-rupee-sign"></i></div>
-                        <div class="fa-text">
-                            <h3>Application Mode | Fees</h3>
-                            <p>AAA</p>
-                        </div>
-                    </div>
-                    <div class="h-point1">
-                        <div class="fa-icon"><i class="fas fa-user"></i></div>
-                        <div class="fa-text">
-                            <h3>Top Recruiters</h3>
-                            <p>AAA</p>
-                        </div>
-                    </div>
-                    <div class="h-point1">
-                        <div class="fa-icon"><i class="fas fa-clipboard-list"></i></div>
-                        <div class="fa-text">
-                            <h3>Facilities available</h3>
-                            <p>AAA</p>
-                        </div>
-                    </div>
-                    <div class="h-point1">
-                        <div class="fa-icon"><i class="fas fa-graduation-cap"></i></div>
-                        <div class="fa-text">
-                            <h3>LPU Brochure</h3>
-                            <p>AAA</p>
-                        </div>
-                    </div>
-                    <div class="h-point1">
-                        <div class="fa-icon"><i class="fas fa-link"></i></div>
-                        <div class="fa-text">
-                            <h3>Official Website</h3>
-                            <p>AAA</p>
-                        </div>
-                    </div>
+
                 </div>
+<!--                <div class="h-points">-->
+<!--                    <div class="h-point1">-->
+<!--                        <div class="fa-icon"><i class="fas fa-university"></i></div>-->
+<!--                        <div class="fa-text">-->
+<!--                            <h3>Established or University Type</h3>-->
+<!--                            <p>AAA</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="h-point1">-->
+<!--                        <div class="fa-icon"><i class="fab fa-affiliatetheme"></i></div>-->
+<!--                        <div class="fa-text">-->
+<!--                            <h3>Affiliated to</h3>-->
+<!--                            <p></p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="h-point1">-->
+<!--                        <div class="fa-icon"><i class="fas fa-clipboard-check"></i></div>-->
+<!--                        <div class="fa-text">-->
+<!--                            <h3>Accredited to</h3>-->
+<!--                            <p>AAA</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="h-point1">-->
+<!--                        <div class="fa-icon"><i class="fas fa-scroll"></i></div>-->
+<!--                        <div class="fa-text">-->
+<!--                            <h3>Entrance Exam</h3>-->
+<!--                            <p>AAA</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="h-point1">-->
+<!--                        <div class="fa-icon"><i class="fas fa-microchip"></i></div>-->
+<!--                        <div class="fa-text">-->
+<!--                            <h3>Total Programs</h3>-->
+<!--                            <p>AAA</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="h-point1">-->
+<!--                        <div class="fa-icon"><i class="fas fa-list"></i></div>-->
+<!--                        <div class="fa-text">-->
+<!--                            <h3>Popular Courses</h3>-->
+<!--                            <p>AAA</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="h-point1">-->
+<!--                        <div class="fa-icon"><i class="fas fa-rupee-sign"></i></div>-->
+<!--                        <div class="fa-text">-->
+<!--                            <h3>Application Mode | Fees</h3>-->
+<!--                            <p>AAA</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="h-point1">-->
+<!--                        <div class="fa-icon"><i class="fas fa-user"></i></div>-->
+<!--                        <div class="fa-text">-->
+<!--                            <h3>Top Recruiters</h3>-->
+<!--                            <p>AAA</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="h-point1">-->
+<!--                        <div class="fa-icon"><i class="fas fa-clipboard-list"></i></div>-->
+<!--                        <div class="fa-text">-->
+<!--                            <h3>Facilities available</h3>-->
+<!--                            <p>AAA</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="h-point1">-->
+<!--                        <div class="fa-icon"><i class="fas fa-graduation-cap"></i></div>-->
+<!--                        <div class="fa-text">-->
+<!--                            <h3>LPU Brochure</h3>-->
+<!--                            <p>AAA</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="h-point1">-->
+<!--                        <div class="fa-icon"><i class="fas fa-link"></i></div>-->
+<!--                        <div class="fa-text">-->
+<!--                            <h3>Official Website</h3>-->
+<!--                            <p>AAA</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
             </div>
             <div class="set-sticky">
                 <h3 class="ou-head">About College</h3>
@@ -194,37 +197,96 @@ use yii\helpers\Url;
                 </div>
             </div>
             <div class="set-sticky">
-                <h3 class="ou-head">Loan Choices</h3>
+                <h3 class="heading-style">Education Loan Options</h3>
                 <div class="table-view">
                     <table>
                         <thead>
                         <tr>
                             <th class="w15">Bank/Financier</th>
-                            <th class="w15">Applicable Rate of Interest</th>
-                            <th class="w20">Maximum Loan Size with Collateral</th>
-                            <th class="w20">Maximum Loan Size without Collateral</th>
-                            <th class="w15">Processing Fees</th>
-                            <th class="w15">Repayment Period</th>
+                            <th class="w15">Rate of Interest</th>
+                            <th class="w18">Loan Amount Available</th>
+                            <th class="w22">Collateral</th>
+                            <th class="w18">Processing Fee</th>
+                            <th class="w10">Coverage</th>
                         </tr>
                         </thead>
                         <tbody>
                         <tr>
                             <td>
                                 <div class="loanProviderIcon">
+                                    <img src="<?= Url::to('@eyAssets/images/pages/education-loans/Bank_of_Baroda_logo.png') ?>">
+                                </div>
+                            </td>
+                            <td>Listed Universities: 8.35% for boys, 7.85% for girls  , <br>
+                                Unlisted Universities: 9.0% for boys, 8.5% for girls.
+                            </td>
+                            <td>Listed Universities: Rs.80 lakh<br>
+                                Unlisted Universities: Rs.60 lakh
+                            </td>
+                            <td>Up to Rs.7.5 lakh: Moratorium period + 10 years<br>
+                                Above Rs.7.5 lakh: Moratorium period + 15 years + Collateral                            </td>
+                            <td>Rs.10,000 + GST (Rs.10,000 refundable); <br>Additional property valuation charge of Rs. 7,500, in case of Real Estate collateral</td>
+                            <td>Pan India</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="loanProviderIcon">
+                                    <img src="<?= Url::to('@eyAssets/images/pages/education-loans/icici_bank_logo.png') ?>">
+                                </div>
+                            </td>
+                            <td>Starting @ 11.5% p.a.</td>
+                            <td>"Rs.1 crore (With Collateral)<br>
+                                Rs. 40 Lakhs (Without Collateral)"</td>
+                            <td>Loan Without Collateral:<br>
+                                For UG - Moratorium period + 5 years<br>
+                                For PG - Moratorium period + 8 years<br>
+                                Loan With Collateral:<br>
+                                For UG - Moratorium period + 7 years<br>
+                                For PG - Moratorium period + 10 years<br>
+                            </td>
+                            <td>1 % of Loan Amount + GST</td>
+                            <td>Pan India</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="loanProviderIcon">
                                     <img src="<?= Url::to('@eyAssets/images/pages/education-loans/avanse-logo.png') ?>">
                                 </div>
                             </td>
-                            <td>12.00 - 14.5%</td>
-                            <td>
-                                <p>No Upper Limit (India)</p>
-                                <p>No Upper Limit (Abroad)</p>
+                            <td>11.75% to 16% p.a.</td>
+                            <td>"Minimum: Rs.1 lakh<br>
+                                Maximum: Rs.40 lakh; though it can be increased depending on the fees"
                             </td>
+                            <td>Case Dependent - With Moratorium</td>
+                            <td>2 % of Loan Amount + GST</td>
+                            <td>Pan India</td>
+                        </tr>
+                        <tr>
                             <td>
-                                <p>20 Lacs (India)</p>
-                                <p>40 Lacs (Abroad)</p>
+                                <div class="loanProviderIcon">
+                                    <img src="<?= Url::to('@eyAssets/images/pages/education-loans/incred_logo.png') ?>">
+                                </div>
                             </td>
-                            <td>1 - 2 %</td>
-                            <td>7 - 15 Years</td>
+                            <td>Up to Rs.10 lakh: 12.75% - 16% <br>
+                                Above Rs.10 lakh: 11.75% - 16%
+                            </td>
+                            <td>Maximum: Rs.40 lakh for Incred unsecured education loan and Rs.1 crore for secured loan.</td>
+                            <td>Case Dependent - With Moratorium</td>
+                            <td>1% to 1.25% + GST</td>
+                            <td>Pan India</td>
+                        </tr>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="loanProviderIcon">
+                                    <img src="<?= Url::to('@eyAssets/images/pages/education-loans/wepay.png') ?>">
+                                </div>
+                            </td>
+                            <td>14 to 16%</td>
+                            <td>50% Of Colaterall Amount</td>
+                            <td>Yes - Without Moratorium</td>
+                            <td>Up To - 4%</td>
+                            <td>Pan India</td>
                         </tr>
                         <tr>
                             <td>
@@ -232,20 +294,30 @@ use yii\helpers\Url;
                                     <img src="<?= Url::to('@eyAssets/images/pages/education-loans/exclusive-logo.png') ?>">
                                 </div>
                             </td>
-                            <td>12.00 - 14.5%</td>
+                            <td>15 to 16%</td>
+                            <td>50% Of Colaterall Amount</td>
+                            <td>Yes - Without Moratorium</td>
+                            <td>Up To - 4%</td>
+                            <td>Punjab</td>
+                        </tr>
+                        <tr>
                             <td>
-                                <p>No Upper Limit (India)</p>
-                                <p>No Upper Limit (Abroad)</p>
+                                <div class="loanProviderIcon">
+                                    <img src="<?= Url::to('@eyAssets/images/pages/education-loans/ezcapital.png') ?>">
+                                </div>
                             </td>
-                            <td>
-                                <p>20 Lacs (India)</p>
-                                <p>40 Lacs (Abroad)</p>
-                            </td>
-                            <td>1 - 2 %</td>
-                            <td>7 - 15 Years</td>
+                            <td>12% Flat</td>
+                            <td>2 Lakh</td>
+                            <td>No - 10 Months Repayment</td>
+                            <td>Up To - 5%</td>
+                            <td>Punjab</td>
                         </tr>
                         </tbody>
                     </table>
+                </div>
+                <div class="displayFlex">
+                    <div>*Terms&conditionsapplicable</div>
+                    <div>**Processingfeedependentonbank/nbfc</div>
                 </div>
             </div>
         </div>
@@ -460,5 +532,29 @@ $(document).on('click','.view-btn',function(e) {
       $('.tab-content').css('height',mainHeight);
   },700);
 });
+
+function getDetails(){
+    var slug = 'erexxtesting'
+    $.ajax({
+        url: "https://ravinder.eygb.me/api/v3/ey-college-profile/college-detail",
+        method: 'POST',
+        data: {slug:slug},
+        success: function (res){
+            if(res.response.status == 200){
+                var re = res.response.data;
+                let Hpoints =  `<div class="h-point1">
+                                    <div class="fa-icon"><i class="fab fa-affiliatetheme"></i></div>
+                                    <div class="fa-text">
+                                    <h3>Affiliated to</h3>
+                                    <p>`re['affiliated_to']`</p>
+                                    </div>
+                                </div>`;
+                console.log(Hpoints);
+                $(Hpoints).appendTo('.h-points');
+            }
+        }
+    })
+}
+getDetails();
 JS;
 $this->registerJS($script);
