2fd91e29ec8e583b7cdebc69774b270f3fe1a628
vasishtshshank <vasishtshshank@gmail.com>
2021-02-22 11:51:04 +0530

    college profile (pending)

--- frontend/views/site/college-main.php
+++ frontend/views/site/college-main.php
@@ -29,12 +29,12 @@ use yii\helpers\Url;
                                     Overview
                                 </a>
                             </li>
-                            <li class="nav-item courses" data-toggle="tab">
-                                <a class="nav-link" href="#courses" data-key="getCourses" data-id="courses" role="tab"
-                                   data-toggle="tab">
-                                    Courses & Fee
-                                </a>
-                            </li>
+<!--                            <li class="nav-item courses" data-toggle="tab">-->
+<!--                                <a class="nav-link" href="#courses" data-key="getCourses" data-id="courses" role="tab"-->
+<!--                                   data-toggle="tab">-->
+<!--                                    Courses & Fee-->
+<!--                                </a>-->
+<!--                            </li>-->
 <!--                            <li class="nav-item placements" data-toggle="tab">-->
 <!--                                <a class="nav-link" href="#placements" data-key="getPlacements" role="tab"-->
 <!--                                   data-toggle="tab">-->
@@ -97,22 +97,22 @@ use yii\helpers\Url;
                                 </svg>
                             </div>
                         </div>
-                        <div id="courses" class="tab-pane fade" role="tabpanel">
-                            <div class="text-center">
-                                <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg"
-                                     class="svg-inline--fa fa-spinner-third fa-w-16 fa-spin" viewBox="0 0 512 512"
-                                     style="max-width: 100px;">
-                                    <g class="fa-group">
-                                        <path fill="#ddd"
-                                              d="M478.71 364.58zm-22 6.11l-27.83-15.9a15.92 15.92 0 0 1-6.94-19.2A184 184 0 1 1 256 72c5.89 0 11.71.29 17.46.83-.74-.07-1.48-.15-2.23-.21-8.49-.69-15.23-7.31-15.23-15.83v-32a16 16 0 0 1 15.34-16C266.24 8.46 261.18 8 256 8 119 8 8 119 8 256s111 248 248 248c98 0 182.42-56.95 222.71-139.42-4.13 7.86-14.23 10.55-22 6.11z"
-                                              class="fa-secondary" style="fill: #ddd;"></path>
-                                        <path fill="#00a0e3"
-                                              d="M271.23 72.62c-8.49-.69-15.23-7.31-15.23-15.83V24.73c0-9.11 7.67-16.78 16.77-16.17C401.92 17.18 504 124.67 504 256a246 246 0 0 1-25 108.24c-4 8.17-14.37 11-22.26 6.45l-27.84-15.9c-7.41-4.23-9.83-13.35-6.2-21.07A182.53 182.53 0 0 0 440 256c0-96.49-74.27-175.63-168.77-183.38z"
-                                              class="fa-primary" style="color: #000;"></path>
-                                    </g>
-                                </svg>
-                            </div>
-                        </div>
+<!--                        <div id="courses" class="tab-pane fade" role="tabpanel">-->
+<!--                            <div class="text-center">-->
+<!--                                <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg"-->
+<!--                                     class="svg-inline--fa fa-spinner-third fa-w-16 fa-spin" viewBox="0 0 512 512"-->
+<!--                                     style="max-width: 100px;">-->
+<!--                                    <g class="fa-group">-->
+<!--                                        <path fill="#ddd"-->
+<!--                                              d="M478.71 364.58zm-22 6.11l-27.83-15.9a15.92 15.92 0 0 1-6.94-19.2A184 184 0 1 1 256 72c5.89 0 11.71.29 17.46.83-.74-.07-1.48-.15-2.23-.21-8.49-.69-15.23-7.31-15.23-15.83v-32a16 16 0 0 1 15.34-16C266.24 8.46 261.18 8 256 8 119 8 8 119 8 256s111 248 248 248c98 0 182.42-56.95 222.71-139.42-4.13 7.86-14.23 10.55-22 6.11z"-->
+<!--                                              class="fa-secondary" style="fill: #ddd;"></path>-->
+<!--                                        <path fill="#00a0e3"-->
+<!--                                              d="M271.23 72.62c-8.49-.69-15.23-7.31-15.23-15.83V24.73c0-9.11 7.67-16.78 16.77-16.17C401.92 17.18 504 124.67 504 256a246 246 0 0 1-25 108.24c-4 8.17-14.37 11-22.26 6.45l-27.84-15.9c-7.41-4.23-9.83-13.35-6.2-21.07A182.53 182.53 0 0 0 440 256c0-96.49-74.27-175.63-168.77-183.38z"-->
+<!--                                              class="fa-primary" style="color: #000;"></path>-->
+<!--                                    </g>-->
+<!--                                </svg>-->
+<!--                            </div>-->
+<!--                        </div>-->
 <!--                        <div id="placements" class="tab-pane fade" role="tabpanel">-->
 <!--                            <div class="text-center">-->
 <!--                                <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg"-->
@@ -559,9 +559,10 @@ function getDetails(){
                 var response = res.response.data;
                 let collegeDet = collegeInfo(res);
                 let Hpoints = overviewTemp(res);
-                console.log(Hpoints);
-                $('.h-points').append(Hpoints);
                 $('.college-main').append(collegeDet);
+                setTimeout(function (){
+                    $('.h-points').append(Hpoints);
+                },100)
             }
         }
     })
@@ -569,7 +570,6 @@ function getDetails(){
 getDetails();
 function overviewTemp(res){
     let mainTemp = '';
-    
     if(res.response.data['affiliated_to']){
         var overviewTemp = `<div class="h-point1">
                                 <div class="fa-icon"><i class="fab fa-affiliatetheme"></i></div>
