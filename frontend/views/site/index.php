ebf65bb94e43db179b0015081b7632f549dbaca8
Kulwinder Sohal <kulwindersohal2@icloud.com>
2021-03-17 13:33:07 +0530

    search for (jaspreet)

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -37,8 +37,8 @@ $this->params['header_dark'] = false;
                                         </div>
                                     </form>
                                 </div><!-- Job Search 2 -->
-                                <span class="feature-links">Search For: <a href="/jobs">Jobs,</a>
-                                    <a href="/internships">Internships,</a> <a href="/training-programs">Training Courses,</a>
+                                <span class="feature-links">Search For: <a href="/organizations">Companies,</a>
+                                    <a href="/jobs">Jobs,</a> <a href="/internships">Internships,</a>
                                     <a href="/reviews">Reviews,</a> <a href="/learning">Learning Hub,</a> <a
                                             href="/courses">Courses,</a> <a href="/career-advice">Career Advice,</a>
                                     <a href="/education-loans">Education Loans</a>
