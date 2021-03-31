491fe1a123837b4a8399508e94e78eeaea25fbab
Shshank <vasishtshshank@gmail.com>
2021-03-31 13:16:04 +0530

    ic hiden

--- frontend/views/widgets/loan-table.php
+++ frontend/views/widgets/loan-table.php
@@ -31,18 +31,18 @@ use yii\helpers\Url;
 <!--                            <td>Loan With & Without Collateral <br>Available +  Moratorium period</td>-->
 <!--                            <td>Rs.10,000 + GST <br>(Rs.10,000 refundable)</td>-->
 <!--                        </tr>-->
-                        <tr>
-                            <td>
-                                <div class="loanProviderIcon">
-                                    <img src="<?= Url::to('@eyAssets/images/pages/education-loans/icici_bank_logo.png') ?>">
-                                </div>
-                            </td>
-                            <td>8% To 12% p.a.</td>
-                            <td>Rs.1 crore (With Collateral)<br>
-                                Rs. 40 Lakhs (Without Collateral)</td>
-                            <td>Loan With & Without Collateral <br>Available +  Moratorium period</td>
-                            <td>1 % of Loan Amount + GST</td>
-                        </tr>
+<!--                        <tr>-->
+<!--                            <td>-->
+<!--                                <div class="loanProviderIcon">-->
+<!--                                    <img src="--><?//= Url::to('@eyAssets/images/pages/education-loans/icici_bank_logo.png') ?><!--">-->
+<!--                                </div>-->
+<!--                            </td>-->
+<!--                            <td>8% To 12% p.a.</td>-->
+<!--                            <td>Rs.1 crore (With Collateral)<br>-->
+<!--                                Rs. 40 Lakhs (Without Collateral)</td>-->
+<!--                            <td>Loan With & Without Collateral <br>Available +  Moratorium period</td>-->
+<!--                            <td>1 % of Loan Amount + GST</td>-->
+<!--                        </tr>-->
                         <tr>
                             <td>
                                 <div class="loanProviderIcon">
