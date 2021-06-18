f1f7e090b9d35e3f994dd72b663b16ba8f97d115
Shshank <vasishtshshank@gmail.com>
2021-06-18 16:53:40 +0530

    Notes Error

--- common/models/HiringProcessNotes.php
+++ common/models/HiringProcessNotes.php
@@ -2,6 +2,8 @@
 
 namespace common\models;
 
+use Yii;
+
 /**
  * This is the model class for table "{{%hiring_process_notes}}".
  *
@@ -21,7 +23,7 @@ namespace common\models;
 class HiringProcessNotes extends \yii\db\ActiveRecord
 {
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public static function tableName()
     {
@@ -29,12 +31,12 @@ class HiringProcessNotes extends \yii\db\ActiveRecord
     }
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function rules()
     {
         return [
-            [['notes_enc_id', 'notes', 'applied_application_enc_id', 'created_by'], 'required'],
+            [['notes_enc_id', 'applied_application_enc_id', 'created_by'], 'required'],
             [['notes'], 'string'],
             [['created_on', 'last_updated_on'], 'safe'],
             [['notes_enc_id', 'applied_application_enc_id', 'created_by', 'last_updated_by'], 'string', 'max' => 100],
@@ -42,6 +44,23 @@ class HiringProcessNotes extends \yii\db\ActiveRecord
             [['applied_application_enc_id'], 'exist', 'skipOnError' => true, 'targetClass' => AppliedApplications::className(), 'targetAttribute' => ['applied_application_enc_id' => 'applied_application_enc_id']],
             [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => Users::className(), 'targetAttribute' => ['created_by' => 'user_enc_id']],
             [['last_updated_by'], 'exist', 'skipOnError' => true, 'targetClass' => Users::className(), 'targetAttribute' => ['last_updated_by' => 'user_enc_id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'notes_enc_id' => Yii::t('app', 'Notes Enc ID'),
+            'notes' => Yii::t('app', 'Notes'),
+            'applied_application_enc_id' => Yii::t('app', 'Applied Application Enc ID'),
+            'created_on' => Yii::t('app', 'Created On'),
+            'created_by' => Yii::t('app', 'Created By'),
+            'last_updated_on' => Yii::t('app', 'Last Updated On'),
+            'last_updated_by' => Yii::t('app', 'Last Updated By'),
         ];
     }
 
