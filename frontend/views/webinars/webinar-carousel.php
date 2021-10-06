d759782ca4876df1cac7e6790e38bf6598dd8fc4
aman <ak669212@gmail.com>
2021-10-06 16:54:15 +0530

    webinar carousel buttons added

--- frontend/views/webinars/webinar-carousel.php
+++ frontend/views/webinars/webinar-carousel.php
@@ -1,211 +1,119 @@
-   <!--image slider start-->
-    <div class="slider">
-      <div class="slides">
-        <!--radio buttons start-->
-        <input type="radio" name="radio-btn" id="radio1">
-        <input type="radio" name="radio-btn" id="radio2">
-        <!--radio buttons end-->
-        <!--slide images start-->
-        <div class="slide first">
-            <?= $this->render('/widgets/webinar-templates/webinar-one-speaker')?>
-        </div>
-        <div class="slide">
-            <?= $this->render('/widgets/webinar-templates/webinar-one-speaker1')?>
-        </div>
-        <!--slide images end-->
-        <!--automatic navigation start-->
-        <div class="navigation-auto">
-          <div class="auto-btn1"></div>
-          <div class="auto-btn2"></div>
-        </div>
-        <!--automatic navigation end-->
-      </div>
-      <!--manual navigation start-->
-      <div class="navigation-manual">
-        <label for="radio1" class="manual-btn"></label>
-        <label for="radio2" class="manual-btn"></label>
-      </div>
-      <!--manual navigation end-->
-      
-    </div>
-    <!--image slider end-->
+<div id="mycarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#mycarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#mycarousel" data-slide-to="1"></li>
+  </ol>
 
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <?= $this->render('/widgets/webinar-templates/webinar-one-speaker') ?>
+    </div>
+
+    <div class="item">
+      <?= $this->render('/widgets/webinar-templates/webinar-one-speaker1') ?>
+    </div>
+
+    <!-- more slides here -->
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#mycarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#mycarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 <?php
 $this->registerCss('
   
-  .slider{
-    width: 100%;
-    overflow: hidden;
-    margin-bottom: 50px;
-  }
-  
-  .slides{
-    width: 500%;
-    display: flex;
-  }
-  
-  .slides input{
-    display: none;
-  }
-  
-  .slide{
-    width: 20%;
-    transition: 2s;
-  }
-  
-  
-  /*css for manual slide navigation*/
-  
-  .navigation-manual{
-    position: absolute;
-    width: 100%;
-    margin-top: 10px;
-    display: flex;
-    justify-content: center;
-  }
-  
-  .manual-btn{
-    border: 2px solid #b7b7b7;
-    padding: 5px;
-    border-radius: 10px;
-    cursor: pointer;
-    transition: 1s;
-  }
-  
-  .manual-btn:not(:last-child){
-    margin-right: 10px;
-  }
-  
-  .manual-btn:hover{
-    background: #b7b7b7;
-  }
-  
-  #radio1:checked ~ .first{
-    margin-left: 0;
-  }
-  
-  #radio2:checked ~ .first{
-    margin-left: -20%;
-  }
-  
-  #radio3:checked ~ .first{
-    margin-left: -40%;
-  }
-  
-  #radio4:checked ~ .first{
-    margin-left: -60%;
-  }
-  
-  /*css for automatic navigation*/
-  
-  .navigation-auto{
-    position: absolute;
-    display: flex;
-    width: 100%;
-    justify-content: center;
-    margin-top: 560px;
-  }
-  
-  .navigation-auto div{
-    border: 2px solid #b7b7b7;
-    padding: 5px;
-    border-radius: 10px;
-    transition: 1s;
-  }
-  
-  .navigation-auto div:not(:last-child){
-    margin-right: 10px;
-  }
-  
-  #radio1:checked ~ .navigation-auto .auto-btn1{
-    background: #b7b7b7;
-  }
-  
-  #radio2:checked ~ .navigation-auto .auto-btn2{
-    background: #b7b7b7;
-  }
-  
-  #radio3:checked ~ .navigation-auto .auto-btn3{
-    background: #b7b7b7;
-  }
-  
-  #radio4:checked ~ .navigation-auto .auto-btn4{
-    background: #b7b7b7;
-  }
+.box {
+  border: 1px solid grey;
+  background-color: #d3d3d3;
+}
 
-  @media only screen and (max-width: 767px){
-    .navigation-auto{
-        margin-top: 760px;
-      }
-  }
-  @media only screen and (max-width: 576px){
-    .navigation-auto{
-        margin-top: 660px;
-      }
-  }
-        
+.large {
+  font-size: 150%;
+}
+
+.navbar-brand {
+  font-size:1.8em;
+}
+
+ #topContainer {
+    background-color: #e0e0e0;
+background-size: cover;
+width:100%;
+}
+
+
+#topRow h1 {
+  font-size: 300%;
+}
+
+.bold {
+  font-weight: bold;
+}
+
+.marginTop {
+  margin-top: 30px;
+}
+
+.center {
+  text-align:center;
+}
+
+.title {
+  margin-top: 100px;
+  font-size: 300%;
+}
+
+
+.full-screen {
+background-size: cover;
+background-position: center;
+background-repeat: no-repeat;
+}
+ 
+.carousel-control{
+  width: 5% !important;
+  background: none !important;
+}
 ');
 
-
-$script = <<<JS
-
-jQuery(document).ready(function ($) {
-
-setInterval(function () {
-    moveRight();
-}, 3000);
-
-var slideCount = $('#slider ul li').length;
-var slideWidth = $('#slider ul li').width();
-var slideHeight = $('#slider ul li').height();
-var sliderUlWidth = slideCount * slideWidth;
-
-$('#slider').css({ width: slideWidth, height: slideHeight });
-
-$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-
-$('#slider ul li:last-child').prependTo('#slider ul');
-
-function moveLeft() {
-    $('#slider ul').animate({
-        left: + slideWidth
-    }, 200, function () {
-        $('#slider ul li:last-child').prependTo('#slider ul');
-        $('#slider ul').css('left', '');
-    });
-};
-
-function moveRight() {
-    $('#slider ul').animate({
-        left: - slideWidth
-    }, 200, function () {
-        $('#slider ul li:first-child').appendTo('#slider ul');
-        $('#slider ul').css('left', '');
-    });
-};
-
-$('a.control_prev').click(function () {
-    moveLeft();
-});
-
-$('a.control_next').click(function () {
-    moveRight();
-});
-
-});    
-
-JS;
 
 ?>
 
 
 <script type="text/javascript">
-    var counter = 1;
-    setInterval(function(){
-      document.getElementById('radio' + counter).checked = true;
-      counter++;
-      if(counter > 2){
-        counter = 1;
-      }
-    }, 12000);
-    </script>
+  $('.carousel').carousel({
+    interval: 6000,
+    pause: "false"
+  });
+
+
+  var $item = $('.carousel .item');
+  var $wHeight = $(window).height();
+
+  $item.height($wHeight);
+  $item.addClass('full-screen');
+
+  $('.carousel img').each(function() {
+    var $src = $(this).attr('src');
+    var $color = $(this).attr('data-color');
+    $(this).parent().css({
+      'background-image': 'url(' + $src + ')',
+      'background-color': $color
+    });
+    $(this).remove();
+  });
+
+  $(window).on('resize', function() {
+    $wHeight = $(window).height();
+    $item.height($wHeight);
+  });
+</script>
