50bd1d874e6cc3709dad9350cd93e2dd11624e1b
vasishtshshank <vasishtshshank@gmail.com>
2021-10-08 16:07:03 +0530

    webinar changes

--- frontend/views/webinars/webinar-carousel.php
+++ frontend/views/webinars/webinar-carousel.php
@@ -1,9 +1,9 @@
 <div id="mycarousel" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#mycarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#mycarousel" data-slide-to="1"></li>
-  </ol>
+<!--  <ol class="carousel-indicators">-->
+<!--    <li data-target="#mycarousel" data-slide-to="0" class="active"></li>-->
+<!--    <li data-target="#mycarousel" data-slide-to="1"></li>-->
+<!--  </ol>-->
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
@@ -15,14 +15,14 @@
   </div>
 
   <!-- Controls -->
-  <a class="left carousel-control" href="#mycarousel" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#mycarousel" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+<!--  <a class="left carousel-control" href="#mycarousel" role="button" data-slide="prev">-->
+<!--    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>-->
+<!--    <span class="sr-only">Previous</span>-->
+<!--  </a>-->
+<!--  <a class="right carousel-control" href="#mycarousel" role="button" data-slide="next">-->
+<!--    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>-->
+<!--    <span class="sr-only">Next</span>-->
+<!--  </a>-->
 </div>
 <?php
 $this->registerCss('
