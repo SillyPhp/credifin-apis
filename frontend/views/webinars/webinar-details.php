5e9846609ae3bc7dfae8ae37a89d4f90aefe4d20
vasishtshshank <vasishtshshank@gmail.com>
2021-10-16 16:10:40 +0530

    Webinar Expired page simple

--- frontend/views/webinars/webinar-details.php
+++ frontend/views/webinars/webinar-details.php
@@ -108,7 +108,7 @@ Yii::$app->view->registerJs('var registeration_status = "' . $registeration_stat
                     <div class="countdown gradient clearfix">
                         <?php if ($is_expired) { ?>
                             <div>
-                                <a id="joinBtn">This Webinar Is Expired</a>
+                                <p class="expiredtext">This Webinar Is Expired</p>
                             </div>
                         <?php } elseif ($webinar['status'] == 1 || $webinar['status'] == 0) { ?>
                             <div id="join">
@@ -841,7 +841,8 @@ display:none;
 #counter{
 display:none;
 }
-#joinBtn, #joinOtherBtn{
+
+#joinBtn, #joinOtherBtn, .expiredtext{
     font-size: 25px;
     padding: 48px;
     display: block;
