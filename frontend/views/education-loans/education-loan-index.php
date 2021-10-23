8cf0b2a1f154d76748c9b3bb96a020d082db724b
vasishtshshank <vasishtshshank@gmail.com>
2021-10-23 15:38:11 +0530

    webinar detail page

--- frontend/views/education-loans/education-loan-index.php
+++ frontend/views/education-loans/education-loan-index.php
@@ -11,7 +11,7 @@ use yii\helpers\Url;
     <?= $this->render('/widgets/loan-products') ?>
     
     <?= $this->render('/widgets/covid-offer-banner', ['availUrl' => '/education-loans/apply']) ?>
-<?= $this->render('/webinars/webinar-carousel')?>
+
     <?= $this->render('/widgets/loan-why-empower-youth') ?>
     <?= $this->render('/widgets/education-loan-internship') ?>
     
@@ -127,7 +127,7 @@ use yii\helpers\Url;
     </section>
 
     <?= $this->render('/widgets/Our-lending-partners') ?>
-    <?= $this->render('/widgets/testimonials') ?>
+   
     <section class="bg-blue pb10">
         <?= $this->render('/widgets/choose-education-loan') ?>
     </section>
