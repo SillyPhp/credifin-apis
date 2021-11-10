085892b89e947a2ab7f496e8e0c77573af8c6f7b
Ravinder Singh <ravindersaini15697@gmail.com>
2021-11-10 13:29:34 +0530

    list filters done

--- api/modules/v3/controllers/QuizController.php
+++ api/modules/v3/controllers/QuizController.php
@@ -24,12 +24,10 @@ class QuizController extends ApiBaseController
 
     public function actionList()
     {
-        $dt = new \DateTime();
-        $tz = new \DateTimeZone('Asia/Kolkata');
-        $dt->setTimezone($tz);
-        $currentTime = $dt->format('Y-m-d H:i:s');
 
-        $quizzes = $this->__getQuizData();
+        $options = Yii::$app->request->post();
+
+        $quizzes = $this->__getQuizData($options);
 
         if ($quizzes) {
             return $this->response(200, ['status' => 200, 'message' => 'success', 'data' => $quizzes]);
@@ -41,10 +39,23 @@ class QuizController extends ApiBaseController
 
     private function __getQuizData($options = null)
     {
+
+        $limit = 10;
+        $page = 1;
+
+        if (isset($options['limit']) && !empty($options['limit'])) {
+            $limit = $options['limit'];
+        }
+
+        if (isset($options['page']) && !empty($options['page'])) {
+            $page = $options['page'];
+        }
+
         $q = Quizzes::find()
             ->alias('a')
             ->select(['a.quiz_enc_id', 'a.name', 'a.price', 'a.is_paid', 'b.name currency_name', 'b.code currency_code', 'b.html_code currency_html_code',
                 'a.title', 'a.slug', 'c1.name category', 'c2.name parent_category', 'a.quiz_start_datetime', 'a.quiz_end_datetime', 'a.duration', 'a.description', 'a.registration_start_datetime', 'a.registration_end_datetime',
+                'a.num_of_ques',
                 'CASE WHEN a.sharing_image IS NOT NULL THEN CONCAT("' . Url::to(Yii::$app->params->digitalOcean->baseUrl . Yii::$app->params->digitalOcean->rootDirectory . Yii::$app->params->upload_directories->quiz->sharing->image, 'https') . '", a.sharing_image_location, "/", a.sharing_image) ELSE NULL END sharing_image',
                 "CASE WHEN a.registration_end_datetime IS NOT NULL THEN DATEDIFF(a.registration_end_datetime, CONVERT_TZ(Now(),'+00:00','+10:30')) ELSE NULL END days_left",
                 "CASE 
@@ -60,16 +71,46 @@ class QuizController extends ApiBaseController
             ->joinWith(['quizRewards d' => function ($d) {
                 $d->select(['d.quiz_reward_enc_id', 'd.quiz_enc_id', 'd.position_enc_id', 'd1.name position_name', 'd.price']);
                 $d->joinWith(['positionEnc d1'], false);
+                $d->joinWith(['quizRewardCertificates d2' => function ($d2) {
+                    $d2->select(['d2.reward_certificate_enc_id', 'd2.quiz_reward_enc_id', 'd2.name']);
+                    $d2->onCondition(['d2.is_deleted' => 0]);
+                }]);
                 $d->onCondition(['d.is_deleted' => 0]);
             }])
-            ->where(['a.is_deleted' => 0])
-            ->orderBy(['a.created_on' => SORT_DESC])
-            ->limit(10)
-            ->asArray();
+            ->where(['a.is_deleted' => 0]);
+
+        // checking quiz categories
+        if (isset($options['category']) && !empty($options['category'])) {
+            $q->andFilterWhere(['c1.name' => $options['category']]);
+        }
+
+        // paid or free
+        if (isset($options['payment']) && !empty($options['payment'])) {
+            if ($options['payment'] == 'paid') {
+                $q->andWhere(['a.is_paid' => 1]);
+            } elseif ($options['payment'] == 'free') {
+                $q->andWhere(['a.is_paid' => 0]);
+            }
+        }
+
+        // expired or live
+        if (isset($options['status']) && !empty($options['status'])) {
+            if ($options['status'] == 'expired') {
+                $q->having(['<', 'days_left', 0]);
+            } elseif ($options['live'] == 'live') {
+                $q->having(['>=', 'days_left', 0]);
+            }
+        }
+
+        $q->orderBy(['a.created_on' => SORT_DESC])
+            ->limit($limit)
+            ->offset = ($page - 1) * $limit;
+
+
         if (isset($options['slug']) && !empty($options['slug'])) {
-            $q = $q->one();
+            $q = $q->andWhere(['a.slug' => $options['slug']])->asArray()->one();
         } else {
-            $q = $q->all();
+            $q = $q->asArray()->all();
         }
 
         return $q;
@@ -86,9 +127,14 @@ class QuizController extends ApiBaseController
         $options['slug'] = $params['slug'];
 
         $detail = $this->__getQuizData($options);
+        unset($options['slug']);
+
+        $options['category'] = $detail['category'];
+        $options['limit'] = 3;
+        $related = $this->__getQuizData($options);
 
         if ($detail) {
-            return $this->response(200, ['status' => 200, 'message' => 'quiz detail', 'data' => $detail]);
+            return $this->response(200, ['status' => 200, 'message' => 'quiz detail', 'detail' => $detail, 'related' => $related]);
         }
 
         return $this->response(404, ['status' => 404, 'message' => 'not found']);
