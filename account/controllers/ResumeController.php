a0c183b551d4b71bb2448d7b3a8c660b20825393
Sneh <snehkant93@gmail.com>
2021-11-23 15:30:47 +0530

    reverse

--- account/controllers/ResumeController.php
+++ account/controllers/ResumeController.php
@@ -286,7 +286,6 @@ class ResumeController extends Controller
             }],false)
             ->groupBy(['e.name'])
             ->where(['a.organization_enc_id'=>Yii::$app->user->identity->organization_enc_id])
-            ->andWhere(['!=','e.category_enc_id',NULL])
             ->asArray()
             ->all();
 
