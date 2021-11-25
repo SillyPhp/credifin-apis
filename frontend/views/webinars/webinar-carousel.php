fc80645715989afe5b8a3dd41c58e3ee6892c78c
Ajay Juneja <ajayjuneja52@gmail.com>
2021-11-25 12:07:05 +0530

    issues fixed

--- frontend/views/webinars/webinar-carousel.php
+++ frontend/views/webinars/webinar-carousel.php
@@ -93,31 +93,31 @@ background-repeat: no-repeat;
 ?>
 
 
-<script type="text/javascript">
-  $('.carousel').carousel({
-    interval: 6000,
-    pause: "false"
-  });
-
-
-  var $item = $('.carousel .item');
-  var $wHeight = $(window).height();
-
-  $item.height($wHeight);
-  $item.addClass('full-screen');
-
-  $('.carousel img').each(function() {
-    var $src = $(this).attr('src');
-    var $color = $(this).attr('data-color');
-    $(this).parent().css({
-      'background-image': 'url(' + $src + ')',
-      'background-color': $color
-    });
-    $(this).remove();
-  });
-
-  $(window).on('resize', function() {
-    $wHeight = $(window).height();
-    $item.height($wHeight);
-  });
-</script>
+<!--<script type="text/javascript">-->
+<!--  $('.carousel').carousel({-->
+<!--    interval: 6000,-->
+<!--    pause: "false"-->
+<!--  });-->
+<!---->
+<!---->
+<!--  var item = $('.carousel .item');-->
+<!--  var wHeight = $(window).height();-->
+<!---->
+<!--  item.height(wHeight);-->
+<!--  item.addClass('full-screen');-->
+<!---->
+<!--  $('.carousel img').each(function() {-->
+<!--    var src = $(this).attr('src');-->
+<!--    var color = $(this).attr('data-color');-->
+<!--    $(this).parent().css({-->
+<!--      'background-image': 'url(' + src + ')',-->
+<!--      'background-color': color-->
+<!--    });-->
+<!--    $(this).remove();-->
+<!--  });-->
+<!---->
+<!--  $(window).on('resize', function() {-->
+<!--    wHeight = $(window).height();-->
+<!--    item.height(wHeight);-->
+<!--  });-->
+<!--</script>-->
