ca30bb68a557f9a8316bd12255b8aac7ebf85732
Aman <ak669212@gmail.com>
2021-12-02 16:10:34 +0530

    resume bank widget

---
+++ common/widgets/resume-bank-widget.php
@@ -0,0 +1,226 @@
+<?php
+
+use yii\helpers\Url;
+?>
+
+<div class="career-for-company">
+    <div class="career-banner">
+        <div class="blue-strip-img">
+            <img src="<?= Url::to('@eyAssets/images/pages/drop-resume/resume-bank-top-left-bg.png'); ?>">
+        </div>
+        <div class="blue-strip-img right-bg">
+            <img src="<?= Url::to('@eyAssets/images/pages/drop-resume/resume-bank-top-right-bg.png'); ?>">
+        </div>
+        <div class="row">
+          <div class="career-heading col-sm-6">
+              <div class="heading-text">
+                  <h1>RESUME BANK</h1>
+                  <p>When you're looking for new employees, use the RESUME BANK feature to strategically manage your applicant pool.</p>
+              </div>
+              <ul class="car-point">
+                  <li><i class="fa fa-chevron-right"></i>Faster identification of qualified applicants</li>
+                  <li><i class="fa fa-chevron-right"></i>Good candidates won't be left behind</li>
+                  <li><i class="fa fa-chevron-right"></i>Time and cost saving</li>
+              </ul>
+              <div class="btn-div">
+                  <a href="/drop-resume" class="btn-generate-link">Learn More</a>
+                  <div class="btn-top-circle"></div>
+              </div>
+          </div>
+          <div class="career-image col-sm-6" style="padding: 40px 0">
+            <div class="career-img">
+              <img src="<?= URL::to('@eyAssets/images/pages/drop-resume/resume-bank-img.png') ?>">
+            </div>
+          </div>
+        </div>
+    </div>
+</div>
+
+<?php
+$this->registerCss('
+#careerPageLink{
+//    visibility: hidden;
+    position: absolute;
+    opacity: 0
+}
+.linkDiv{
+    background: #fff;
+    display: none;
+    width: fit-content;
+    padding: 10px 40px 10px 15px;
+    border: 1px solid #00a0e3;
+    border-radius: 25px;
+    position: relative;
+}
+.linkDiv a{
+    color: #333;
+    font-size: 13px;   
+}
+.linkDiv a:hover,
+.linkDiv button:hover{
+    color: #00a0e3;
+    transition: .3s ease;
+}
+.linkDiv button{
+    position:absolute;
+    right: 13px;
+    padding: 0px;
+    background: none;
+    color: #333;
+    border: none;
+}
+
+.career-banner{
+    box-shadow: 0px 1px 10px 2px #eee !important;
+    display: flex;
+    position: relative;
+    align-items: center;
+    margin-bottom:30px;
+    justify-content: center;
+}
+.heading-text h1 {
+  margin: 0;
+  font-family: Roboto;
+  font-weight: 700;
+  font-size: 35px;
+}
+.heading-text p {
+  font-family: roboto;
+  color: #333;
+  font-size: 16px;
+  margin: 0;
+  line-height: 1.3;
+}
+ul.car-point {
+  list-style: none;
+  margin-bottom: 20px;
+}
+ul.car-point li i {
+  margin-right: 8px;
+  color: #00a0e3;
+}
+ul.car-point li {
+  margin-bottom: 3px;
+  font-family: roboto;
+  color: #333;
+  font-size: 16px;
+}
+.blue-strip-img{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 80px;
+}
+.blue-strip-img.right-bg{
+  right: 0;
+  left: unset;
+  width: 380px;
+}
+
+.blue-strip-img img{
+  width: 100%;
+}
+.career-heading{
+  padding: 70px 0 50px 60px !important;
+}
+.heading-text{
+  margin-bottom: 15px;
+}
+.career-image{
+  padding: 70px 50px 60px 0 !important;
+}
+.career-image img{
+  width: 100%;
+}
+.career-img{
+  max-width: 432px;
+  margin: auto;
+}
+.btn-div{
+  position: relative;
+  z-index: 1;
+}
+.btn-generate-link{
+    padding: 10px 20px;
+    border-radius: 8px;
+    display: inline-block;
+    border:none;
+    background: #00A0e3;
+    color: #fff;
+    font-size: 100%;
+    transition: all .3s linear;
+}
+.btn-generate-link:hover {
+    padding: 10px 35px;
+    transition: all .3 linear;
+    color: #fff;
+}
+.btn-top-circle{
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  left: 0;
+  top: 0;
+  transform: translate(-30%, -40%);
+  border-radius: 50%;
+  z-index: -1;
+  background: linear-gradient(180deg, #8CC8F0 0%, #319EE7 100%);
+  display: none;
+}
+.image-mobile{
+  display: none;
+}
+@media only screen and (max-width: 600px) {
+  .career-image{
+    display: none;
+  }
+  .career-heading{
+    width: 100%;
+    font-size: 105%;
+    padding: 70px 20px !important;
+  }
+  .image-mobile{
+    position: absolute;
+    display: block;
+    width: 240px;
+    bottom: 20px;
+    right: 0;
+  }
+  .image-mobile img{
+    width: 100%;
+  }
+}
+@media only screen and (max-width: 767px){
+  .career-image{
+    display: none;
+  }
+  .blue-strip-img.right-bg{
+    display: none;
+  }
+  .career-heading{
+    padding: 70px 30px !important;
+  }
+}
+');
+$script = <<<JS
+$('.btn-generate-link').on('click', function (){
+    $('.btn-generate-link').hide()
+    $('.linkDiv').show()
+})
+// $('.copyLinkBtn').on('click', function (){
+//     let careerLink = $(this).attr('data-link');
+//     careerLink.select();
+//     careerLink.setSelectionRange(0, 9999);
+//     document.execCommand("copy");
+// })
+JS;
+$this->registerJS($script);
+?>
+<script>
+copyLink = () => {
+    let careerLink = document.querySelector('#careerPageLink');
+    careerLink.select();
+    careerLink.setSelectionRange(0, 9999);
+    document.execCommand("copy");
+}
+</script>
