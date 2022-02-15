23184279dfaa544604fdd08eca83b424bad35f84
Ravinder Singh <ravindersaini15697@gmail.com>
2022-02-15 11:55:19 +0530

    minor errors solved

--- account/controllers/EducationLoansController.php
+++ account/controllers/EducationLoansController.php
@@ -4,6 +4,7 @@ namespace account\controllers;
 
 use account\models\loanApplications\LoanSanctionedForm;
 use common\models\AssignedLoanProvider;
+use common\models\EducationLoanPayments;
 use common\models\LoanApplicationLogs;
 use common\models\LoanApplications;
 use common\models\LoanDocuments;
@@ -542,9 +543,9 @@ class EducationLoansController extends Controller
                 ->andWhere(['z.report_enc_id' => $id])
                 ->asArray()->one();
             if ($data['loanAppEnc']['created_by'] && $data['loanAppEnc']['created_by'] == Yii::$app->user->identity->user_enc_id) {
-                    return $this->render('emi-details', [
-                        'data' => $data
-                    ]);
+                return $this->render('emi-details', [
+                    'data' => $data
+                ]);
             } else {
                 throw new HttpException(404, Yii::t('account', 'Page not found.'));
             }
@@ -566,57 +567,69 @@ class EducationLoansController extends Controller
                 $c->select(['c.assigned_loan_provider_enc_id', 'c.loan_application_enc_id', 'c.status', 'c1.name', '(CASE
                 WHEN c1.logo IS NULL OR c1.logo = "" THEN
                 CONCAT("https://ui-avatars.com/api/?name=", c1.name, "&size=50&rounded=false&background=", REPLACE(c1.initials_color, "#", ""), "&color=ffffff") ELSE
-                CONCAT("' . Yii::$app->params->digitalOcean->baseUrl . Yii::$app->params->digitalOcean->rootDirectory .  Yii::$app->params->upload_directories->organizations->logo . '", c1.logo_location, "/", c1.logo) END
+                CONCAT("' . Yii::$app->params->digitalOcean->baseUrl . Yii::$app->params->digitalOcean->rootDirectory . Yii::$app->params->upload_directories->organizations->logo . '", c1.logo_location, "/", c1.logo) END
                 ) organization_logo']);
-                $c->joinWith(['providerEnc c1'],false);
-                $c->onCondition(['c.is_deleted'=>0]);
+                $c->joinWith(['providerEnc c1'], false);
+                $c->onCondition(['c.is_deleted' => 0]);
             }])
-            ->joinWith(['loanApplicationNotifications e' => function($e){
-                $e->select(['e.message','e.loan_application_enc_id','e.created_on']);
+            ->joinWith(['loanApplicationNotifications e' => function ($e) {
+                $e->select(['e.message', 'e.loan_application_enc_id', 'e.created_on']);
             }])
-            ->joinWith(['loanSanctionReports d'=>function($d){
-                $d->select(['d.report_enc_id','d.loan_app_enc_id','d.loan_amount','d.processing_fee','d.rate_of_interest']);
-                $d->joinWith(['loanEmiStructures d1'=>function($d1){
-                    $d1->select(['d1.loan_structure_enc_id','d1.sanction_report_enc_id','d1.due_date','d1.amount','d1.is_advance']);
+            ->joinWith(['loanSanctionReports d' => function ($d) {
+                $d->select(['d.report_enc_id', 'd.loan_app_enc_id', 'd.loan_amount', 'd.processing_fee', 'd.rate_of_interest']);
+                $d->joinWith(['loanEmiStructures d1' => function ($d1) {
+                    $d1->select(['d1.loan_structure_enc_id', 'd1.sanction_report_enc_id', 'd1.due_date', 'd1.amount', 'd1.is_advance']);
                 }]);
             }])
-            ->where(['a.is_deleted'=>0,'a.created_by'=>Yii::$app->user->identity->user_enc_id])
+            ->where(['a.is_deleted' => 0, 'a.created_by' => Yii::$app->user->identity->user_enc_id])
             ->groupBy(['a.loan_app_enc_id'])
-            ->orderBy(['a.created_on'=>SORT_DESC])
+            ->orderBy(['a.created_on' => SORT_DESC])
             ->asArray()
             ->all();
         $loan_apps = LoanApplications::find()
-            ->select(['loan_app_enc_id','applicant_name','amount'])
-            ->where(['is_deleted'=>0,'created_by'=>Yii::$app->user->identity->user_enc_id])
+            ->select(['loan_app_enc_id', 'applicant_name', 'amount'])
+            ->where(['is_deleted' => 0, 'created_by' => Yii::$app->user->identity->user_enc_id])
             ->groupBy(['loan_app_enc_id'])
-            ->orderBy(['created_on'=>SORT_DESC])
+            ->orderBy(['created_on' => SORT_DESC])
             ->asArray()
             ->all();
 
+        if ($loan_application) {
+            foreach ($loan_application as $key => $val) {
+                if (!$loan_application['educationLoanPayments']) {
+                    $get_amount = EducationLoanPayments::find()->where(['loan_app_enc_id' => $val['loan_app_enc_id']])->one();
+                    $loan_application[$key]['payment_token'] = $get_amount->payment_token;
+                    $loan_application[$key]['amount'] = $get_amount->payment_amount	;
+                }
+            }
+        }
+
 //        print_r($loan_application);
 //        die();
-        return $this->render('loan-details',[
+        return $this->render('loan-details', [
             'applications' => $loan_application,
             'loan_apps' => $loan_apps
         ]);
     }
-    public function actionLoanProviderDetail($loan_provider_id){
+
+    public function actionLoanProviderDetail($loan_provider_id)
+    {
         $assigned_loan_provider = AssignedLoanProvider::find()
             ->alias('a')
-            ->select(['a.assigned_loan_provider_enc_id','a.loan_application_enc_id','a.status','b.name',
+            ->select(['a.assigned_loan_provider_enc_id', 'a.loan_application_enc_id', 'a.status', 'b.name',
                 '(CASE
                         WHEN b.logo IS NULL OR b.logo = "" THEN
                         CONCAT("https://ui-avatars.com/api/?name=", b.name, "&size=200&rounded=false&background=", REPLACE(b.initials_color, "#", ""), "&color=ffffff") ELSE
-                        CONCAT("' . Yii::$app->params->digitalOcean->baseUrl . Yii::$app->params->digitalOcean->rootDirectory .  Yii::$app->params->upload_directories->organizations->logo . '", b.logo_location, "/", b.logo) END
+                        CONCAT("' . Yii::$app->params->digitalOcean->baseUrl . Yii::$app->params->digitalOcean->rootDirectory . Yii::$app->params->upload_directories->organizations->logo . '", b.logo_location, "/", b.logo) END
                     ) organization_logo',
-                'c.loan_type','c.amount'])
-            ->joinWith(['providerEnc b'],false)
-            ->joinWith(['loanApplicationEnc c'],false)
-            ->where(['a.assigned_loan_provider_enc_id'=>$loan_provider_id])
+                'c.loan_type', 'c.amount'])
+            ->joinWith(['providerEnc b'], false)
+            ->joinWith(['loanApplicationEnc c'], false)
+            ->where(['a.assigned_loan_provider_enc_id' => $loan_provider_id])
             ->asArray()
             ->one();
-        return $this->renderAjax('/widgets/education-loan/loan-detail-individual-dashboard',[
-            'loanApplication'=>$assigned_loan_provider,
+        return $this->renderAjax('/widgets/education-loan/loan-detail-individual-dashboard', [
+            'loanApplication' => $assigned_loan_provider,
         ]);
     }
 }
