8b03d89737846ef8de48681e975b5c3eb929f16e
Kulwinder Sohal <kulwindersohal2@icloud.com>
2022-03-07 13:49:05 +0530

    absolute url issue solved

--- frontend/views/learning/video-detail.php
+++ frontend/views/learning/video-detail.php
@@ -6,13 +6,13 @@ use yii\helpers\Url;
 
 $this->title = $video_detail['title'];
 $this->params['header_dark'] = true;
-$link = Yii::$app->request->getAbsoluteUrl("https");
+$link = Url::to(Yii::$app->request->url,'https');
 $keywords = 'Learning';
 $description = $video_detail['description'];
 $image = $video_detail['cover_image'];
 $this->params['seo_tags'] = [
     'rel' => [
-        'canonical' => Yii::$app->request->getAbsoluteUrl("https"),
+        'canonical' => Url::to(Yii::$app->request->url,'https'),
     ],
     'name' => [
         'keywords' => $keywords,
@@ -27,7 +27,7 @@ $this->params['seo_tags'] = [
         'og:locale' => 'en',
         'og:type' => 'website',
         'og:site_name' => 'Empower Youth',
-        'og:url' => Yii::$app->request->getAbsoluteUrl("https"),
+        'og:url' => Url::to(Yii::$app->request->url,'https'),
         'og:title' => Yii::t('frontend', $this->title) . ' ' . Yii::$app->params->seo_settings->title_separator . ' ' . Yii::$app->params->site_name,
         'og:description' => $description,
         'og:image' => $image,
--- frontend/views/organizations/genrate-blog.php
+++ frontend/views/organizations/genrate-blog.php
@@ -20,7 +20,7 @@ $description = 'Empower Youth is a career development platform where you can fin
 $image = Url::to('/assets/common/images/top10.jpg', 'https');
 $this->params['seo_tags'] = [
     'rel' => [
-        'canonical' => Yii::$app->request->getAbsoluteUrl("https"),
+        'canonical' => Url::to(Yii::$app->request->url,'https'),
     ],
     'name' => [
         'keywords' => $keywords,
@@ -35,7 +35,7 @@ $this->params['seo_tags'] = [
         'og:locale' => 'en',
         'og:type' => 'website',
         'og:site_name' => 'Empower Youth',
-        'og:url' => Yii::$app->request->getAbsoluteUrl("https"),
+        'og:url' => Url::to(Yii::$app->request->url,'https'),
         'og:title' => Yii::t('frontend', $this->title) . ' ' . Yii::$app->params->seo_settings->title_separator . ' ' . Yii::$app->params->site_name,
         'og:description' => $description,
         'og:image' => $image,
