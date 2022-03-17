21a4e36063275d88f92898524330395b335945d1
Ajay Juneja <ajayjuneja52@gmail.com>
2022-03-17 18:07:31 +0530

    Godaddy Academy section.

--- frontend/views/webinars/webinars-landing.php
+++ frontend/views/webinars/webinars-landing.php
@@ -354,7 +354,10 @@ if ($optedWebinar) {
         </div>
     </div>
 </section>
-
+<?= $this->render('/widgets/godaddy_offer', [
+//        'course' => 'Web Design Course',
+        'discount' => '20'
+]); ?>
 <section class="webinar-on-device">
     <div class="container">
         <div class="row">
