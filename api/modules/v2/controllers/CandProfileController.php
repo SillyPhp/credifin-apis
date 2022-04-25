90a8bde0eb7d48dc40fa438e37b5a2ad11a88307
sneh <sizzlyboysk@gmail.com>
2022-04-25 12:15:24 +0530

    limit and offset setup api permission key,  user profiles

--- api/modules/v2/controllers/CandProfileController.php
+++ api/modules/v2/controllers/CandProfileController.php
@@ -821,52 +821,6 @@ class CandProfileController extends ApiBaseController
         }
     }
 
-    public function actionGetUncompletedProfileUsers($page = null, $limit = null, $permissionKey)
-    {
-        if ($permissionKey == Yii::$app->params->EmpowerYouth->permissionKey) {
-            if (!$page) {
-                $page = 1;
-            }
-            if (!$limit) {
-                $limit = 10;
-            }
-            $offset = ($page - 1) * $limit;
-            $users = Users::find()
-                ->alias('a')
-                ->distinct()
-                ->select(['a.user_enc_id', 'a.email', 'CONCAT(a.first_name, " ", a.last_name) name', 'a.username', 'a.created_on'])
-                ->joinWith(['userTypeEnc b'])
-                ->joinWith(['userEducations c' => function ($c) {
-                    $c->addSelect(['c.user_enc_id', 'c.education_enc_id']);
-                }])
-                ->joinWith(['userSkills d' => function ($d) {
-                    $d->addSelect(['d.created_by', 'd.user_skill_enc_id']);
-                }])
-                ->joinWith(['userSpokenLanguages e' => function ($e) {
-                    $e->addSelect(['e.created_by', 'e.user_language_enc_id']);
-                }])
-                ->andWhere(['a.is_deleted' => 0])
-                ->offset($offset)
-                ->limit($limit)
-                ->orderBy(['a.created_on' => SORT_DESC])
-                ->asArray()
-                ->all();
-            if ($users) {
-                for ($i = 0; $i < count($users); $i++) {
-                    if ($users[$i]['gender'] == '' || $users[$i]['description'] == '' || $users[$i]['image'] == '' || $users[$i]['city_enc_id'] == '' || $users[$i]['dob'] == '' || $users[$i]['experience'] == '' || $users[$i]['job_function'] == '' || empty($users[$i]['userEducations']) || empty($users[$i]['userSkills']) || empty($users[$i]['userSpokenLanguages'])) {
-                        $percentage = self::getProfileCompleted($users[$i]['user_enc_id']);
-                        $users[$i] += ['profile_percentage' => $percentage];
-                    }
-                }
-                return $this->response(200, ['status' => 200, 'data' => $users]);
-            } else {
-                return $this->response(201, ['status' => 201, 'message' => 'Data Not Found']);
-            }
-        } else {
-            return $this->response(401, ['status' => 401, 'message' => 'unauthorized']);
-        }
-    }
-
     private function getProfileCompleted($key)
     {
         $d = (new Query())
