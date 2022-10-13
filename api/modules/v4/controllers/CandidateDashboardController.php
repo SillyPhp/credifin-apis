9fd1117bf2de532a6ed2459bcda9e650a38284ce
Ravinder Singh <ravindersaini15697@gmail.com>
2022-10-13 13:40:50 +0530

    minor change

--- api/modules/v4/controllers/CandidateDashboardController.php
+++ api/modules/v4/controllers/CandidateDashboardController.php
@@ -175,7 +175,7 @@ class CandidateDashboardController extends ApiBaseController
                 ->joinWith(['loanApplicationOptions c'], false)
                 ->joinWith(['loanDisbursementSchedules d'], false)
                 ->where(['a.created_by' => $user->user_enc_id, 'a.is_deleted' => 0, 'a.loan_type' => 'Vehicle Loan', 'a.source' => 'EmpowerFintech'])
-                ->andWhere(['b1.slug' => 'rav1', 'b.status' => 5])
+                ->andWhere(['b1.slug' => 'phfleasing', 'b.status' => 5])
                 ->andWhere(['>=', "d.disbursed_date", $date])
                 ->andWhere([
                     'or',
