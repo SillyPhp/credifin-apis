fa6a7d648cd6532bb09277260e01b4e7042233d1
nishtha <nishthapctebtech19cse@gmail.com>
2023-08-23 17:09:33 +0530

    added branch code and product code in organization location and financer loan product respectively

--- api/modules/v4/controllers/OrganizationsController.php
+++ api/modules/v4/controllers/OrganizationsController.php
@@ -112,7 +112,7 @@ class OrganizationsController extends ApiBaseController
             $orgLocations->location_enc_id = $utilitiesModel->encrypt();
             $orgLocations->organization_enc_id = $user->organization_enc_id;
             $orgLocations->location_name = $params['location_name'];
-//            $orgLocations->branch_code = $params['branch_code'];
+            $orgLocations->branch_code = $params['branch_code'];
             $orgLocations->organization_code = $params['organization_code'];
             $orgLocations->location_for = json_encode(['1']);
             $orgLocations->address = $params['address'];
@@ -155,6 +155,7 @@ class OrganizationsController extends ApiBaseController
             (!empty($params['location_name'])) ? $location->location_name = $params['location_name'] : "";
             (!empty($params['city_id'])) ? $location->city_enc_id = $params['city_id'] : "";
             (!empty($params['organization_code'])) ? $location->organization_code = $params['organization_code'] : "";
+            (!empty($params['branch_code'])) ? $location->branch_code = $params['branch_code'] : "";
             (!empty($params['address'])) ? $location->address = $params['address'] : "";
             (!empty($params['status'])) ? $location->status = $params['status'] : "";
             $location->last_updated_by = $user->user_enc_id;
@@ -192,7 +193,7 @@ class OrganizationsController extends ApiBaseController
 
             $locations = OrganizationLocations::find()
                 ->alias('a')
-                ->select(['a.location_enc_id', 'a.location_enc_id as id', 'b.city_code', 'a.organization_code', 'a.location_name', 'a.location_for', 'a.address', 'b.name city', 'CONCAT(a.location_name , ", ", b.name) as value', 'b.city_enc_id', 'a.status'])
+                ->select(['a.location_enc_id', 'a.location_enc_id as id', 'b.city_code', 'a.branch_code', 'a.organization_code', 'a.location_name', 'a.location_for', 'a.address', 'b.name city', 'CONCAT(a.location_name , ", ", b.name) as value', 'b.city_enc_id', 'a.status'])
                 ->joinWith(['cityEnc b'], false)
                 ->andWhere(['a.is_deleted' => 0, 'a.organization_enc_id' => $org])
                 ->asArray()
@@ -1018,6 +1019,7 @@ class OrganizationsController extends ApiBaseController
             $utilitiesModel = new \common\models\Utilities();
             $utilitiesModel->variables['string'] = time() . rand(10, 100000);
             $product->financer_loan_product_enc_id = $utilitiesModel->encrypt();
+            $product->product_code = $params['product_code'];
             $product->created_by = $user->user_enc_id;
             $product->created_on = date('Y-m-d H:i:s');
             $save = 'save';
@@ -1029,6 +1031,9 @@ class OrganizationsController extends ApiBaseController
 
         if (!empty($params['assigned_financer_loan_type_enc_id'])) {
             $product->assigned_financer_loan_type_enc_id = $params['assigned_financer_loan_type_enc_id'];
+        }
+        if (!empty($params['product_code'])) {
+            $product->product_code = $params['product_code'];
         }
 
         $product->updated_by = $user->user_enc_id;
