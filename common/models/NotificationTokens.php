4e1c11927c78b00fb7e4ab85737a1a973ef7345d
Shshank Vasisht <vasishtshshank@gmail.com>
2023-09-12 16:20:25 +0530

    Models updated

--- common/models/NotificationTokens.php
+++ common/models/NotificationTokens.php
@@ -15,6 +15,8 @@ use Yii;
  * @property string $created_on
  * @property string $token_expired_on
  * @property int $is_deleted
+ *
+ * @property Users $userEnc
  */
 class NotificationTokens extends \yii\db\ActiveRecord
 {
@@ -32,14 +34,21 @@ class NotificationTokens extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['token_enc_id', 'user_enc_id', 'token', 'created_on'], 'required'],
+            [['token_enc_id', 'user_enc_id', 'token'], 'required'],
             [['created_on', 'token_expired_on'], 'safe'],
             [['is_deleted'], 'integer'],
             [['token_enc_id', 'user_enc_id', 'device_id'], 'string', 'max' => 100],
             [['token'], 'string', 'max' => 255],
+            [['token_enc_id'], 'unique'],
+            [['user_enc_id'], 'exist', 'skipOnError' => true, 'targetClass' => Users::className(), 'targetAttribute' => ['user_enc_id' => 'user_enc_id']],
         ];
     }
 
+   
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
     public function getUserEnc()
     {
         return $this->hasOne(Users::className(), ['user_enc_id' => 'user_enc_id']);
