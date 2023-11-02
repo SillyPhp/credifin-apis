3da871f214d81a83d7e8b707963bac4e1d402fc7
Ajay Juneja <ajayjuneja52@gmail.com>
2023-11-03 00:50:19 +0530

    data move query changes

--- frontend/controllers/TestCacheController.php
+++ frontend/controllers/TestCacheController.php
@@ -1,15 +1,24 @@
 <?php
 
 namespace frontend\controllers;
+use common\models\Categories;
 use common\models\CreditLoanApplicationReports;
-use common\models\EmiCollection;
 use common\models\LoanApplicantResidentialInfo;
-use common\models\LoanApplications;
 use common\models\LoanCoApplicants;
+use common\models\OpenTitles;
+use common\models\User;
+use common\models\Usernames;
+use common\models\UserTypes;
+use frontend\models\applications\Careerjet_API;
+use common\models\AppliedApplications;
+use common\models\Auth;
+use common\models\EducationLoanPayments;
+use common\models\LoanApplications;
+use common\models\Posts;
+use common\models\SkillsUpPostAssignedBlogs;
+use common\models\Users;
+use common\models\EmiCollection;
 use common\models\RandomColors;
-use common\models\spaces\Spaces;
-use phpDocumentor\Reflection\Types\Null_;
-use Razorpay\Api\Api;
 use common\models\Utilities;
 use yii\helpers\Url;
 use yii\web\Controller;
@@ -18,15 +27,86 @@ use yii\web\Response;
 
 class TestCacheController extends Controller
 {
-//    public function actionTokenTest(){
-//        $options['org_id'] = 'R09YXEkaql0a9WWvJ8Y27531Wdo82J';
-//        $keys = \common\models\credentials\Credentials::getrazorpayKey($options);
-//        print_r($keys);
-//    }
+    public function actionTest()
+    {
+        try {
+            $model = new Auth();
+            $model->user_id = 12;
+            if (!$model->save()) //model errors
+            {
+                throw new \Exception (implode("<br />", \yii\helpers\ArrayHelper::getColumn($model->errors, 0, false)));
+            }
 
-    public function actionTokenTest(){
-       $x = $this->generateAllCloudAuthHeader('GET','https://staging.allcloud.in/apiv2phfleasing/api/Customer/GetCustomerByCIFIdAsync/1','');
-      print_r($x);
+            //some kind of err
+        } catch (\Exception $exception) {
+            return $exception->getMessage(); //final messege for user
+        }
+    }
+
+    public function actionBulkEmail($start=0,$end=0){
+        $user_type = UserTypes::findOne([
+            'user_type' => 'Individual',
+        ]);
+        $query = LoanApplications::find()
+            ->alias('a')
+            ->select(['a.loan_app_enc_id','a.applicant_name','a.email','a.phone'])
+            ->joinWith(['educationLoanPayments b'],false)
+            ->where(['a.created_by'=>null])
+//                ->andWhere(['b.payment_status'=>'captured'])
+            ->asArray()
+            ->all();
+        for ($i=$start;$i<$end;$i++){
+            $username = $this->generate_username($query[$i]['applicant_name'], 10000);
+            $array_name = explode(' ',$query[$i]['applicant_name']);
+            $utilitiesModel = new Utilities();
+            $utilitiesModel->variables['password'] = $query[$i]['phone'];
+            $utilitiesModel->variables['string'] = time() . rand(100, 100000);
+            $transaction = Yii::$app->db->beginTransaction();
+            try {
+                $user = new Users([
+                    'username' => $username,
+                    'user_enc_id' => Yii::$app->security->generateRandomString(32),
+                    'first_name' =>$array_name[0],
+                    'last_name' => $array_name[1].(($array_name[2])?$array_name[2]:null),
+                    'email' => $query['email'],
+                    'phone' => $query['phone'],
+                    'password' => $utilitiesModel->encrypt_pass(),
+                    'auth_key' => Yii::$app->security->generateRandomString(8),
+                    'user_type_enc_id' => $user_type->user_type_enc_id,
+                    'status' => 'Active',
+                    'initials_color' => RandomColors::one(),
+                    'is_credential_change' => 1,
+                ]);
+                if ($user->save()) {
+                    $usernamesModel = new Usernames();
+                    $usernamesModel->username = $username;
+                    $usernamesModel->assigned_to = 1;
+                    if (!$usernamesModel->validate() || !$usernamesModel->save()) {
+                        $transaction->rollBack();
+                        //throw new \Exception (implode("<br />", \yii\helpers\ArrayHelper::getColumn($usernamesModel->errors, 0, false)));
+                    }
+                    $loan = LoanApplications::findOne(['loan_app_enc_id'=>$query['loan_app_enc_id']]);
+                    $loan->created_by = $user->user_enc_id;
+                    if (!$loan->save){
+                        $transaction->rollBack();
+                        //throw new \Exception (implode("<br />", \yii\helpers\ArrayHelper::getColumn($loan->errors, 0, false)));
+                    }else{
+                        $params = [];
+                        $params['username'] = $username;
+                        $params['password'] = $query['phone'];
+                        $params['email'] = $query['email'];
+                        $params['name'] = $query['applicant_name'];
+                        echo $this->educationLoanRegister($params);
+                    }
+                }else{
+                    $transaction->rollBack();
+                    //throw new \Exception (implode("<br />", \yii\helpers\ArrayHelper::getColumn($user->errors, 0, false)));
+                }
+            } catch (Exception $e) {
+                $transaction->rollBack();
+                //return $e;
+            }
+        }
     }
 
     private function generate_username($string_name=null, $rand_no = 200){
@@ -141,6 +221,7 @@ class TestCacheController extends Controller
                 $model = $model->where(['between','created_on',$start,$end]);
             }
             $model = $model->limit($limit)
+                ->andWhere(['not', ['is_deleted' => 2]])
                 ->offset($offset)
                 ->asArray()->all();
 
@@ -196,6 +277,7 @@ class TestCacheController extends Controller
                 $model = $model->where(['between','created_on',$start,$end]);
             }
             $model = $model->limit($limit)
+                ->andWhere(['not', ['is_deleted' => 2]])
                 ->offset($offset)
                 ->asArray()->all();
             $transaction = Yii::$app->db->beginTransaction();
@@ -235,6 +317,7 @@ class TestCacheController extends Controller
                 $model = $model->where(['between','created_on',$start,$end]);
             }
             $model = $model->limit($limit)
+                ->andWhere(['not', ['is_deleted' => 2]])
                 ->offset($offset)
                 ->asArray()->all();
 
