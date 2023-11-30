8d84d8b9d3289202424e58404be8f3ad1003dc48
Sneh <snehkant93@gmail.com>
2023-11-30 16:14:29 +0530

    removed test controller codes

--- frontend/controllers/TestCacheController.php
+++ frontend/controllers/TestCacheController.php
@@ -1,30 +1,6 @@
 <?php
 
 namespace frontend\controllers;
-use common\models\Categories;
-use common\models\CreditLoanApplicationReports;
-use common\models\LoanApplicantResidentialInfo;
-use common\models\LoanCoApplicants;
-use common\models\OpenTitles;
-use common\models\User;
-use common\models\Usernames;
-use common\models\UserTypes;
-use frontend\models\applications\Careerjet_API;
-use common\models\AppliedApplications;
-use common\models\Auth;
-use common\models\EducationLoanPayments;
-use common\models\LoanApplications;
-use common\models\Posts;
-use common\models\SkillsUpPostAssignedBlogs;
-use common\models\Users;
-use common\models\EmiCollection;
-use common\models\RandomColors;
-use common\models\Utilities;
-use yii\helpers\Url;
-use yii\web\Controller;
-use Yii;
-use yii\web\Response;
-
 class TestCacheController extends Controller
 {
 
