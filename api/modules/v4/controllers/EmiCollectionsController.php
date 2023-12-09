f186018b1faa047d4e34f2a7dbe9f61bd5ad654a
Sujal Dhamija <sujaldhamija@gmail.com>
2023-12-09 18:08:14 +0530

    condition applied for emi list api

--- api/modules/v4/controllers/EmiCollectionsController.php
+++ api/modules/v4/controllers/EmiCollectionsController.php
@@ -64,6 +64,7 @@ class EmiCollectionsController extends ApiBaseController
             $this->response(401, ["message" => "unauthorized"]);
         }
         $params = Yii::$app->request->get();
+        $c = !empty($params['company']);
         if (empty($params["start_date"]) || empty($params["end_date"])) {
             return $this->response(500, ["error" => "'start date' or 'end date' missing"]);
         }
@@ -92,11 +93,13 @@ class EmiCollectionsController extends ApiBaseController
                 "b.company_id",
                 "b.company_name",
             ])
-            ->innerJoinWith(["loanAccountEnc b" => function ($b) {
-                $b->andOnCondition(["NOT", [
-                    "b.company_id" => [null, ''],
-                    "b.company_name" => [null, '']
-                ]]);
+            ->innerJoinWith(["loanAccountEnc b" => function ($b) use ($c) {
+                if (!$c) {
+                    $b->andOnCondition(["NOT", [
+                        "b.company_id" => [null, ''],
+                        "b.company_name" => [null, '']
+                    ]]);
+                }
             }], false)
             ->joinWith(["assignedLoanPayments c" => function ($c) {
                 $c->andOnCondition(["IS NOT", "c.emi_collection_enc_id", null]);
