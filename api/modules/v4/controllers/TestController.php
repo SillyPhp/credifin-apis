95e0e110b142b2c1b8545bf2d770f2984ad8cd49
Sujal Dhamija <sujaldhamija@gmail.com>
2023-12-16 13:18:53 +0530

    fields added in pool api

--- api/modules/v4/controllers/TestController.php
+++ api/modules/v4/controllers/TestController.php
@@ -142,6 +142,7 @@ class TestController extends ApiBaseController
                 "abc.aadhaar_number",
                 "abc.voter_card_number",
                 "abc.pan_number",
+                "abc.marital_status",
                 "a.loan_purpose",
                 "a.chassis_number",
                 "a.invoice_number",
@@ -183,7 +184,7 @@ class TestController extends ApiBaseController
                 $abc->andOnCondition(["abc.is_deleted" => 0, "abc.borrower_type" => "Borrower"]);
             }], false)
             ->joinWith(["loanCoApplicants d" => function ($d) {
-                $d->select(["d.loan_app_enc_id", "d.name", "d1.address", "d.co_applicant_dob", "d1.postal_code", "d.phone",
+                $d->select(["d.loan_app_enc_id", "d.name", "d1.address", "d.co_applicant_dob", "d1.postal_code", "d.phone", "d.marital_status",
                     "(CASE WHEN d.gender = 1 THEN 'Male' WHEN d.gender = 2 THEN 'Female' ELSE 'Other' END) gender",
                     "d.borrower_type", "d.loan_co_app_enc_id", "d.aadhaar_number", "d.voter_card_number", "d.pan_number",
                     "d.cibil_score", "d.driving_license_number", "d.marital_status"]);
@@ -225,22 +226,6 @@ class TestController extends ApiBaseController
             unset($item['loanPurposes']);
         }
 
-        foreach ($query as &$item) {
-            $cibils = $this->CreditReports($item["loan_app_enc_id"]);
-            if (!empty($cibils)) {
-
-                if (in_array($item["loan_app_enc_id"], array_keys($cibils))) {
-                    $item["cibil_score"] = $cibils[$item["loan_app_enc_id"]];
-                }
-                if (!empty($item["loanCoApplicants"])) {
-                    foreach ($item["loanCoApplicants"] as &$loanCoApplicant) {
-                        if (in_array($loanCoApplicant["loan_co_app_enc_id"], array_keys($cibils))) {
-                            $loanCoApplicant["cibil_score"] = $cibils[$loanCoApplicant["loan_co_app_enc_id"]];
-                        }
-                    }
-                }
-            }
-        }
         return $this->response(200, ["status" => 200, "data" => $query]);
     }
 
@@ -597,9 +582,9 @@ class TestController extends ApiBaseController
         }
     }
 
-    public static function generateApplicationNumber($cityCode,$purposeCode,$loap_p_code,$yearmonth)
+    public static function generateApplicationNumber($cityCode, $purposeCode, $loap_p_code, $yearmonth)
     {
-        for ($i=0;$i<=100;$i++){
+        for ($i = 0; $i <= 100; $i++) {
             $loan_num['product_code'] = $loap_p_code;
             $branchCode = '';
             $cityCode = $cityCode;
@@ -616,8 +601,8 @@ class TestController extends ApiBaseController
                 ->select(['a.application_number'])
                 ->where([
                     'OR',
-                    ['LIKE', 'application_number', $pattern1,false],
-                    ['LIKE', 'application_number', $pattern2,false]
+                    ['LIKE', 'application_number', $pattern1, false],
+                    ['LIKE', 'application_number', $pattern2, false]
                 ])
                 ->orderBy([
                     "CAST(SUBSTRING_INDEX(application_number, '-', -1) AS UNSIGNED)" => SORT_DESC
@@ -637,18 +622,19 @@ class TestController extends ApiBaseController
         }
     }
 
-    public function actionDuplicate($page=1,$limit=500){
+    public function actionDuplicate($page = 1, $limit = 500)
+    {
         $offset = ($page - 1) * $limit;
         $data = LoanApplications::find()
-            ->select(['application_number','COUNT(*) count'])
+            ->select(['application_number', 'COUNT(*) count'])
 //            ->joinWith(['assignedLoanProviders b'=>function($c){
 //                $c->andWhere(['!=','b.status',31]);
 //            }],false,'INNER JOIN')
             ->groupBy('application_number')
             ->where([
                 'or',
-                ['!=','application_number',Null],
-                ['!=','application_number','']
+                ['!=', 'application_number', Null],
+                ['!=', 'application_number', '']
             ])
             ->having('COUNT(*) > 1')
             ->limit($limit)
@@ -676,17 +662,17 @@ class TestController extends ApiBaseController
                 ];
             }
             // print_r($result);exit();
-            foreach ($result as $dat){
+            foreach ($result as $dat) {
                 $loan_array = explode("-", $dat['application_number']);
-                if (count($loan_array)==4){
-                    for ($i=0;$i<($dat['count']-1);$i++) {
-                        echo  $newSeries = self::generateApplicationNumber($loan_array[1],null,$loan_array[0],$loan_array[2]);
-                        self::saveNewSeries($newSeries,$dat['IDs'][$i]);
+                if (count($loan_array) == 4) {
+                    for ($i = 0; $i < ($dat['count'] - 1); $i++) {
+                        echo $newSeries = self::generateApplicationNumber($loan_array[1], null, $loan_array[0], $loan_array[2]);
+                        self::saveNewSeries($newSeries, $dat['IDs'][$i]);
                     }
-                }else if (count($loan_array)==5){
-                    for ($i=0;$i<($dat['count']-1);$i++) {
-                        $newSeries = self::generateApplicationNumber($loan_array[2],$loan_array[1],$loan_array[0],$loan_array[3]);
-                        self::saveNewSeries($newSeries,$dat['IDs'][$i]);
+                } else if (count($loan_array) == 5) {
+                    for ($i = 0; $i < ($dat['count'] - 1); $i++) {
+                        $newSeries = self::generateApplicationNumber($loan_array[2], $loan_array[1], $loan_array[0], $loan_array[3]);
+                        self::saveNewSeries($newSeries, $dat['IDs'][$i]);
                     }
                 }
             }
@@ -694,27 +680,31 @@ class TestController extends ApiBaseController
             echo 'no results left';
         endif;
     }
-    private function saveNewSeries($newSeries,$id){
-        $model = LoanApplications::findOne(['loan_app_enc_id'=>$id]);
+
+    private function saveNewSeries($newSeries, $id)
+    {
+        $model = LoanApplications::findOne(['loan_app_enc_id' => $id]);
         $model->application_number = $newSeries;
-        if (!$model->save()){
+        if (!$model->save()) {
             print_r($model->getErrors());
-        }else{
+        } else {
             return false;
         }
     }
-    public function actionCopyDuplicates($page=1,$limit=500){
+
+    public function actionCopyDuplicates($page = 1, $limit = 500)
+    {
         $offset = ($page - 1) * $limit;
         $data = LoanApplications::find()
-            ->select(['application_number','COUNT(*) count'])
+            ->select(['application_number', 'COUNT(*) count'])
 //            ->joinWith(['assignedLoanProviders b'=>function($c){
 //                $c->andWhere(['!=','b.status',31]);
 //            }],false,'INNER JOIN')
             ->groupBy('application_number')
             ->where([
                 'or',
-                ['!=','application_number',Null],
-                ['!=','application_number','']
+                ['!=', 'application_number', Null],
+                ['!=', 'application_number', '']
             ])
             ->having('COUNT(*) > 1')
             ->limit($limit)
@@ -723,10 +713,10 @@ class TestController extends ApiBaseController
             ->all();
         $updateAll = [];
         if ($data):
-            foreach ($data as $dat){
+            foreach ($data as $dat) {
                 $loan_array = explode("-", $dat['application_number']);
-                if (count($loan_array)>=4):
-                    $updateAll[] =  LoanApplications::updateAll(['old_application_number'=>$dat['application_number']],['application_number'=>$dat['application_number']]);
+                if (count($loan_array) >= 4):
+                    $updateAll[] = LoanApplications::updateAll(['old_application_number' => $dat['application_number']], ['application_number' => $dat['application_number']]);
                 endif;
             }
             echo count($updateAll);
