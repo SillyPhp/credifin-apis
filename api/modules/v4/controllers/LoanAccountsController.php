322d26825d9c6a44e07c68395a7856ae8a3db250
Ajay Juneja <ajayjuneja52@gmail.com>
2023-12-20 20:00:36 +0530

    Collection managers query changes

--- api/modules/v4/controllers/LoanAccountsController.php
+++ api/modules/v4/controllers/LoanAccountsController.php
@@ -1418,9 +1418,9 @@ class LoanAccountsController extends ApiBaseController
             ->all();
 
         $inserted = 0;
-        foreach($data as $key => $val){
-            $utilitiesModel = new Utilities();
-            $utilitiesModel->variables["string"] = time() . rand(100, 100000);
+        $utilitiesModel = new Utilities();
+        foreach($data as $val){
+            $utilitiesModel->variables["string"] = time() . rand(100, 10000000);
 
             $insert = Yii::$app->db->createCommand()
                 ->insert(AssignedLoanAccounts::tableName(), [
