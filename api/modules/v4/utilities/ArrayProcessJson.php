15a0509bb9d12b293265c5a46b5f2029a9cb6214
Sneh <snehkant93@gmail.com>
2024-02-16 18:04:13 +0530

    emi collection reporrt

--- api/modules/v4/utilities/ArrayProcessJson.php
+++ api/modules/v4/utilities/ArrayProcessJson.php
@@ -8,7 +8,7 @@ class ArrayProcessJson
             $list[$key]['SMAO']['total_cases_count'] = $lst['total_cases_count_SMA0'];
             $list[$key]['SMAO']['collected_cases_count'] = $lst['collected_cases_count_SMA0'];
             $list[$key]['SMAO']['target_amount'] = $lst['target_amount_SMA0'];
-            $list[$key]['SMAO']['collected_verified_amoun'] = $lst['collected_verified_amount_SMA0'];
+            $list[$key]['SMAO']['collected_verified_amount'] = $lst['collected_verified_amount_SMA0'];
             $list[$key]['SMAO']['collected_unverified_amount'] = $lst['collected_unverified_amount_SMA0'];
 
             unset($list[$key]['total_cases_count_SMA0']);
@@ -20,7 +20,7 @@ class ArrayProcessJson
             $list[$key]['SMA1']['total_cases_count'] = $lst['total_cases_count_SMA1'];
             $list[$key]['SMA1']['collected_cases_count'] = $lst['collected_cases_count_SMA1'];
             $list[$key]['SMA1']['target_amount'] = $lst['target_amount_SMA1'];
-            $list[$key]['SMA1']['collected_verified_amoun'] = $lst['collected_verified_amount_SMA1'];
+            $list[$key]['SMA1']['collected_verified_amount'] = $lst['collected_verified_amount_SMA1'];
             $list[$key]['SMA1']['collected_unverified_amount'] = $lst['collected_unverified_amount_SMA1'];
 
             unset($list[$key]['total_cases_count_SMA1']);
@@ -32,7 +32,7 @@ class ArrayProcessJson
             $list[$key]['SMA2']['total_cases_count'] = $lst['total_cases_count_SMA2'];
             $list[$key]['SMA2']['collected_cases_count'] = $lst['collected_cases_count_SMA2'];
             $list[$key]['SMA2']['target_amount'] = $lst['target_amount_SMA2'];
-            $list[$key]['SMA2']['collected_verified_amoun'] = $lst['collected_verified_amount_SMA2'];
+            $list[$key]['SMA2']['collected_verified_amount'] = $lst['collected_verified_amount_SMA2'];
             $list[$key]['SMA2']['collected_unverified_amount'] = $lst['collected_unverified_amount_SMA2'];
 
             unset($list[$key]['total_cases_count_SMA2']);
@@ -44,7 +44,7 @@ class ArrayProcessJson
             $list[$key]['NPA']['total_cases_count'] = $lst['total_cases_count_NPA'];
             $list[$key]['NPA']['collected_cases_count'] = $lst['collected_cases_count_NPA'];
             $list[$key]['NPA']['target_amount'] = $lst['target_amount_NPA'];
-            $list[$key]['NPA']['collected_verified_amoun'] = $lst['collected_verified_amount_NPA'];
+            $list[$key]['NPA']['collected_verified_amount'] = $lst['collected_verified_amount_NPA'];
             $list[$key]['NPA']['collected_unverified_amount'] = $lst['collected_unverified_amount_NPA'];
 
             unset($list[$key]['total_cases_count_NPA']);
@@ -56,7 +56,7 @@ class ArrayProcessJson
             $list[$key]['OnTime']['total_cases_count'] = $lst['total_cases_count_OnTime'];
             $list[$key]['OnTime']['collected_cases_count'] = $lst['collected_cases_count_OnTime'];
             $list[$key]['OnTime']['target_amount'] = $lst['target_amount_OnTime'];
-            $list[$key]['OnTime']['collected_verified_amoun'] = $lst['collected_verified_amount_OnTime'];
+            $list[$key]['OnTime']['collected_verified_amount'] = $lst['collected_verified_amount_OnTime'];
             $list[$key]['OnTime']['collected_unverified_amount'] = $lst['collected_unverified_amount_OnTime'];
 
             unset($list[$key]['total_cases_count_OnTime']);
