f9fee5d94cc2b5ae4b840b7e1493a57ae7d7b966
nishtha <nishthapctebtech19cse@gmail.com>
2024-03-12 14:17:53 +0530

    added fields in emi search

--- api/modules/v4/controllers/OrganizationsController.php
+++ api/modules/v4/controllers/OrganizationsController.php
@@ -2404,9 +2404,28 @@ class OrganizationsController extends ApiBaseController
             $query = LoanAccountsExtended::find()
                 ->alias('a')
                 ->select([
-                    'a.loan_account_enc_id', 'a.loan_account_number', 'a.name', 'a.phone', 'a.loan_account_enc_id AS id',
+                    'a.loan_account_enc_id', "(CASE WHEN a.nach_approved = 0 THEN 'Inactive' WHEN a.nach_approved = 1 THEN 'Active' ELSE '' END) AS nach_approved", "CONCAT(ac.first_name, ' ', COALESCE(ac.last_name, '')) as assigned_caller",
+                    'a.loan_account_number', 'a.name', 'a.phone', 'a.loan_account_enc_id AS id',
+                    "CASE WHEN a.bucket = 'onTime' THEN a.emi_amount ELSE
+                    (CASE WHEN COALESCE(SUM(a.ledger_amount), 0) + COALESCE(SUM(a.overdue_amount), 0) < a.emi_amount * (CASE 
+                        WHEN a.bucket = 'sma-0' THEN 1.25
+                        WHEN a.bucket IN ('sma-1', 'sma-2') THEN 1.50
+                        WHEN a.bucket = 'npa' THEN 2
+                        ELSE 1
+                    END)  
+                    THEN COALESCE(SUM(a.ledger_amount), 0) + COALESCE(SUM(a.overdue_amount), 0)  
+                        ELSE emi_amount * 
+                            (CASE 
+                                WHEN a.bucket = 'sma-0' THEN 1.25
+                                WHEN a.bucket IN ('sma-1', 'sma-2') THEN 1.50
+                                WHEN a.bucket = 'npa' THEN 2
+                                ELSE 1
+                        END) 
+                    END) 
+                END target_collection_amount", "COALESCE(SUM(a.ledger_amount), 0) + COALESCE(SUM(a.overdue_amount), 0) AS total_pending_amount",
                     'a.emi_amount', 'a.overdue_amount', 'a.ledger_amount', 'a.loan_type', 'a.emi_date'
                 ])
+                ->joinWith(["assignedCaller ac"], false)
                 ->where(['a.is_deleted' => 0])
                 ->andWhere([
                     'OR',
@@ -2415,6 +2434,7 @@ class OrganizationsController extends ApiBaseController
                     ['LIKE', 'a.phone', $params['loan_number']],
                     ['LIKE', 'a.lms_loan_account_number', $params['loan_number']],
                 ])
+                ->groupBy(['a.loan_account_enc_id'])
                 ->limit(20)
                 ->asArray()
                 ->all();
